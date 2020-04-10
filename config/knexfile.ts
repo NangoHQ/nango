71d7b0ce224b98383d93fdd448a0c2d7d09ef0d5
Tanguy Antoine <antoine@bearer.sh>
2020-04-10 15:31:10 +0200

    build: enless command

--- config/knexfile.ts
+++ config/knexfile.ts
@@ -2,6 +2,17 @@
 import { connection } from './constants'
 
 module.exports = {
+  development: {
+    connection,
+    client: 'postgresql',
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
   production: {
     connection,
     client: 'postgresql',
