ca8cb896b716366de81a65ac476f491da7d22c6c
Cédric Fabianski <cfabianski@me.com>
2020-04-15 02:42:43 +0200

    fix: fix migrations

--- config/migrations/20200415004008_add_setup_id_to_authentications.ts
+++ config/migrations/20200415004008_add_setup_id_to_authentications.ts
@@ -3,13 +3,13 @@ import * as Knex from 'knex'
 export async function up(knex: Knex): Promise<any> {
   // tslint:disable-next-line:ter-prefer-arrow-callback
   return knex.schema.alterTable('authentications', function(table) {
-    table.string('setup-id')
+    table.string('setup_id')
   })
 }
 
 export async function down(knex: Knex): Promise<any> {
   // tslint:disable-next-line:ter-prefer-arrow-callback
   return knex.schema.alterTable('authentications', function(table) {
-    table.dropColumn('setup-id')
+    table.dropColumn('setup_id')
   })
 }
