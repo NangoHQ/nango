aa6ba66929ea7a404728d88b92aa7d2fb6aeb806
Corentin Brossault <corentin.brossault@gmail.com>
2020-05-04 17:25:02 +0200

    feat: add integration methods

--- src/clients/javascript/src/types.d.ts
+++ src/clients/javascript/src/types.d.ts
@@ -1,8 +1,9 @@
+import { AxiosRequestConfig } from 'axios'
+
 declare namespace Types {
   export interface ConnectOptions {
     authId?: string
     setupId?: string
-    configId?: string
   }
 
   export interface ConnectSuccess {
@@ -10,6 +11,20 @@ declare namespace Types {
   }
 
   export interface ConnectError extends Error {}
+
+  export interface IntegrationOptions {
+    authId?: string
+    setupId?: string
+  }
+
+  export type RequestHeaders = Record<string, string | number | undefined>
+  export type RequestMethod = AxiosRequestConfig['method']
+
+  export interface RequestParameters {
+    headers?: RequestHeaders
+    query?: Record<string, string | number>
+    body?: any
+  }
 }
 
 export default Types
