e51a114814f2864da11b88a3c0031b5eebdb1e8c
Corentin Brossault <corentin.brossault@gmail.com>
2020-05-06 17:33:16 +0200

    feat: improve common page (home, errors, etc.)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,8 @@
-import { auth } from './auth'
-import { proxy } from './proxy'
 import { api } from './api'
+import { auth } from './auth'
 import { dashboard } from './dashboard'
+import { home } from './home'
+import { proxy } from './proxy'
 import * as legacy from './legacy'
 
-export { auth, proxy, api, dashboard, legacy }
+export { api, auth, dashboard, home, proxy, legacy }
