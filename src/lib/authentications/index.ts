7fb31f9078e38a43726bf500c9fc40718e5c9203
Corentin Brossault <corentin.brossault@gmail.com>
2020-05-12 15:38:20 +0200

    feat: support of proxy requests for OAuth2

--- src/lib/authentications/index.ts
+++ src/lib/authentications/index.ts
@@ -3,15 +3,17 @@
  */
 
 import { store } from '../database'
+import { Types } from '../../types'
 
 /**
  * Retrieve an authentication from the database
+ *
  * @param authId (string) - The authentication ID
  */
 
-export const getAuthentication = async (integration: string, authId: string) => {
+export const getAuthentication = async (integration: string, authId: string): Promise<Types.Authentication> => {
   return await store('authentications')
-    .select('auth_id', 'user_attributes', 'created_at', 'updated_at')
+    .select('auth_id', 'payload', 'created_at', 'updated_at')
     .where({ buid: integration, auth_id: authId })
     .first()
 }
--- src/routes/proxy.ts
+++ src/routes/proxy.ts
@@ -1,90 +1,55 @@
 /**
  * Proxy feature
  *
- * Use Pizzly as a proxy and let handle the authentication as well as the right path.
+ * Use Pizzly as a proxy to make authenticated
+ * requests to third-party APIs.
  */
 
-import * as express from 'express'
-import https from 'https'
-import * as integrations from '../lib/integrations'
-import * as authentications from '../lib/authentications'
+import express from 'express'
+import { incomingRequestHandler } from '../lib/proxy'
 
 const proxy = express.Router()
 
-proxy.all('/:integration*', async (req, res, next) => {
-  // Pseudocode
-  // 1. Get setupId
-  // 2. Get authId
-  // 3. Get configuration
-  // 4. Make the request
-  // 5. If it failed for an authorization reason
-  //    1. Refresh the token
-  //    2. Make the request (again)
-  // 6. Return response
+/**
+ * Handle proxy requests.
+ *
+ * Some examples:
+ *  - GET /github/user/ will retrieve information from GitHub API on the "/user" endpoint
+ *  - POST /slack/reminders.add will create a reminder on Slack API "/reminders.add" endpoint.
+ */
 
-  // Retrieve request inputs
-  const authId = req.get('Pizzly-Auth-Id') || ''
-  const integrationName = req.params.integration
-
-  // Retrieve integration & authentication details
-  const integration = await integrations.get(integrationName)
-  const authentication = await authentications.get(integrationName, authId)
-
-  // Prepare the request
-  const endpoint = req.originalUrl.substring(('/proxy/' + integrationName).length + 1)
-  const url = new URL(endpoint, integration.request.baseURL)
-  const options = {
-    url: URL,
-    method: req.method,
-    headers: { ...integration.request.headers }
-  }
-
-  if (authentication) {
-  }
-
-  // Make the request
-  // @ts-ignore
-  const externalRequest = https.request(url, options, externalResponse => {
-    // Return status code
-    res.status(externalResponse.statusCode)
-
-    // Set headers
-    for (let header in externalResponse.headers) {
-      res.setHeader(header, externalResponse.headers[header])
-    }
-
-    // Append data
-    externalResponse.on('data', chunk => {
-      res.send(chunk)
-    })
-
-    // Close request
-    externalResponse.on('end', () => {
-      res.end()
-    })
-  })
-
-  // Handle error
-  externalRequest.on('error', error => {
-    throw new Error(error)
-  })
-
-  // TODO handle passing body to the third-party API
-  // externalRequest.write(req.body)
-  externalRequest.end()
-})
+proxy.all('/:integration*', incomingRequestHandler)
 
 /**
- * Error handling (middleware)
+ * Error handling
  */
 
 proxy.use((req, res, next) => {
-  return res.status(404).json({ error: true, message: 'Ressource not found' })
+  return res.status(404).json({ error: { type: 'missing', message: 'Ressource not found' } })
 })
 
 proxy.use((err, req, res, next) => {
   console.error(err)
-  return res.status(500).json({ error: true, message: 'Bad request (error logged).' })
+
+  let status = 400
+  let type = 'invalid'
+  let message = 'Bad request'
+
+  switch (err.message) {
+    case 'missing_auth_id':
+      type = err.message
+      message = 'A valid auth_id is required to proceed with the proxy request.'
+      break
+    case 'unknown_integration':
+      type = err.message
+      message = 'The provided integration could not be found on the server.'
+      break
+    case 'unknown_authentication':
+      type = err.message
+      message = 'The provided authId could not be found on the database.'
+      break
+  }
+  return res.status(status).json({ error: { type, message } })
 })
 
 export { proxy }
