8aea658e770185788ab3733f814c49c6fef31b90
Corentin Brossault <corentin.brossault@gmail.com>
2020-05-13 18:03:50 +0200

    feat: option to limit frontend call to the proxy service

--- src/routes/proxy.ts
+++ src/routes/proxy.ts
@@ -11,8 +11,24 @@ import { incomingRequestHandler } from '../lib/proxy'
 
 const proxy = express.Router()
 
+/**
+ * Proxy authentication middleware
+ *
+ * Authenticated requests to the proxy service can use both a publishable key
+ * (using ?pizzly_pkey=... as a query string) or a secret key (using
+ * the Authentication header).
+ *
+ * By default requests using a publishable key are allowed, even if, as its name
+ * implies, the publishable key is public (available in your website source code).
+ * It's still considered safe, as you need both a valid publishable key and authId
+ * to make requests to a third party API.
+ */
+
 proxy.use((req, res, next) => {
-  if (req.query['pizzly_pkey']) {
+  // Limit access to the requests having a valid secret key only
+  const proxyUsesSecretKeyOnly = Boolean(process.env.PROXY_USES_SECRET_KEY_ONLY)
+
+  if (!proxyUsesSecretKeyOnly && req.query['pizzly_pkey']) {
     return access.publishableKey(req, res, next)
   } else {
     return access.secretKey(req, res, next)
