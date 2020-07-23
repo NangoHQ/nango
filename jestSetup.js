66845c114ba8877a8637096ccdaf60a7ab0b35bb
Corentin Brossault <corentin.brossault@gmail.com>
2020-07-23 10:02:29 +0200

    reduce the amount of file in root

--- jestSetup.js
+++ jestSetup.js
@@ -1,17 +1,10 @@
-const fs = require('fs')
-const path = require('path')
-nock = require('nock')
+const nock = require('nock')
 
 nock.disableNetConnect()
 nock.enableNetConnect(/127.0.0.1:.*/)
 
-const env = fs.readFileSync(path.join(__dirname, 'config', 'config.test.json'), {
-  encoding: 'utf8'
-})
-
 process.env = {
   ...process.env,
   STAGE: 'test',
-  COOKIE_SECRET: 'secret',
-  ...JSON.parse(env)
+  COOKIE_SECRET: 'secret'
 }
