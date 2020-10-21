PentoHQ/Pizzly

chore: report error

src/routes/api.ts
@@ -395,6 +395,9 @@ api.delete('/:integrationId/authentications/:authId', async (req, res, next) => 
             'Content-Type': 'application/x-www-form-urlencoded'
           }
         })
+        if (response.status > 299) {
+          next(new PizzlyError(`could not reboke token, got status: ${response.statusText}`))
+        }
       } catch (err) {
         next(new PizzlyError(err))
       }

NangoHQ/nango

src/routes/api.ts
@@ -395,6 +395,9 @@ api.delete('/:integrationId/authentications/:authId', async (req, res, next) => 
             'Content-Type': 'application/x-www-form-urlencoded'
           }
         })
+        if (response.status > 299) {
+          next(new PizzlyError(`could not revoke token, got status: ${response.statusText}`))
+        }
       } catch (err) {
         next(new PizzlyError(err))
       }