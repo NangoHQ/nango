44a33d3363b3d44fafe92fa963594126ea44f99b
Björn Heinrichs <manfie@protonmail.ch>
2020-10-28 23:31:53 +0100

    Allow generator to watch for files

--- integrations/gen.js
+++ integrations/gen.js
@@ -1,29 +1,38 @@
 const fs = require('fs')
 const path = require('path');
+const chokidar = require('chokidar');
 const root = __dirname;
 
-const stream = fs.createWriteStream(path.join(root, "index.ts"), {flags:'w'});
-
-function visit(dir, ext, pkgPrefix) {
-    if (pkgPrefix !== './') {
-        throw new "Error: No directories within integrations allowed."
-    }
-    const integrations = fs.readdirSync(dir)
-    integrations.forEach(name => {
-        const packageName = pkgPrefix + name
-        const file = path.join(dir, name)
-        const s = fs.lstatSync(file)
-        if (s.isDirectory()) {
-            visit(file, ext, packageName + '/')
-            return
+function generate(reason) {
+    function visit(dir, ext, pkgPrefix) {
+        if (pkgPrefix !== './') {
+            throw new "Error: No directories within integrations allowed."
         }
-        if (name.endsWith(ext)) {
-            const imp = path.relative(root, file)
-            stream.write("import '" + packageName + "'\n")
-        }
-    });
-};
+        const integrations = fs.readdirSync(dir)
+        integrations.forEach(name => {
+            const packageName = pkgPrefix + name
+            const file = path.join(dir, name)
+            const s = fs.lstatSync(file)
+            if (s.isDirectory()) {
+                visit(file, ext, packageName + '/')
+                return
+            }
+            if (name.endsWith(ext)) {
+                const imp = path.relative(root, file)
+                stream.write("import '" + packageName + "'\n")
+            }
+        });
+    };
 
-visit(root, 'json', './')
+    console.log('Generation started: ', reason)
+    const stream = fs.createWriteStream(path.join(root, "index.ts"), {flags:'w'});
+    visit(root, 'json', './')
+    stream.end();
+    console.log('Generation finished')
+}
 
-stream.end();
+generate('Initial generation');
+
+chokidar.watch(root, {ignoreInitial: true, ignored: /.[j|t]s$/})
+    .on('add', path => generate('New file added: ' + path))
+    .on('unlink', path => generate('File removed: ' + path));
