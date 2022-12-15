e1a4a3cb05faf0177002e9d32810353dd1e4530f
Bastien Beurier <bastienbeurier@gmail.com>
2022-12-15 13:19:03 +0100

    Small fixes

--- packages/auth/lib/db/config.ts
+++ packages/auth/lib/db/config.ts
@@ -5,11 +5,11 @@ let config: { development: Knex.Config<any>; production: Knex.Config<any> } = {
         client: 'pg',
         connection: {
             host: process.env['NANGO_DB_HOST'] || (process.env['SERVER_RUN_MODE'] === 'DOCKERIZED' ? 'nango-db' : 'localhost'),
-            port: +(process.env['NANGO_DB_PORT'] ||  5432),
+            port: +(process.env['NANGO_DB_PORT'] || 5432),
             user: process.env['NANGO_DB_USER'] || 'nango',
             database: process.env['NANGO_DB_NAME'] || 'nango',
             password: process.env['NANGO_DB_PASSWORD'] || 'nango',
-            ssl: process.env['NANGO_DB_SSL'] ? { rejectUnauthorized: false } : undefined
+            ssl: process.env['NANGO_DB_SSL'] != null && process.env['NANGO_DB_SSL'].toLowerCase() === 'true' ? { rejectUnauthorized: false } : undefined
         },
         migrations: {
             directory: './migrations',
