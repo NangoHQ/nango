6a853b66bb9a47f21417ec7a55685332ae467780
Bastien Beurier <bastienbeurier@gmail.com>
2023-01-28 00:34:51 +0100

    Node SDK bug fix

--- packages/node-client/lib/index.ts
+++ packages/node-client/lib/index.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import https from 'https';
 
 export class Nango {
     serverUrl: string;
@@ -74,7 +73,7 @@ export class Nango {
 
     private enrichHeaders(headers: Record<string, string | number | boolean> = {}) {
         if (this.secretKey) {
-            headers['Authorization'] = 'Basic ' + Buffer.from(process.env['NANGO_SECRET_KEY'] + ':').toString('base64');
+            headers['Authorization'] = 'Basic ' + Buffer.from(this.secretKey + ':').toString('base64');
         }
 
         return headers;
