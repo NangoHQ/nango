f26a6f2928f4cdc4fec02197b83f7485cd21fc2f
Bastien Beurier <bastienbeurier@gmail.com>
2023-02-23 17:19:44 +0100

    Use Tailwind to replace css

--- webapp/pages/_app.tsx
+++ webapp/pages/_app.tsx
@@ -1,12 +1,6 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { GeistProvider, CssBaseline } from '@geist-ui/core';
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <GeistProvider>
-            <CssBaseline />
-            <Component {...pageProps} />;
-        </GeistProvider>
-    );
+    return <Component {...pageProps} />;
 }
--- webapp/pages/_document.tsx
+++ webapp/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en" className="h-full bg-gray-50">
+            <Head />
+            <body className="h-full">
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
