e3bb6b32a84bd16ca4c44ed68a47c0c95e71665f
Bastien Beurier <bastienbeurier@gmail.com>
2023-02-28 21:14:50 +0100

    Add tailwind css

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a className="App-link ml-10" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ webapp/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        fontFamily: {
+            sans: ['Inter', 'sans-serif']
+        },
+        extend: {
+            colors: {
+                'bg-black': '#0E1014',
+                'bg-dark-gray': '#181B20',
+                'text-light-gray': '#A9A9A9',
+                'bg-cta-green': '#75E270',
+                'text-cta-green1': '#224421',
+                'border-gray': '#474747',
+                'border-blue': '#1489DF',
+                'text-blue': '#1489DF',
+                'text-light-blue': '#76C5FF',
+                'bg-dark-blue': '#182633',
+                white: '#FFFFFF'
+            }
+        }
+    },
+    plugins: [require('@tailwindcss/forms')]
+};
