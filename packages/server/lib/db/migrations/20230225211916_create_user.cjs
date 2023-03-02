5e6cb403f5cf27c22918c0d838a0b2d32054a1d0
Bastien Beurier <bastienbeurier@gmail.com>
2023-03-02 15:21:10 +0100

    Update Dockerfile

--- packages/server/lib/db/migrations/20230225211916_create_user.cjs
+++ packages/server/lib/db/migrations/20230225211916_create_user.cjs
@@ -4,8 +4,8 @@ exports.up = async function (knex, _) {
         table.timestamps(true, true);
         table.string('name').notNullable();
         table.string('email').notNullable();
-        table.string('hashed_password').notNullable();
-        table.string('salt').notNullable();
+        table.string('hashed_password').notNullable().defaultTo(knex.raw('uuid_generate_v4()'));
+        table.string('salt').notNullable().defaultTo(knex.raw('uuid_generate_v4()'));
         table.integer('account_id').references('id').inTable('nango._nango_accounts').notNullable();
         table.unique('email');
     });
