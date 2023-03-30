cc058c75e0f9bae4e5db0065b15a5a91f3f08cec
Oluwatobi Adenekan <oluwatobi.adenekan@taxfix.de>
2023-03-30 22:00:58 +0200

    feat: added oAuth0 schema.

--- packages/server/lib/db/migrations/20230326083713_create_cache.cjs
+++ packages/server/lib/db/migrations/20230326083713_create_cache.cjs
@@ -1,13 +1,23 @@
-exports.up = function (knex, _) {
-    return knex.schema.withSchema('nango').createTable('_nango_cache', function (table) {
-        table.string('key').notNullable();
-        table.json('value').notNullable();
+exports.up = function(knex, _) {
+    return knex.schema.withSchema('nango').createTable('_nango_oauth_sessions', function(table) {
+        table.uuid('id').notNullable();
+        table.string('providerConfigKey').notNullable();
+        table.string('provider').notNullable();
+        table.string('connectionId').notNullable();
+        table.string('callbackUrl').notNullable();
+        table.string('authMode').notNullable();
+        table.string('codeVerifier');
+        table.json('connectionConfig');
+        table.string('accountId');
+        table.string('webSocketClientId');
+        table.string('codeVerifier');
+        table.string('request_token_secret');
         table.timestamps(true, true);
 
-        table.unique('key')
+        table.unique('id');
     });
 };
 
-exports.down = function (knex, _) {
-    return knex.schema.withSchema('nango').dropTable('_nango_cache');
+exports.down = function(knex, _) {
+    return knex.schema.withSchema('nango').dropTable('_nango_oauth_sessions');
 };
