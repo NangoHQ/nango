62abf8e7a1b74769a5bcf4b3de0ea6741188a5b3
Chakravarthy7102 <medicharlachakravarthi@gmail.com>
2023-04-20 19:52:42 +0530

    added custom error fallback component

--- packages/webapp/src/components/ErrorBoundary.tsx
+++ packages/webapp/src/components/ErrorBoundary.tsx
@@ -30,11 +30,21 @@ class ErrorBoundary extends Component<Props, State> {
             }
 
             return (
-                <div>
-                    <h2>Oops, there is an error!</h2>
-                    <button type="button" onClick={() => this.setState({ hasError: false })}>
-                        Try again?
-                    </button>
+                <div className="flex h-screen text-white justify-center items-center">
+                    <div className="mx-auto">
+                        <img className="h-8" src="/logo-circled.svg" alt="Nango" />
+                        <h2 className="text-emphasis mt-6 text-2xl font-medium">It&apos;s not you, it&apos;s us.</h2>
+                        <p className="text-default mt-4 mb-6 max-w-2xl text-sm">
+                            Something went wrong on our end. Get in touch with our support team, and we’ll get it fixed right away for you.
+                        </p>
+
+                        <button
+                            onClick={() => this.setState({ hasError: false })}
+                            className="mt-auto mb-4 px-4 h-10 rounded-md text-sm text-black bg-white hover:bg-gray-300"
+                        >
+                            Try again?
+                        </button>
+                    </div>
                 </div>
             );
         }
