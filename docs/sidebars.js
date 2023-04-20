b186e124f7368f6043e3b8c151f5079841acd293
David Rousselie <david@rousselie.name>
2023-04-20 14:35:27 +0200

    Add Todoist API provider (#476)

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -233,6 +233,7 @@ const sidebars = {
                 'providers/strava',
                 'providers/stripe',
                 'providers/teamwork',
+                'providers/todoist',
                 'providers/trello',
                 'providers/twitch',
                 'providers/twitter',
