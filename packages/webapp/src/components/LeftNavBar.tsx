5da7a315c05368eeeb2c74bf85b0de01c99e03ea
Khaliq <khaliqgant@gmail.com>
2023-05-01 11:01:03 +0300

    [gh-#610] add icon and start basic layout

--- packages/webapp/src/components/LeftNavBar.tsx
+++ packages/webapp/src/components/LeftNavBar.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import Activity from '@geist-ui/icons/activity'
+import Activity from '@geist-ui/icons/activity';
 
 export enum LeftNavBarItems {
     Integrations = 0,
--- packages/webapp/src/pages/Activity.tsx
+++ packages/webapp/src/pages/Activity.tsx
@@ -1,3 +1,5 @@
+import { CheckInCircle, AlertCircle, Link as LinkIcon } from '@geist-ui/icons'
+
 import DashboardLayout from '../layout/DashboardLayout';
 import { LeftNavBarItems } from '../components/LeftNavBar';
 
@@ -10,7 +12,22 @@ export default function Activity() {
                     <table className="table-auto">
                         <tbody className="px-4">
                                 <tr key="1">
+                                    <td className={`mx-8 flex items-center place-content-center h-16`}>
+                                        <CheckInCircle className="stroke-green-500" />
+                                        <div className="bg-pink-300">
+                                            <LinkIcon className="stroke-pink-500" />
+                                            auth
+                                        </div>
+                                        <div className="w-largecell text-t font-mono">1</div>
+                                        <div className="w-80 flex pl-8">
+                                            <img src={`images/template-logos/github.svg`} alt="" className="h-7 mt-0.5 mr-0.5" />
+                                            <p className="mt-1.5 mr-4 ml-0.5">Foo</p>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr key="1">
                                     <td className={`mx-8 flex place-content-center h-16`}>
+                                        <AlertCircle className="stroke-red-500" />
                                         <div className="mt-5 w-largecell text-t font-mono">1</div>
                                         <div className="mt-4 w-80 flex pl-8">
                                             <img src={`images/template-logos/github.svg`} alt="" className="h-7 mt-0.5 mr-0.5" />
