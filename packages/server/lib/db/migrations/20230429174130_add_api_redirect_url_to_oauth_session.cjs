8506d132a339a7962ed71e18105b80fbb5320c0f
Oluwatobi Adenekan <oluwatobi.t.adenekan@gmail.com>
2023-05-08 09:54:52 +0200

    fix: api-oauth-flow.

--- packages/server/lib/db/migrations/20230429174130_add_api_redirect_url_to_oauth_session.cjs
+++ packages/server/lib/db/migrations/20230429174130_add_api_redirect_url_to_oauth_session.cjs
@@ -6,6 +6,6 @@ exports.up = async function (knex, _) {
 
 exports.down = function (knex, _) {
     return knex.schema.withSchema('nango').alterTable('_nango_oauth_sessions', function (table) {
-        table.text('api_redirect_url');
+        table.dropColumn('api_redirect_url');
     });
 };
