9ad2a2b161718fea5b81f4e6303edeea96820aa2
tilda <iamtheworst@programmer.net>
2023-05-10 05:52:05 -0500

    Add osu! API provider (#619)
    * Add osu! API provider
    
    * add osu! entry to sidebars
    
    ---------
    
    Co-authored-by: Bastien Beurier <bastienbeurier@gmail.com>

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -175,6 +175,7 @@ const sidebars = {
                 'providers/mural',
                 'providers/notion',
                 'providers/one-drive',
+                'providers/osu',
                 'providers/outreach',
                 'providers/pagerduty',
                 'providers/pandadoc',
