560f73f10cb1ee221e6babddfd4225557181259c
Khaliq <khaliqgant@gmail.com>
2023-05-12 11:46:02 +0300

    [gh-#627] update refactor logic

--- packages/shared/lib/index.ts
+++ packages/shared/lib/index.ts
@@ -3,7 +3,6 @@ import db from './database.js';
 import configService from './services/config.service.js';
 import encryptionManager from './utils/encryption.manager.js';
 
-export * from './services/sync.service.js';
 export * from './services/connection.service.js';
 export * from './services/activity.service.js';
 export * from './models/index.js';
