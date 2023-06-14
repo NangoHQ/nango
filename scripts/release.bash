cbe8487a08ed4c574dcb8ec8724d8d07f096ad58
Khaliq <khaliqgant@gmail.com>
2023-06-14 14:24:07 +0300

    [gh-#637] clean up script further

--- scripts/release.bash
+++ scripts/release.bash
@@ -13,6 +13,13 @@ function update_shared_dep() {
     jq --arg NEW_VERSION "$NEW_VERSION" '.dependencies["@nangohq/shared"] = $NEW_VERSION' --indent 4 $PACKAGE_JSON | sponge $PACKAGE_JSON
 }
 
+function update_frontend_dep() {
+    PACKAGE_JSON=$1
+    NEW_VERSION=$2
+
+    jq --arg NEW_VERSION "$NEW_VERSION" '.dependencies["@nangohq/frontend"] = $NEW_VERSION' --indent 4 $PACKAGE_JSON | sponge $PACKAGE_JSON
+}
+
 function update_package_json_version() {
     PACKAGE_JSON=$1
     SPECIFIC_VERSION=$2
@@ -56,6 +63,16 @@ if git diff --quiet -- ./packages/shared || git diff --cached --quiet -- ./packa
     cd ./packages/node-client && npm publish --access public && cd ../../
 fi
 
+# update the webapp and frontend
+FRONTEND_PACKAGE_JSON="packages/frontend/package.json"
+update_package_json_version $FRONTEND_PACKAGE_JSON $4
+cd ./packages/frontend && npm publish --access public && cd ../../
+
+update_frontend_dep "packages/webapp/package.json" $(jq -r '.version' $FRONTEND_PACKAGE_JSON)
+WEBAPP_PACKAGE_JSON="packages/webapp/package.json"
+npm i
+update_package_json_version $WEBAPP_PACKAGE_JSON $4
+
 SERVER_VERSION=$1
 WORKER_VERSION=$2
 ENV=$3
@@ -66,6 +83,7 @@ npm run ts-build
 cd ./packages/webapp && npm run build && cd ../../
 
 ./scripts/docker-publish.bash nango-server $SERVER_VERSION true $3 &
+./scripts/docker-publish.bash nango-server $SERVER_VERSION true $3 hosted &
 ./scripts/docker-publish.bash nango-worker $WORKER_VERSION true hosted &
 
 SERVER_IMAGE="nangohq/nango-server"
