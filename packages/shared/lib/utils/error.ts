b82d9a06735757987fe1db4fc17dd66eb430e1f2
Khaliq <khaliqgant@gmail.com>
2023-06-16 20:43:46 +0300

    [gh-#672] use payload in the error response

--- packages/shared/lib/utils/error.ts
+++ packages/shared/lib/utils/error.ts
@@ -119,6 +119,9 @@ export class NangoError extends Error {
             case 'unknown_provider_config':
                 this.status = 400;
                 this.message = `There is no Provider Configuration matching this key.`;
+                if (this.payload) {
+                    this.message += ` Please make sure this value exists in the Nango dashboard ${JSON.stringify(this.payload, null, 2)}}`;
+                }
                 break;
 
             case 'missing_provider_template':
