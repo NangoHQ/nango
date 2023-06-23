d6958543733696e4d6bbcb116eabdbd47cea7406
Khaliq <khaliqgant@gmail.com>
2023-06-23 13:08:02 +0300

    [gh-#684] update packages and release script

--- scripts/release.bash
+++ scripts/release.bash
@@ -50,6 +50,27 @@ function update_package_json_version() {
     echo "$PACKAGE_JSON version bumped successfully!"
 }
 
+#
+# Publish packages flow (NPM)
+# 1) Node client is the slimmest package and has no other dependencies. Bump its
+# version, install, build and then publish it
+# 2) Shared depends on the node-client so update the shared dependency on node-client.
+# npm install to update that dependency.
+# 3) Bump the shared package version, install build and publish it
+# 4) server, worker, and cli all depend on the shared package. Update their shared
+# dependency.
+# 5) The frontend has no dependencies so bump that version and publish it
+# 6) The webapp depends on the frontend so update that dependency, install, and build it.
+#
+# Docker publish flow
+# 1) Build the nango-server cloud version (staging or prod) and push it to dockerhub
+# 2) Build the nango-server hosted version and push it to dockerhub
+# 3) Build the nango-worker hosted version and push it to dockerhub
+# 4) Update the docker-compose.yaml in the cli to use the new server and worker versions
+# 5) Bump the cli package.json, copy the NangoSync type declaration since that is used
+# for integration scripts. Publish the package and then npm install at the root
+# to ensure all dependencies are updated
+
 # node client is the first to be published since shared depends on it
 NODE_CLIENT_PACKAGE_JSON="packages/node-client/package.json"
 update_package_json_version $NODE_CLIENT_PACKAGE_JSON $3
@@ -59,6 +80,8 @@ cd ./packages/node-client && npm publish --access public && cd ../../
 
 # update the shared package that depends on the node client
 update_node_dep "packages/shared/package.json" $(jq -r '.version' $NODE_CLIENT_PACKAGE_JSON)
+
+npm i
 
 # Update the shared package and then bump the cli, server and worker versions that depend on it
 SHARED_PACKAGE_JSON="packages/shared/package.json"
@@ -80,21 +103,22 @@ cd ./packages/frontend && npm publish --access public && cd ../../
 update_frontend_dep "packages/webapp/package.json" $(jq -r '.version' $FRONTEND_PACKAGE_JSON)
 
 WEBAPP_PACKAGE_JSON="packages/webapp/package.json"
-npm i
 update_package_json_version $WEBAPP_PACKAGE_JSON $3
-
-SERVER_WORKER_VERSION=$1
-ENV=$2
-DOCKER_COMPOSE_FILE="packages/cli/docker/docker-compose.yaml"
 
 npm i
 npm run ts-build
 cd ./packages/webapp && npm run build && cd ../../
 
+SERVER_WORKER_VERSION=$1
+ENV=$2
+DOCKER_COMPOSE_FILE="packages/cli/docker/docker-compose.yaml"
+
 WORKER_PACKAGE_JSON="packages/worker/package.json"
 SERVER_PACKAGE_JSON="packages/server/package.json"
 update_package_json_version $SERVER_PACKAGE_JSON $3
 update_package_json_version $WORKER_PACKAGE_JSON $3
+
+# End npm package publish flow (Minus the CLI which is published last since that depends on docker versions)
 
 rm -rf ./packages/webapp/build/fonts
 ./scripts/docker-publish.bash nango-server $SERVER_WORKER_VERSION true $2
@@ -119,4 +143,5 @@ update_package_json_version $CLI_PACKAGE_JSON $3
 cp ./packages/shared/dist/lib/sdk/sync.d.ts ./packages/cli/dist/nango-sync.d.ts
 
 cd ./packages/cli && npm publish --access public && cd ../../
+
 npm i
