bc6fbe17a0bc375e473db04169a9be6372cc515a
Khaliq <khaliqgant@gmail.com>
2023-07-01 19:37:43 +0300

    [gh-#723] start

--- packages/cli/lib/index.ts
+++ packages/cli/lib/index.ts
@@ -21,6 +21,7 @@ class NangoCommand extends Command {
         cmd.option('--secret-key [secretKey]', 'Set the secret key. Overrides the `NANGO_SECRET_KEY` value set in the .env file');
         cmd.option('--host [host]', 'Set the host. Overrides the `NANGO_HOSTPORT` value set in the .env file');
         cmd.option('--auto-confirm', 'Auto confirm yes to all prompts.');
+        cmd.option('--debug', 'Run cli in debug mode, outputting verbose logs.');
         cmd.hook('preAction', async () => {
             await upgradeAction();
         });
