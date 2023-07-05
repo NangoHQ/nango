d3b9ae3559259e55b2a39841f826937118ca708d
Khaliq <khaliqgant@gmail.com>
2023-07-05 15:19:56 +0300

    [gh-#721] install globally and not via npx

--- packages/cli/lib/utils.ts
+++ packages/cli/lib/utils.ts
@@ -115,6 +115,13 @@ export async function verifyNecessaryFiles(autoConfirm: boolean, debug = false) 
 }
 
 export async function upgradeAction(debug = false) {
+    if (!isLocallyInstalled('nango') && process.env['npm_config_user_agent']?.includes('npx')) {
+        console.log(
+            chalk.red(`It appears you are running nango via npx. We recommend installing nango globally ("npm install nango -g") and running it directly.`)
+        );
+        process.exit(1);
+    }
+
     if (process.env['NANGO_NO_PROMPT_FOR_UPGRADE'] === 'true') {
         return;
     }
