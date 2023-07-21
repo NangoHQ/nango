ec169672d4926565dc1bbda4caed63cead02a4b0
Khaliq <khaliqgant@gmail.com>
2023-07-21 13:02:06 +0300

    [gh-#322] type declarations

--- packages/node-client/lib/index.ts
+++ packages/node-client/lib/index.ts
@@ -1,6 +1,15 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
-import { AuthModes, OAuth1Credentials, OAuth2Credentials, ProxyConfiguration, GetRecordsRequestConfig } from './types.js';
+import {
+    AuthModes,
+    OAuth1Credentials,
+    OAuth2Credentials,
+    ProxyConfiguration,
+    GetRecordsRequestConfig,
+    BasicApiCredentials,
+    ApiKeyCredentials,
+    Connection
+} from './types.js';
 import { validateProxyConfiguration, validateSyncRecordConfiguration } from './utils.js';
 
 export const stagingHost = 'https://api-staging.nango.dev';
@@ -20,6 +29,11 @@ interface CreateConnectionOAuth1 extends OAuth1Credentials {
     connection_id: string;
     provider_config_key: string;
     type: AuthModes.OAuth1;
+}
+
+interface OAuth1Token {
+    oAuthToken: string;
+    oAuthTokenSecret: string;
 }
 
 interface CreateConnectionOAuth2 extends OAuth2Credentials {
@@ -107,7 +121,11 @@ export class Nango {
      * @param [forceRefresh] - When set, this is used to  obtain a new refresh token from the provider before the current token has expired,
      * you can set the forceRefresh argument to true.
      * */
-    public async getToken(providerConfigKey: string, connectionId: string, forceRefresh?: boolean) {
+    public async getToken(
+        providerConfigKey: string,
+        connectionId: string,
+        forceRefresh?: boolean
+    ): Promise<string | OAuth1Token | BasicApiCredentials | ApiKeyCredentials> {
         const response = await this.getConnectionDetails(providerConfigKey, connectionId, forceRefresh);
 
         switch (response.data.credentials.type) {
@@ -142,7 +160,7 @@ export class Nango {
      * you can set the forceRefresh argument to true.
      * @param [refreshToken] - When set this returns the refresh token as part of the response
      */
-    public async getConnection(providerConfigKey: string, connectionId: string, forceRefresh?: boolean, refreshToken?: boolean) {
+    public async getConnection(providerConfigKey: string, connectionId: string, forceRefresh?: boolean, refreshToken?: boolean): Promise<Connection> {
         const response = await this.getConnectionDetails(providerConfigKey, connectionId, forceRefresh, refreshToken);
         return response.data;
     }
