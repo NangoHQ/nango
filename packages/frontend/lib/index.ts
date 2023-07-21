74b98d446cda15fa2e95af819ec6e2b80161b13f
Khaliq <khaliqgant@gmail.com>
2023-07-21 22:21:27 +0300

    [gh-#322] scope and creds fix

--- packages/frontend/lib/index.ts
+++ packages/frontend/lib/index.ts
@@ -52,7 +52,7 @@ export default class Nango {
         connectionId: string,
         conectionConfigOrCredentials?: ConnectionConfig | BasicApiCredentials | ApiKeyCredentials
     ): Promise<any> {
-        if (conectionConfigOrCredentials && 'credentials' in conectionConfigOrCredentials && Object.keys(conectionConfigOrCredentials.credentials).length > 1) {
+        if (conectionConfigOrCredentials && 'credentials' in conectionConfigOrCredentials && Object.keys(conectionConfigOrCredentials.credentials).length > 0) {
             const credentials = conectionConfigOrCredentials.credentials as BasicApiCredentials | ApiKeyCredentials;
             const { credentials: _, ...connectionConfig } = conectionConfigOrCredentials as ConnectionConfig;
             return this.apiAuth(providerConfigKey, connectionId, this.convertCredentialsToConfig(credentials), connectionConfig);
