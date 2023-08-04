32f3c729f2a062abe9b3172cb1b661f30a062867
Khaliq <khaliqgant@gmail.com>
2023-08-04 12:47:38 +0200

    [gh-#874] improve example

--- packages/node-client/bin/metadata.js
+++ packages/node-client/bin/metadata.js
@@ -3,9 +3,11 @@ const args = process.argv.slice(2);
 const nango = new Nango({ secret: args[0], host: 'http://localhost:3003' });
 
 const metadata = {
-    slack_channel_id: 'Slack_ID__c',
-    primary_support_rep: 'Primary_Support_Rep__c',
-    secondary_support_rep: 'Secondary_Support_Rep__c'
+    fieldMapping: {
+        slack_channel_id: 'Slack_ID__c',
+        primary_support_rep: 'Primary_Support_Rep__c',
+        secondary_support_rep: 'Secondary_Support_Rep__c'
+    }
 };
 
 nango.setMatadata(metadata, args[1], args[2]).then((response) => {
