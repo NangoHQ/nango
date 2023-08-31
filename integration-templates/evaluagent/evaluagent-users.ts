04ce511ba091e80cbe58bf1eb927ae8c914ae873
Alex Richards <ajtrichards@gmail.com>
2023-08-31 15:08:36 +0100

    Integration - EvaluAgent API (#947)
    * EvaluAgent integration
    
    * Added syncs for EvaluAgent Users, Groups and Roles
    
    * Removed typo from Region
    
    * Added message about having an active EA Subscription
    
    * Fixed error where i'd left eventbrite after copy n paste

---
+++ integration-templates/evaluagent/evaluagent-users.ts
@@ -0,0 +1,29 @@
+import type { NangoSync, EvaluAgentUser } from './models';
+
+export default async function fetchData(nango: NangoSync): Promise<{EvaluAgentUser: EvaluAgentUser[]}> {
+    let payload = {
+        endpoint: '/v1/org/users'
+    }
+
+    const response = await nango.get(payload);
+
+    let returnedData = response.data.data;
+
+    const mappedUsers: EvaluAgentUser[] = returnedData.map((user: EvaluAgentUser) => ({
+        id: user.id,
+        forename: user.attributes.forename,
+        surname: user.attributes.surname,
+        email: user.attributes.email,
+        username: user.attributes.username,
+        startDate: user.attributes.start_date,
+        active: user.attributes.active,
+        thirdPartyId: user.attributes.third_party_id,
+    }));
+
+    if (mappedUsers.length > 0) {
+        await nango.batchSave<EvaluAgentUser>(mappedUsers, 'EvaluAgentUser');
+        await nango.log(`Sent ${mappedUsers.length} users`);
+    }
+
+    return {EvaluAgentUser: [] };
+}
