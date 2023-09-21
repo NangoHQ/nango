1a88dbf4371132cc05e88bb25997fb7796d54702
Khaliq <khaliqgant@gmail.com>
2023-09-21 11:44:40 +0300

    remove creds

--- packages/server/lib/utils/utils.unit.test.ts
+++ packages/server/lib/utils/utils.unit.test.ts
@@ -41,24 +41,23 @@ describe('Utils unit tests', () => {
 
         const params = {
             ok: true,
-            app_id: 'A03MTRVRNHM',
             scope: 'chat:write,channels:read,team.billing:read,users:read,channels:history,channels:join,incoming-webhook',
             token_type: 'bot',
-            bot_user_id: 'U03NAA9Q77D',
+            bot_user_id: 'abcd',
             enterprise: null,
             is_enterprise_install: false,
             incoming_webhook: {
-                channel_id: 'D055NBEBGHH',
-                configuration_url: 'https://nangohq.slack.com/services/B05TVQV8ARE',
-                url: 'https://hooks.slack.com/services/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'
+                channel_id: 'foo',
+                configuration_url: 'https://nangohq.slack.com',
+                url: 'https://hooks.slack.com'
             }
         };
 
         const result = getConnectionMetadataFromTokenResponse(params, template);
         expect(result).toEqual({
-            'incoming_webhook.url': 'https://hooks.slack.com/services/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',
+            'incoming_webhook.url': 'https://hooks.slack.com',
             ok: true,
-            bot_user_id: 'U03NAA9Q77D',
+            bot_user_id: 'abcd',
             scope: 'chat:write,channels:read,team.billing:read,users:read,channels:history,channels:join,incoming-webhook'
         });
     });
@@ -69,17 +68,12 @@ describe('Utils unit tests', () => {
         } as ProviderTemplate;
 
         const params = {
-            app_id: 'A03MTRVRNHM',
-            authed_user: { id: 'U056C66B8L8' },
             scope: 'chat:write,channels:read,team.billing:read,users:read,channels:history,channels:join,incoming-webhook',
             token_type: 'bot',
-            access_token: 'xoxb-2710526930471-3758349823251-vY07FoRfczmorznPfEiqt88x',
-            bot_user_id: 'U03NAA9Q77D',
-            team: { id: 'T02LWFGTCDV', name: 'Nango' },
             enterprise: null,
             is_enterprise_install: false,
             incoming_webhook: {
-                configuration_url: 'https://nangohq.slack.com/services/B05TVQV8ARE'
+                configuration_url: 'foo.bar'
             }
         };
 
