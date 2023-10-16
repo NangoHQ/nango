7dfbb2f1604adccbe1ecc9c939d4405edc1a2987
Khaliq <khaliqgant@gmail.com>
2023-10-16 11:53:07 +0200

    [gh-#1109] add syncs to the get integration object

--- packages/shared/lib/models/Provider.ts
+++ packages/shared/lib/models/Provider.ts
@@ -1,5 +1,6 @@
 import type { AuthModes } from './Auth.js';
 import type { TimestampsAndDeleted } from './Generic.js';
+import type { Sync } from './Sync.js';
 
 export interface Config extends TimestampsAndDeleted {
     id?: number;
@@ -55,9 +56,11 @@ export interface IntegrationWithCreds extends Integration {
     scopes: string;
     auth_mode: AuthModes;
     app_link?: string;
+    syncs: Sync[];
 }
 
 export interface Integration {
     unique_key: string;
     provider: string;
+    syncs: Sync[];
 }
