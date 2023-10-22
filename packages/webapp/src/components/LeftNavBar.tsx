fa88514d9a94ab44da29630c19036d6071fb602d
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2023-10-22 02:02:54 -0400

    refactor(LeftNavBar): remove additional space between elements

--- packages/webapp/src/components/LeftNavBar.tsx
+++ packages/webapp/src/components/LeftNavBar.tsx
@@ -162,7 +162,6 @@ export default function LeftNavBar(props: LeftNavBarProps) {
                         </div>
                     )}
                 </div>
-
             </div>
         </div>
     );
