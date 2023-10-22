c9234b352e41bcd1a35b728989c8af965d319047
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2023-10-22 01:51:01 -0400

    refactor(webapp): improve sidebar ui design

--- packages/webapp/src/components/LeftNavBar.tsx
+++ packages/webapp/src/components/LeftNavBar.tsx
@@ -57,14 +57,14 @@ export default function LeftNavBar(props: LeftNavBarProps) {
 
     return (
         <div>
-            <div className="h-[calc(100vh-1rem)] mt-14 border-r-2 border-t-2 border-border-gray flex flex-col h-full w-60 fixed bg-bg-black z-50 justify-between">
-                <div className="">
+            <div className="h-full pt-14 border-r-2 border-t-2 border-border-gray flex flex-col w-60 fixed bg-bg-black z-49 justify-between">
+                <div className="mt-8 px-6">
                     {envs.length > 0 && (
-                        <div className="mt-10">
+                        <div className="mb-8">
                             <select
                                 id="environment"
                                 name="env"
-                                className="ml-8 border-border-gray bg-bg-black text-text-light-gray block h-11 w-24 appearance-none rounded-md border px-3 py-2 text-base shadow-sm active:outline-none focus:outline-none active:border-white focus:border-white"
+                                className="border-border-gray bg-bg-black text-text-light-gray block w-full appearance-none rounded-md border px-3 py-2 text-base shadow-sm active:outline-none focus:outline-none active:border-white focus:border-white"
                                 onChange={handleEnvChange}
                                 value={env}
                             >
@@ -76,73 +76,93 @@ export default function LeftNavBar(props: LeftNavBarProps) {
                             </select>
                         </div>
                     )}
-                    <div className="ml-4 mt-8 space-y-1">
+                    <div className="space-y-1">
                         <Link
                             to="/integrations"
-                            className={`flex h-10 rounded-md ml-4 pl-2 pr-3 pt-2.5 text-sm text-white mt-3 w-44 ${
+                            className={`flex h-10 p-2 gap-x-3 items-center rounded-md text-sm text-white ${
                                 props.selectedItem === LeftNavBarItems.Integrations ? 'bg-gray-800' : 'hover:bg-gray-700'
                             }`}
                         >
-                            <img className="h-5 mr-3" src="/images/integrations-icon.svg" alt="" />
+                            <img className="h-5" src="/images/integrations-icon.svg" alt="" />
                             <p>Integrations</p>
                         </Link>
                         <Link
                             to="/connections"
-                            className={`flex h-10 rounded-md ml-4 pl-2 pr-3 pt-2.5 text-sm text-white w-44 ${
+                            className={`flex h-10 p-2 gap-x-3 items-center rounded-md text-sm text-white ${
                                 props.selectedItem === LeftNavBarItems.Connections ? 'bg-gray-800' : 'hover:bg-gray-700'
                             }`}
                         >
-                            <img className="h-5 mr-3" src="/images/connections-icon.svg" alt="" />
+                            <img className="h-5" src="/images/connections-icon.svg" alt="" />
                             <p>Connections</p>
                         </Link>
                         <Link
                             to="/syncs"
-                            className={`flex h-10 rounded-md ml-4 pl-2 pr-3 pt-2.5 text-sm text-white w-44 ${
+                            className={`flex h-10 p-2 gap-x-3 items-center rounded-md text-sm text-white ${
                                 props.selectedItem === LeftNavBarItems.Syncs ? 'bg-gray-800' : 'hover:bg-gray-700'
                             }`}
                         >
-                            <img className="h-5 mr-3 stroke-white fill-white" src="/images/sync-code-icon.svg" alt="" />
+                            <img className="h-5 stroke-white fill-white" src="/images/sync-code-icon.svg" alt="" />
                             <p>Syncs & Actions</p>
                         </Link>
                         <Link
                             to="/activity"
-                            className={`flex h-10 rounded-md ml-4 pl-2 pr-3 pt-2.5 text-sm text-white w-44 ${
+                            className={`flex h-10 p-2 gap-x-3 items-center rounded-md text-sm text-white ${
                                 props.selectedItem === LeftNavBarItems.Activity ? 'bg-gray-800' : 'hover:bg-gray-700'
                             }`}
                         >
-                            <Activity className="h-5 mr-3" />
+                            <Activity className="h-5" />
                             <p>Activity</p>
                         </Link>
                         <Link
                             to="/project-settings"
-                            className={`flex h-10 rounded-md ml-4 pl-2 pr-3 pt-2.5 text-sm text-white w-44 ${
+                            className={`flex h-10 p-2 gap-x-3 items-center rounded-md text-sm text-white ${
                                 props.selectedItem === LeftNavBarItems.ProjectSettings ? 'bg-gray-800' : 'hover:bg-gray-700'
                             }`}
                         >
-                            <img className="h-5 mr-3" src="/images/settings-icon.svg" alt="" />
+                            <img className="h-5" src="/images/settings-icon.svg" alt="" />
                             <p>Project Settings</p>
                         </Link>
-                        {version && (<span className="flex pt-4 border-t-2 border-border-gray items-center text-center text-gray-500 justify-center text-sm">v{version}</span>)}
                     </div>
                 </div>
-                {isCloud() && (
-                    <div className="w-60 border-t-2 border-border-gray">
-                        <ul className="pt-8 mb-24 px-6 text-white space-y-4 text-sm">
-                            <li>
-                                <Link to="/account-settings" className={`flex h-10 rounded-md ml-4 pl-2 pr-3 pt-2.5 text-sm text-white w-44 ${props.selectedItem === LeftNavBarItems.AccountSettings ? 'bg-gray-800' : 'hover:bg-gray-700'}`}>
-                                    <Briefcase className="h-5 mr-3" />
-                                    Account Settings
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/user-settings" className={`flex h-10 rounded-md ml-4 pl-2 pr-3 pt-2.5 text-sm text-white w-44 ${props.selectedItem === LeftNavBarItems.UserSettings ? 'bg-gray-800' : 'hover:bg-gray-700'}`}>
-                                    <User className="h-5 mr-3" />
-                                    User Settings
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                )}
+                <div className='px-6'>
+                    {isCloud() && (
+                        <div className="">
+                            <ul className="text-white space-y-1 text-sm">
+                                <li>
+                                    <Link
+                                        to="/account-settings"
+                                        className={`flex h-10 p-2 gap-x-3 items-center rounded-md text-sm text-white ${
+                                            props.selectedItem === LeftNavBarItems.AccountSettings ? 'bg-gray-800' : 'hover:bg-gray-700'
+                                        }`}
+                                    >
+                                        <Briefcase className="h-5" />
+                                        Account Settings
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        to="/user-settings"
+                                        className={`flex h-10 p-2 gap-x-3 items-center rounded-md text-sm text-white ${
+                                            props.selectedItem === LeftNavBarItems.UserSettings ? 'bg-gray-800' : 'hover:bg-gray-700'
+                                        }`}
+                                    >
+                                        <User className="h-5" />
+                                        User Settings
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    )}
+                    {version && (
+                        <div>
+                            <hr className="border-border-gray border my-1" />
+                            <span className="flex py-1 items-center text-center text-gray-500 justify-center text-sm">
+                                v{version}
+                            </span>
+                        </div>
+                    )}
+                </div>
+
             </div>
         </div>
     );
