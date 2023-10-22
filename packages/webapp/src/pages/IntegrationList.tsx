368cf1cd4ad4553e6eb1d7a058520ad3861a0d05
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2023-10-22 16:07:35 -0400

    fix(integrationlist): table columns should resize as needed

--- packages/webapp/src/pages/IntegrationList.tsx
+++ packages/webapp/src/pages/IntegrationList.tsx
@@ -44,15 +44,15 @@ export default function IntegrationList() {
     return (
         <DashboardLayout selectedItem={LeftNavBarItems.Integrations}>
             {integrations && !!integrations.length && (
-                <div className="w-fit">
+                <div className="">
                     <div className="flex justify-between">
                         <h2 className="text-left text-3xl font-semibold tracking-tight text-white mb-12">Integrations</h2>
                         <Link to="/integration/create" className="mt-auto mb-4 pt-2.5 px-4 h-10 rounded-md text-sm text-black bg-white hover:bg-gray-300">
                             Add New
                         </Link>
                     </div>
-                    <div className="h-fit w-fit border border-border-gray rounded-md text-white text-sm">
-                        <table className="table-auto">
+                    <div className="h-fit min-w-fit border border-border-gray rounded-md text-white text-sm">
+                        <table className="table-auto w-full">
                             <tbody className="px-4">
                                 {integrations.map(({ uniqueKey, provider, connectionCount, creationDate }) => (
                                     <tr key={`tr-${uniqueKey}`}>
@@ -61,16 +61,16 @@ export default function IntegrationList() {
                                                 uniqueKey !== integrations.at(-1)?.uniqueKey ? 'border-b border-border-gray' : ''
                                             } h-16`}
                                         >
-                                            <div className="mt-5 w-80">{uniqueKey}</div>
-                                            <div className="mt-4 w-80 flex pl-8">
+                                            <div className="mt-5 flex-1">{uniqueKey}</div>
+                                            <div className="mt-4 flex flex-1 pl-8">
                                                 <img src={`images/template-logos/${provider}.svg`} alt="" className="h-7 mt-0.5 mr-0.5" />
                                                 <p className="mt-1.5 mr-4 ml-0.5">{provider}</p>
                                             </div>
-                                            <div className="pl-8 mt-4 flex w-40">
+                                            <div className="pl-8 mt-4 flex shrink-0">
                                                 <img src="images/connections-icon.svg" alt="Connections" className="h-5 mt-1.5 mr-1.5" />
                                                 <p className="mt-1.5 mr-4">{connectionCount}</p>
                                             </div>
-                                            <div className="pl-8 flex pt-4">
+                                            <div className="pl-8 flex pt-4 shrink-0">
                                                 <p className="mt-1.5 mr-4 text-text-dark-gray">{new Date(creationDate).toLocaleDateString()}</p>
                                                 <Link
                                                     to={`/connections/create/${uniqueKey}`}
