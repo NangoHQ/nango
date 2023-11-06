b4c25870100157b30514331638b8d16b425ce841
Khaliq <khaliqgant@gmail.com>
2023-11-06 15:43:52 +0100

    [gh-#1200] no isolate

--- packages/shared/lib/services/sync/data/delete.service.integration.test.ts
+++ packages/shared/lib/services/sync/data/delete.service.integration.test.ts
@@ -1,5 +1,5 @@
 import { expect, describe, it, beforeAll } from 'vitest';
-//import { multipleMigrations } from '../../../db/database.js';
+import { multipleMigrations } from '../../../db/database.js';
 import type { DataResponse } from '../../../models/Data.js';
 import * as DataService from './data.service.js';
 import connectionService from '../../connection.service.js';
@@ -13,6 +13,7 @@ const environmentName = 'delete-service';
 
 describe('Data delete service integration tests', () => {
     beforeAll(async () => {
+        await multipleMigrations();
         await createConfigSeeds(environmentName);
     });
 
