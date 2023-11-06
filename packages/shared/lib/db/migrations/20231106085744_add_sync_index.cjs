f5471f7470649ab7a929de70d7d59a2c2f536956
Khaliq <khaliqgant@gmail.com>
2023-11-06 12:14:46 +0100

    [gh-#1228] drop the named index

--- packages/shared/lib/db/migrations/20231106085744_add_sync_index.cjs
+++ packages/shared/lib/db/migrations/20231106085744_add_sync_index.cjs
@@ -8,6 +8,6 @@ exports.up = function (knex, _) {
 
 exports.down = function (knex, _) {
     return knex.schema.withSchema('nango').table(tableName, function (table) {
-        table.dropIndex(['nango_connection_id', 'deleted']);
+        table.dropIndex('nango_syncs_nango_connection_id_deleted_index');
     });
 };
