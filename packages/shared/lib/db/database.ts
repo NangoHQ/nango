fd36e8087a5e3df392acfc53aede4032a78e9319
Khaliq <khaliqgant@gmail.com>
2023-11-07 12:33:27 +0100

    [gh-#1238] improve try catch

--- packages/shared/lib/db/database.ts
+++ packages/shared/lib/db/database.ts
@@ -31,26 +31,22 @@ export const dbNamespace = '_nango_';
 
 export const multipleMigrations = async (): Promise<void> => {
     try {
-        // Ensure the schema exists without raising an error if it already does.
         await db.knex.raw(`CREATE SCHEMA IF NOT EXISTS ${db.schema()}`);
-        console.log(`Checked for schema existence: ${db.schema()}`);
 
-        // Check for pending migrations.
         const [_, pendingMigrations] = await db.knex.migrate.list({
             directory: String(process.env['NANGO_DB_MIGRATION_FOLDER'])
         });
 
         if (pendingMigrations.length === 0) {
-            console.log('No pending migrations. All migrations are up to date.');
+            console.log('No pending migrations, skipping migration step.');
         } else {
-            console.log(`Pending migrations found: ${pendingMigrations.join(', ')}. Running migrations.`);
-            // Run only the pending migrations.
-            await db.knex.migrate.up({
+            console.log('Migrations pending, running migrations.');
+            await db.knex.migrate.latest({
                 directory: String(process.env['NANGO_DB_MIGRATION_FOLDER'])
             });
-            console.log('Migrations completed successfully.');
+            console.log('Migrations completed.');
         }
     } catch (error: any) {
-        console.error(`Error during migration process: ${error.message}`);
+        console.error(error?.message);
     }
 };
