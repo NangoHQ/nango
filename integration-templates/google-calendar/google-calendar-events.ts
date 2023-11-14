f109e782d52f15298e2967223aa076c5d7965b01
Khaliq <khaliqgant@gmail.com>
2023-11-14 15:15:13 +0100

    [gh-#1216] simplify

--- integration-templates/google-calendar/google-calendar-events.ts
+++ integration-templates/google-calendar/google-calendar-events.ts
@@ -6,23 +6,11 @@ export default async function fetchData(nango: NangoSync): Promise<void> {
     oneMonthAgo.setHours(0, 0, 0, 0);
     const timeMin = oneMonthAgo.toISOString();
 
-    let batch: GoogleCalendarEvent[] = [];
-    const batchSize = 100;
-    const maxResults = batchSize.toString();
+    const maxResults = '100';
 
     const endpoint = `calendar/v3/calendars/primary/events?timeMin=${encodeURIComponent(timeMin)}`;
 
     for await (const eventPage of nango.paginate<GoogleCalendarEvent>({ endpoint, params: { maxResults } })) {
-        for (const event of eventPage) {
-            batch.push(event);
-
-            if (batch.length === batchSize) {
-                await nango.batchSave<GoogleCalendarEvent>(batch, 'GoogleCalendarEvent');
-            }
-        }
-    }
-
-    if (batch.length > 0) {
-        await nango.batchSave<GoogleCalendarEvent>(batch, 'GoogleCalendarEvent');
+        await nango.batchSave<GoogleCalendarEvent>(eventPage, 'GoogleCalendarEvent');
     }
 }
--- integration-templates/google-calendar/google-calendars.ts
+++ integration-templates/google-calendar/google-calendars.ts
@@ -1,21 +1,9 @@
 import type { NangoSync, GoogleCalendar } from './models';
 
 export default async function fetchData(nango: NangoSync): Promise<void> {
-    let batch: GoogleCalendar[] = [];
-    const batchSize = 100;
-    const maxResults = batchSize.toString();
+    const maxResults = '100';
 
     for await (const eventPage of nango.paginate<GoogleCalendar>({ endpoint: 'calendar/v3/users/me/calendarList', params: { maxResults } })) {
-        for (const event of eventPage) {
-            batch.push(event);
-
-            if (batch.length === batchSize) {
-                await nango.batchSave<GoogleCalendar>(batch, 'GoogleCalendar');
-            }
-        }
-    }
-
-    if (batch.length > 0) {
-        await nango.batchSave<GoogleCalendar>(batch, 'GoogleCalendar');
+        await nango.batchSave<GoogleCalendar>(eventPage, 'GoogleCalendar');
     }
 }
