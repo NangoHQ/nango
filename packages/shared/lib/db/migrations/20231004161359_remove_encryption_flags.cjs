e600597351bb4cae58fca324deeb76fa27ce6299
Khaliq <khaliqgant@gmail.com>
2023-11-17 12:50:43 +0100

    don't drop json_iv and json_tag from records table

--- packages/shared/lib/db/migrations/20231004161359_remove_encryption_flags.cjs
+++ packages/shared/lib/db/migrations/20231004161359_remove_encryption_flags.cjs
@@ -2,10 +2,10 @@ const RECORDS_TABLE = '_nango_sync_data_records';
 const DELETED_RECORDS_TABLE = '_nango_sync_data_records_deletes';
 
 exports.up = async function (knex, _) {
-    await knex.schema.withSchema('nango').alterTable(RECORDS_TABLE, function (table) {
-        table.dropColumn('json_iv');
-        table.dropColumn('json_tag');
-    });
+    //await knex.schema.withSchema('nango').alterTable(RECORDS_TABLE, function (table) {
+        //table.dropColumn('json_iv');
+        //table.dropColumn('json_tag');
+    //});
     return knex.schema.withSchema('nango').alterTable(DELETED_RECORDS_TABLE, function (table) {
         table.dropColumn('json_iv');
         table.dropColumn('json_tag');
