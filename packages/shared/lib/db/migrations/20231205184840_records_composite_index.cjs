9d257ded20d1d90cd5d7a1c2192a53654e96df88
Khaliq <khaliqgant@gmail.com>
2023-12-05 22:47:51 +0100

    [nan-103] run migrations concurrently

--- packages/shared/lib/db/migrations/20231205184840_records_composite_index.cjs
+++ packages/shared/lib/db/migrations/20231205184840_records_composite_index.cjs
@@ -1,13 +1,11 @@
 const tableName = '_nango_sync_data_records';
 
-exports.up = function (knex, _) {
-    return knex.schema.withSchema('nango').alterTable(tableName, function (table) {
-        table.index(['nango_connection_id', 'model', 'created_at', 'id'], 'idx_nango_records_composite');
-    });
+exports.config = { transaction: false };
+
+exports.up = function(knex) {
+  return knex.schema.raw('CREATE INDEX CONCURRENTLY idx_nango_records_composite ON nango._nango_sync_data_records (nango_connection_id, model, created_at, id)');
 };
 
-exports.down = function (knex, _) {
-    return knex.schema.withSchema('nango').table(tableName, function (table) {
-        table.dropIndex(['nango_connection_id', 'model', 'created_at', 'id'], 'idx_nango_records_composite');
-    });
+exports.down = function(knex) {
+  return knex.schema.raw('DROP INDEX CONCURRENTLY idx_nango_records_composite');
 };
