9129432d2dc27816a638e6570a37f67625ce1dbe
Khaliq <khaliqgant@gmail.com>
2023-12-05 20:38:05 +0100

    [nan-103] update composite index

--- packages/shared/lib/db/migrations/20231205184840_records_composite_index.cjs
+++ packages/shared/lib/db/migrations/20231205184840_records_composite_index.cjs
@@ -2,12 +2,12 @@ const tableName = '_nango_sync_data_records';
 
 exports.up = function (knex, _) {
     return knex.schema.withSchema('nango').alterTable(tableName, function (table) {
-        table.index(['created_at', 'id']);
+        table.index(['nango_connection_id', 'model', 'created_at', 'id'], 'idx_nango_records_composite');
     });
 };
 
 exports.down = function (knex, _) {
     return knex.schema.withSchema('nango').table(tableName, function (table) {
-        table.dropIndex(['created_at', 'id']);
+        table.dropIndex(['nango_connection_id', 'model', 'created_at', 'id'], 'idx_nango_records_composite');
     });
 };
