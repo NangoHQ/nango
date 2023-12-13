17da3fa9495ea0948a4bdb6fd721b21d5cc6778a
Thomas Bonnin <233326+TBonnin@users.noreply.github.com>
2023-12-13 15:24:58 +0100

    Drop indexes for table sync_data_records that are not/little used (#1426)
    Used the query described in the following blog post to determine which indexes
    are not (or rarely) used
    http://www.databasesoup.com/2014/05/new-finding-unused-indexes-query.html

---
+++ packages/shared/lib/db/migrations/20231213124728_drop_sync_data_records_indexes.cjs
@@ -0,0 +1,17 @@
+exports.config = { transaction: false };
+
+exports.up = function(knex) {
+    return knex.schema
+        .raw('DROP INDEX CONCURRENTLY created_at_index')
+        .raw('DROP INDEX CONCURRENTLY _nango_sync_data_records_data_hash_index')
+        .raw('DROP INDEX CONCURRENTLY _nango_sync_data_records_sync_job_id_index')
+        .raw('DROP INDEX CONCURRENTLY _nango_sync_data_records_external_is_deleted_index')
+        .raw('DROP INDEX CONCURRENTLY _nango_sync_data_records_external_deleted_at_index')
+        .raw('DROP INDEX CONCURRENTLY _nango_sync_data_records_deletes_data_hash_index')
+        .raw('DROP INDEX CONCURRENTLY _nango_sync_data_records_deletes_sync_job_id_index')
+        .raw('DROP INDEX CONCURRENTLY _nango_sync_data_records_deletes_external_is_deleted_index')
+        .raw('DROP INDEX CONCURRENTLY _nango_sync_data_records_deletes_external_deleted_at_index')
+        .raw('DROP INDEX CONCURRENTLY _nango_sync_jobs_deleted_index');
+};
+
+exports.down = function(knex) { };
