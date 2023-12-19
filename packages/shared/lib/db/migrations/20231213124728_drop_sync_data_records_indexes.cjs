375aca2b26d7c596694101fdf449cf0b06ae080a
Khaliq <khaliq@nango.dev>
2023-12-19 12:41:05 -0500

    comment out migration that is causing issues (#1446)
    * comment out migration that is causing issues
    
    * return promise

--- packages/shared/lib/db/migrations/20231213124728_drop_sync_data_records_indexes.cjs
+++ packages/shared/lib/db/migrations/20231213124728_drop_sync_data_records_indexes.cjs
@@ -1,6 +1,9 @@
 exports.config = { transaction: false };
 
 exports.up = function(knex) {
+    return Promise.resolve();
+    /*
+     * Production only migration
     return knex.schema
         .raw('DROP INDEX CONCURRENTLY _nango_sync_data_records_data_hash_index')
         .raw('DROP INDEX CONCURRENTLY _nango_sync_data_records_sync_job_id_index')
@@ -11,6 +14,9 @@ exports.up = function(knex) {
         .raw('DROP INDEX CONCURRENTLY _nango_sync_data_records_deletes_external_is_deleted_index')
         .raw('DROP INDEX CONCURRENTLY _nango_sync_data_records_deletes_external_deleted_at_index')
         .raw('DROP INDEX CONCURRENTLY _nango_sync_jobs_deleted_index');
+    */
 };
 
-exports.down = function(knex) { };
+exports.down = function(knex) {
+    return Promise.resolve();
+};
