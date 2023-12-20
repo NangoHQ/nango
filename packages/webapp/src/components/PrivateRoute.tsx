473a25db35a3c1e6fe7f1e79129c58e43a5809dd
Khaliq <khaliqgant@gmail.com>
2023-12-19 22:26:49 -0500

    [nan-164] improve spacing

--- packages/webapp/src/components/PrivateRoute.tsx
+++ packages/webapp/src/components/PrivateRoute.tsx
@@ -3,7 +3,7 @@ import { isCloud, isEnterprise } from '../utils/utils';
 import { isSignedIn } from '../utils/user';
 
 const PrivateRoute = (_: any) => {
-    return <>{(!isCloud() && !isEnterprise() )|| isSignedIn() ? <Outlet /> : <Navigate to="/signin" replace />}</>;
+    return <>{(!isCloud() && !isEnterprise()) || isSignedIn() ? <Outlet /> : <Navigate to="/signin" replace />}</>;
 };
 
 export default PrivateRoute;
