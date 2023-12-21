9991c72c33be00dcf76f774e088650dc6bc61950
Khaliq <khaliq@nango.dev>
2023-12-21 08:41:23 -0500

    [nan-164] update enterprise logic (#1455)
    * [nan-164] update enterprise logic
    
    * [nan-164] copy just the filename

--- packages/jobs/lib/temporal.ts
+++ packages/jobs/lib/temporal.ts
@@ -29,40 +29,44 @@ export class Temporal {
             key = await fs.readFile(`/etc/secrets/${this.namespace}.key`);
         }
 
-        const connection = await NativeConnection.connect({
-            address: process.env['TEMPORAL_ADDRESS'] || 'localhost:7233',
-            tls:
-                !isProd() && !isEnterprise()
-                    ? false
-                    : {
-                          clientCertPair: {
-                              crt: crt as Buffer,
-                              key: key as Buffer
+        try {
+            const connection = await NativeConnection.connect({
+                address: process.env['TEMPORAL_ADDRESS'] || 'localhost:7233',
+                tls:
+                    !isProd() && !isEnterprise()
+                        ? false
+                        : {
+                              clientCertPair: {
+                                  crt: crt as Buffer,
+                                  key: key as Buffer
+                              }
                           }
-                      }
-        });
+            });
 
-        const syncWorker = {
-            connection,
-            namespace: this.namespace,
-            workflowsPath: createRequire(import.meta.url).resolve('./workflows'),
-            activities,
-            maxConcurrentWorkflowTaskExecutions: 50,
-            taskQueue: SYNC_TASK_QUEUE
-        };
+            const syncWorker = {
+                connection,
+                namespace: this.namespace,
+                workflowsPath: createRequire(import.meta.url).resolve('./workflows'),
+                activities,
+                maxConcurrentWorkflowTaskExecutions: 50,
+                taskQueue: SYNC_TASK_QUEUE
+            };
 
-        const webhookWorker = {
-            connection,
-            namespace: this.namespace,
-            workflowsPath: createRequire(import.meta.url).resolve('./workflows'),
-            activities,
-            maxConcurrentWorkflowTaskExecutions: 50,
-            maxActivitiesPerSecond: 50,
-            taskQueue: WEBHOOK_TASK_QUEUE
-        };
+            const webhookWorker = {
+                connection,
+                namespace: this.namespace,
+                workflowsPath: createRequire(import.meta.url).resolve('./workflows'),
+                activities,
+                maxConcurrentWorkflowTaskExecutions: 50,
+                maxActivitiesPerSecond: 50,
+                taskQueue: WEBHOOK_TASK_QUEUE
+            };
 
-        this.workers = await Promise.all([Worker.create(syncWorker), Worker.create(webhookWorker)]);
-        await Promise.all(this.workers.map((worker) => worker.run()));
+            this.workers = await Promise.all([Worker.create(syncWorker), Worker.create(webhookWorker)]);
+            await Promise.all(this.workers.map((worker) => worker.run()));
+        } catch (e) {
+            console.log(e);
+        }
     }
 
     stop() {
