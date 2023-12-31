fd73260a9a86152532ef7c3c9c74a82f042a0be2
Khaliq <khaliqgant@gmail.com>
2023-12-31 11:38:01 -0500

    [nan-207] quiet errors

--- scripts/deploy/runners.bash
+++ scripts/deploy/runners.bash
@@ -18,30 +18,33 @@ while true; do
     break
   fi
 
-  CURSOR=$(echo "$response" | jq -r '.[-1].cursor')
+  CURSOR=$(echo "$response" | jq -r '.[-1].cursor' 2>/dev/null)
 
   if [ -z "$CURSOR" ]; then
     break
   fi
 
-  json_array=$(echo "$response" | jq -c '.[]')
+  json_array=$(echo "$response" | jq -c '.[]' 2>/dev/null)
 
   for item in $json_array; do
-    name=$(echo "$item" | jq -r '.service.name')
+    name=$(echo "$item" | jq -r '.service.name' 2>/dev/null)
     if [[ "$name" != "$ENVIRONMENT-runner-"* ]]; then
       continue
     fi
 
-    serviceId=$(echo "$item" | jq -r '.service.id')
-    echo "Deploying service with ID: $serviceId and name $name"
+    serviceId=$(echo "$item" | jq -r '.service.id' 2>/dev/null)
+    if [ -n "$serviceId" ]; then
+      echo "Deploying service with ID: $serviceId and name $name"
 
-    #curl --request POST \
-      #--url "https://api.render.com/v1/services/$serviceId/deploys" \
-      #--header "accept: application/json" \
-      #--header "authorization: Bearer $API_KEY" \
-      #--header "content-type: application/json"
+      # Uncomment the following lines when you want to make the POST request
+      #curl --request POST \
+      #  --url "https://api.render.com/v1/services/$serviceId/deploys" \
+      #  --header "accept: application/json" \
+      #  --header "authorization: Bearer $API_KEY" \
+      #  --header "content-type: application/json"
+    fi
 
-    RESPONSE_LENGTH=$(echo "$response" | jq -r '. | length')
+    RESPONSE_LENGTH=$(echo "$response" | jq -r '. | length' 2>/dev/null)
     if [ "$RESPONSE_LENGTH" -lt "$LIMIT" ]; then
       break
     fi
