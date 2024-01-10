359ae66a368fd9cbcba65474dffc9da60c951f52
Thomas Bonnin <233326+TBonnin@users.noreply.github.com>
2024-01-10 12:31:01 +0100

    fix npm publish: update shared and node package version in package.json (#1488)
    * fix npm publish: update shared and node package version in package.json
    
    * update node and shared to 0.36.81
    
    * fix: ts-node and typescript are used to compile integrations.
    
    * integration-flows gh action: install CLI from code rather than from npm

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -29,18 +29,15 @@ npm install
 
 # Node client
 bump_and_npm_publish "@nangohq/node" "$VERSION"
-npm update @nangohq/node -w @nangohq/shared
+npm install @nangohq/node@latest -w @nangohq/shared
 
 # Shared
 node scripts/flows.js
 bump_and_npm_publish "@nangohq/shared" "$VERSION"
-npm update @nangohq/shared -w nango -w @nangohq/nango-server -w @nangohq/nango-jobs @nangohq/nango-runner
+npm install @nangohq/shared@latest -w nango -w @nangohq/nango-server -w @nangohq/nango-jobs -w @nangohq/nango-runner
 
 # CLI
 bump_and_npm_publish "nango" "$VERSION"
 
 # Frontend
 bump_and_npm_publish "@nangohq/frontend" "$VERSION"
-
-# update package-lock.json
-npm install
