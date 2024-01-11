55a33cfd8355bbe28d7e27723fd5d85305f02e09
Thomas Bonnin <233326+TBonnin@users.noreply.github.com>
2024-01-11 18:11:44 +0100

    Suspend idle runners (#1503)

--- packages/jobs/lib/server.ts
+++ packages/jobs/lib/server.ts
@@ -1,6 +1,8 @@
 import { initTRPC } from '@trpc/server';
 import { createHTTPServer } from '@trpc/server/adapters/standalone';
 import superjson from 'superjson';
+import { z } from 'zod';
+import { suspendRunner } from './runner/runner.js';
 
 export const t = initTRPC.create({
     transformer: superjson
@@ -11,7 +13,8 @@ const publicProcedure = t.procedure;
 // TODO: add logging middleware
 
 const appRouter = router({
-    health: healthProcedure()
+    health: healthProcedure(),
+    idle: idleProcedure()
 });
 
 export type AppRouter = typeof appRouter;
@@ -22,6 +25,15 @@ export const server = createHTTPServer({
 
 function healthProcedure() {
     return publicProcedure.query(async () => {
+        return { status: 'ok' };
+    });
+}
+
+function idleProcedure() {
+    return publicProcedure.input(z.object({ runnerId: z.string().nonempty(), idleTimeMs: z.number() })).mutation(async ({ input }) => {
+        const { runnerId, idleTimeMs } = input;
+        console.log(`[IDLE]: runner '${runnerId}' has been idle for ${idleTimeMs}ms. Suspending...`);
+        await suspendRunner(runnerId);
         return { status: 'ok' };
     });
 }
