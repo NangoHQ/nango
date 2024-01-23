77fd8a6bc111fc665b432eb3b0e80e42951b784b
Khaliq <khaliqgant@gmail.com>
2024-01-23 20:31:20 +0200

    [nan-234] only update creds if the cred type is oauth

--- packages/shared/lib/integrations/scripts/connection/github-app-oauth-post-connection.ts
+++ packages/shared/lib/integrations/scripts/connection/github-app-oauth-post-connection.ts
@@ -1,4 +1,5 @@
 import type { InternalNango as Nango } from './connection.manager.js';
+import { AuthModes, OAuthCredentials } from '../../../models/Auth.js';
 
 export default async function execute(nango: Nango) {
     const response = await nango.proxy({
@@ -12,6 +13,17 @@ export default async function execute(nango: Nango) {
     const handle = response.data.login;
 
     const connection = await nango.getConnection();
+
+    let updatedConfig: Record<string, string | OAuthCredentials> = {
+        handle
+    };
+
+    if (connection.credentials.type === AuthModes.OAuth2) {
+        updatedConfig = {
+            ...updatedConfig,
+            userCredentials: connection.credentials
+        };
+    }
 
     await nango.updateConnectionConfig({ handle, userCredentials: connection.credentials });
 }
