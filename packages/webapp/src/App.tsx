2ba785099379de2b26265363b04f19416d6005f1
danivazx <danivazx@gmail.com>
2024-01-24 18:23:38 +0100

    webapp: reduce js bundle size by a 1/3 with lazy routes

--- packages/webapp/src/App.tsx
+++ packages/webapp/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import {
     Routes,
     Route,
@@ -11,29 +11,30 @@ import {
 import { MantineProvider } from '@mantine/core';
 import * as Sentry from "@sentry/react";
 
-import Signup from './pages/Signup';
-import InviteSignup from './pages/InviteSignup';
-import Signin from './pages/Signin';
-import GettingStarted from './pages/GettingStarted';
-import IntegrationList from './pages/IntegrationList';
-import IntegrationCreate from './pages/IntegrationCreate';
-import ConnectionList from './pages/ConnectionList';
-import ConnectionCreate from './pages/ConnectionCreate';
-import FlowCreate from './pages/FlowCreate';
-import ConnectionDetails from './pages/ConnectionDetails';
-import ProjectSettings from './pages/ProjectSettings';
-import PrivateRoute from './components/PrivateRoute';
-import ForgotPassword from './pages/ForgotPassword';
-import ResetPassword from './pages/ResetPassword';
-import Activity from './pages/Activity';
-import Syncs from './pages/Syncs';
-import AuthLink from './pages/AuthLink';
-import AccountSettings from './pages/AccountSettings';
-import UserSettings from './pages/UserSettings';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { isCloud, isEnterprise } from './utils/utils';
 import { useStore } from './store';
+
+const Signup = lazy(() => import('./pages/Signup'));
+const InviteSignup = lazy(() => import('./pages/InviteSignup'));
+const Signin = lazy(() => import('./pages/Signin'));
+const GettingStarted = lazy(() => import('./pages/GettingStarted'));
+const IntegrationList = lazy(() => import('./pages/IntegrationList'));
+const IntegrationCreate = lazy(() => import('./pages/IntegrationCreate'));
+const ConnectionList = lazy(() => import('./pages/ConnectionList'));
+const ConnectionCreate = lazy(() => import('./pages/ConnectionCreate'));
+const FlowCreate = lazy(() => import('./pages/FlowCreate'));
+const ConnectionDetails = lazy(() => import('./pages/ConnectionDetails'));
+const ProjectSettings = lazy(() => import('./pages/ProjectSettings'));
+const PrivateRoute = lazy(() => import('./components/PrivateRoute'));
+const ForgotPassword = lazy(() => import('./pages/ForgotPassword'));
+const ResetPassword = lazy(() => import('./pages/ResetPassword'));
+const Activity = lazy(() => import('./pages/Activity'));
+const Syncs = lazy(() => import('./pages/Syncs'));
+const AuthLink = lazy(() => import('./pages/AuthLink'));
+const AccountSettings = lazy(() => import('./pages/AccountSettings'));
+const UserSettings = lazy(() => import('./pages/UserSettings'));
 
 Sentry.init({
   dsn: process.env.REACT_APP_PUBLIC_SENTRY_KEY,
@@ -74,58 +75,60 @@ const App = () => {
                 })
             }}
         >
-            <SentryRoutes>
-                <Route path="/" element={<Navigate to={landingPage} replace />} />
-                <Route path="/getting-started" element={<PrivateRoute />}>
-                    <Route path="/getting-started" element={<GettingStarted />} />
-                </Route>
-                <Route path="/integrations" element={<PrivateRoute />}>
-                    <Route path="/integrations" element={<IntegrationList />} />
-                </Route>
-                <Route path="/integration/create" element={<PrivateRoute />}>
-                    <Route path="/integration/create" element={<IntegrationCreate />} />
-                </Route>
-                <Route path="/integration/:providerConfigKey" element={<PrivateRoute />}>
-                    <Route path="/integration/:providerConfigKey" element={<IntegrationCreate />} />
-                </Route>
-                <Route path="/syncs" element={<PrivateRoute />}>
-                    <Route path="/syncs" element={<Syncs />} />
-                </Route>
-                <Route path="/connections" element={<PrivateRoute />}>
-                    <Route path="/connections" element={<ConnectionList />} />
-                </Route>
-                <Route path="/connections/create" element={<PrivateRoute />}>
-                    <Route path="/connections/create" element={<ConnectionCreate />} />
-                </Route>
-                <Route path="/connections/create/:providerConfigKey" element={<PrivateRoute />}>
-                    <Route path="/connections/create/:providerConfigKey" element={<ConnectionCreate />} />
-                </Route>
-                <Route path="/connections/:providerConfigKey/:connectionId" element={<PrivateRoute />}>
-                    <Route path="/connections/:providerConfigKey/:connectionId" element={<ConnectionDetails />} />
-                </Route>
-                <Route path="/activity" element={<PrivateRoute />}>
-                    <Route path="/activity" element={<Activity />} />
-                </Route>
-                <Route path="/project-settings" element={<PrivateRoute />}>
-                    <Route path="/project-settings" element={<ProjectSettings />} />
-                </Route>
-                <Route path="/auth-link" element={<AuthLink />} />
-                <Route path="/flow/create" element={<PrivateRoute />}>
-                    <Route path="/flow/create" element={<FlowCreate />} />
-                </Route>
-                {(isCloud() || isEnterprise()) && (
-                    <>
-                        <Route path="/account-settings" element={<AccountSettings />} />
-                        <Route path="/user-settings" element={<UserSettings />} />
-                        <Route path="/signin" element={<Signin />} />
-                        <Route path="/signup" element={<Signup />} />
-                        <Route path="/signup/:token" element={<InviteSignup />} />
-                        <Route path="/forgot-password" element={<ForgotPassword />} />
-                        <Route path="/reset-password/:token" element={<ResetPassword />} />
-                    </>
-                )}
-                <Route path="*" element={<Navigate to="/" replace />} />
-            </SentryRoutes>
+            <Suspense>
+                <SentryRoutes >
+                    <Route path="/" element={<Navigate to={landingPage} replace />} />
+                    <Route path="/getting-started" element={<PrivateRoute />}>
+                        <Route path="/getting-started" element={<GettingStarted />} />
+                    </Route>
+                    <Route path="/integrations" element={<PrivateRoute />}>
+                        <Route path="/integrations" element={<IntegrationList />} />
+                    </Route>
+                    <Route path="/integration/create" element={<PrivateRoute />}>
+                        <Route path="/integration/create" element={<IntegrationCreate />} />
+                    </Route>
+                    <Route path="/integration/:providerConfigKey" element={<PrivateRoute />}>
+                        <Route path="/integration/:providerConfigKey" element={<IntegrationCreate />} />
+                    </Route>
+                    <Route path="/syncs" element={<PrivateRoute />}>
+                        <Route path="/syncs" element={<Syncs />} />
+                    </Route>
+                    <Route path="/connections" element={<PrivateRoute />}>
+                        <Route path="/connections" element={<ConnectionList />} />
+                    </Route>
+                    <Route path="/connections/create" element={<PrivateRoute />}>
+                        <Route path="/connections/create" element={<ConnectionCreate />} />
+                    </Route>
+                    <Route path="/connections/create/:providerConfigKey" element={<PrivateRoute />}>
+                        <Route path="/connections/create/:providerConfigKey" element={<ConnectionCreate />} />
+                    </Route>
+                    <Route path="/connections/:providerConfigKey/:connectionId" element={<PrivateRoute />}>
+                        <Route path="/connections/:providerConfigKey/:connectionId" element={<ConnectionDetails />} />
+                    </Route>
+                    <Route path="/activity" element={<PrivateRoute />}>
+                        <Route path="/activity" element={<Activity />} />
+                    </Route>
+                    <Route path="/project-settings" element={<PrivateRoute />}>
+                        <Route path="/project-settings" element={<ProjectSettings />} />
+                    </Route>
+                    <Route path="/auth-link" element={<AuthLink />} />
+                    <Route path="/flow/create" element={<PrivateRoute />}>
+                        <Route path="/flow/create" element={<FlowCreate />} />
+                    </Route>
+                    {(isCloud() || isEnterprise()) && (
+                        <>
+                            <Route path="/account-settings" element={<AccountSettings />} />
+                            <Route path="/user-settings" element={<UserSettings />} />
+                            <Route path="/signin" element={<Signin />} />
+                            <Route path="/signup" element={<Signup />} />
+                            <Route path="/signup/:token" element={<InviteSignup />} />
+                            <Route path="/forgot-password" element={<ForgotPassword />} />
+                            <Route path="/reset-password/:token" element={<ResetPassword />} />
+                        </>
+                    )}
+                    <Route path="*" element={<Navigate to="/" replace />} />
+                </SentryRoutes>
+            </Suspense>
             <ToastContainer />
         </MantineProvider>
     );
