83ae799b202d77e1d60afa2516ea560817f795e7
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-02-06 17:36:05 +0100

    add post connection

---
+++ packages/shared/lib/integrations/scripts/connection/linear-post-connection.ts
@@ -0,0 +1,25 @@
+import type { InternalNango as Nango } from './connection.manager.js';
+
+export default async function execute(nango: Nango) {
+    const query = `
+        query {
+            organization {
+                id
+            }
+        }`;
+
+    const response = await nango.proxy({
+        baseUrlOverride: 'https://api.linear.app',
+        endpoint: '/graphql',
+        data: { query },
+        method: 'POST'
+    });
+
+    if (!response || !response.data || !response.data.data?.organization?.id) {
+        return;
+    }
+
+    const organizationId = response.data.data.organization.id;
+
+    await nango.updateConnectionConfig({ organizationId });
+}
