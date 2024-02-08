a34c7b6c76baa9524363c65025c0847529f0fd1e
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-02-08 11:06:08 +0100

    proper error

--- packages/shared/lib/utils/result.ts
+++ packages/shared/lib/utils/result.ts
@@ -1,12 +1,15 @@
 export type ResultRes<T> = { ok: true; res: T };
 export type ResultErr<E extends Error> = { ok: false; err: E };
 export type Result<T, E extends Error = Error> = ResultRes<T> | ResultErr<E>;
-export function ok<T>(res: T): ResultRes<T> {
+export function resultOk<T>(res: T): ResultRes<T> {
     return { ok: true, res };
 }
-export function err<E extends Error>(errMsg: string): ResultErr<E> {
-    const e = new Error(errMsg) as E;
-    return { ok: false, err: e };
+export function resultErr<E extends Error>(e: Error | string): ResultErr<E> {
+    if (e instanceof Error) {
+        return { ok: false, err: e as E };
+    } else {
+        return { ok: false, err: new Error(e) as E };
+    }
 }
 export function isOk<T, E extends Error>(result: Result<T, E>): result is ResultRes<T> {
     return result.ok;
