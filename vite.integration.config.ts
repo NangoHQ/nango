5c59d9025708e7cf8698d0cf4e9515e10030eb56
Khaliq <khaliqgant@gmail.com>
2024-02-13 22:06:32 +0200

    [nan-347] vitest version restore

--- vite.integration.config.ts
+++ vite.integration.config.ts
@@ -8,9 +8,7 @@ export default defineConfig({
     test: {
         include: ['**/*.integration.{test,spec}.?(c|m)[jt]s?(x)'],
         globalSetup: './tests/setup.ts',
-        poolOptions: {
-            singleThread: true
-        },
+        threads: false,
         testTimeout: 20000
     }
 });
