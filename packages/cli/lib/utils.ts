c096eca870db2becea1fb21c74bfe4c73ee292f4
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-02-14 13:07:12 +0100

    do not ignore when version is equal

--- packages/cli/lib/utils.ts
+++ packages/cli/lib/utils.ts
@@ -153,7 +153,6 @@ export async function upgradeAction(debug = false) {
         }
 
         if (!semver.gt(latestVersion, version)) {
-            state.set('lastIgnoreUpgrade', Date.now());
             return;
         }
 
