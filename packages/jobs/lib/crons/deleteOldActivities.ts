4152a3c39c6d2a9733fd4345932b61e5ab1551cb
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-02-20 19:56:04 +0100

    fix(cron): fine tune delete old activities

--- packages/jobs/lib/crons/deleteOldActivities.ts
+++ packages/jobs/lib/crons/deleteOldActivities.ts
@@ -1,30 +1,26 @@
 import * as cron from 'node-cron';
-import { deleteLog, deleteLogsMessages, errorManager, ErrorSourceEnum, findOldActivities, logger } from '@nangohq/shared';
-import { SpanTypes } from '@nangohq/shared';
+import { deleteLog, deleteLogsMessages, errorManager, ErrorSourceEnum, findOldActivities, logger, MetricTypes, telemetry } from '@nangohq/shared';
 import tracer from '../tracer.js';
 import { setTimeout } from 'node:timers/promises';
 
 // Retention in days
 const retention = parseInt(process.env['NANGO_CLEAR_ACTIVIES_RETENTION'] || '', 10) || 15;
-const limitLog = parseInt(process.env['NANGO_CLEAR_ACTIVIES_LIMIT'] || '', 10) || 1000;
+const limitLog = parseInt(process.env['NANGO_CLEAR_ACTIVIES_LIMIT'] || '', 10) || 2000;
 const limitMsg = parseInt(process.env['NANGO_CLEAR_ACTIVIES_MSG_LIMIT'] || '', 10) || 5000;
 
 export async function deleteOldActivityLogs(): Promise<void> {
     /**
      * Delete all activity logs older than 15 days
      */
-    cron.schedule('*/15 * * * *', async () => {
-        const span = tracer.startSpan(SpanTypes.JOBS_CLEAN_ACTIVITY_LOGS);
-        tracer.scope().activate(span, async () => {
-            try {
-                await exec();
-            } catch (err: unknown) {
-                const e = new Error('failed_to_clean_activity_logs_table', { cause: err instanceof Error ? err.message : err });
-                errorManager.report(e, { source: ErrorSourceEnum.PLATFORM }, tracer);
-            }
-
-            span.finish();
-        });
+    cron.schedule('*/10 * * * *', async () => {
+        const start = Date.now();
+        try {
+            await exec();
+        } catch (err: unknown) {
+            const e = new Error('failed_to_clean_activity_logs_table', { cause: err instanceof Error ? err.message : err });
+            errorManager.report(e, { source: ErrorSourceEnum.PLATFORM }, tracer);
+        }
+        telemetry.duration(MetricTypes.JOBS_CLEAN_ACTIVITY_LOGS, Date.now() - start);
     });
 }
 
@@ -45,7 +41,7 @@ export async function exec(): Promise<void> {
             logger.info(`[oldActivity] deleted ${count} rows`);
 
             // Free the CPU
-            await setTimeout(250);
+            await setTimeout(200);
         } while (count >= limitMsg);
 
         await deleteLog({ activityLogId: log.id });
