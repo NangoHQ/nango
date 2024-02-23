150a6367ffb5b4c28f42046e1d30d15e00af8a94
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-02-23 17:39:33 +0100

    use one

--- packages/shared/lib/db/migrations/20240223162238_connections-index.cjs
+++ packages/shared/lib/db/migrations/20240223162238_connections-index.cjs
@@ -2,15 +2,10 @@ exports.config = { transaction: false };
 
 exports.up = async function (knex) {
     await knex.schema.raw(
-        'CREATE INDEX CONCURRENTLY "idx_connections_connectionid_envid_provider_where_deleted" ON "nango"."_nango_connections" USING BTREE ("connection_id","environment_id","provider_config_key") WHERE deleted=false'
-    );
-
-    await knex.schema.raw(
-        'CREATE INDEX CONCURRENTLY "idx_connections_envid_where_deleted" ON "nango"."_nango_connections" USING BTREE ("environment_id") WHERE deleted=false'
+        'CREATE INDEX "idx_connections_envid_connectionid_provider_where_deleted" ON "nango"."_nango_connections" USING BTREE ("environment_id","connection_id","provider_config_key") WHERE deleted=false'
     );
 };
 
 exports.down = async function (knex) {
-    await knex.schema.raw('DROP INDEX CONCURRENTLY idx_connections_connectionid_envid_provider_where_deleted');
-    await knex.schema.raw('DROP INDEX CONCURRENTLY idx_connections_envid_where_deleted');
+    await knex.schema.raw('DROP INDEX CONCURRENTLY idx_connections_envid_connectionid_provider_where_deleted');
 };
