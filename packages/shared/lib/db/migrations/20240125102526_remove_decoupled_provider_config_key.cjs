6ff206458d902ba198726761b6aad5d44897009e
Khaliq <khaliqgant@gmail.com>
2024-02-23 11:31:41 +0200

    check for objects

--- packages/shared/lib/db/migrations/20240125102526_remove_decoupled_provider_config_key.cjs
+++ packages/shared/lib/db/migrations/20240125102526_remove_decoupled_provider_config_key.cjs
@@ -9,9 +9,15 @@ exports.up = async function(knex) {
     const connections = await knex.select('id', 'provider_config_key', 'environment_id').from(`nango.${CONNECTIONS_TABLE}`);
 
     for (const connection of connections) {
+        if (!connection) {
+            continue;
+        }
         const { id, provider_config_key, environment_id } = connection;
         const config = await knex.select('id').from(`${CONFIGS_TABLE}`).where({ unique_key: provider_config_key, environment_id }).first();
-        await knex(`nango.${CONNECTIONS_TABLE}`).where({ id }).update({ config_id: config.id });
+
+        if (config) {
+            await knex(`nango.${CONNECTIONS_TABLE}`).where({ id }).update({ config_id: config.id });
+        }
     }
 };
 
