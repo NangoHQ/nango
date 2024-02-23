8010f58b66a898863da1dc0b2afdbab45b6d1a78
Khaliq <khaliqgant@gmail.com>
2024-02-23 16:57:30 +0200

    [nan-425] don't reload everything on filter add

--- packages/webapp/src/pages/Activity.tsx
+++ packages/webapp/src/pages/Activity.tsx
@@ -347,7 +347,7 @@ export default function Activity() {
         );
     }
 
-    if (activities === null) {
+    if (!filterSet && activities === null) {
         return (
             <DashboardLayout selectedItem={LeftNavBarItems.Activity} marginBottom={60}>
                 <Loading spaceRatio={2.5} className="-top-36" />
@@ -469,7 +469,7 @@ export default function Activity() {
                             <span onClick={decrementPage} className={`flex ${Number(offset) - limit >= 0 ? 'cursor-pointer hover:bg-gray-700' : ''} h-8 mr-2 rounded-md px-3 pt-1.5 text-sm text-white bg-gray-800`}>
                               <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clipRule="evenodd"></path></svg>
                             </span>
-                            <span onClick={incrementPage} className={`flex ${activities.length < limit ? '' : 'cursor-pointer hover:bg-gray-700'} h-8 rounded-md px-3 pt-1.5 text-sm text-white bg-gray-800`}>
+                            <span onClick={incrementPage} className={`flex ${activities && activities?.length < limit ? '' : 'cursor-pointer hover:bg-gray-700'} h-8 rounded-md px-3 pt-1.5 text-sm text-white bg-gray-800`}>
                               <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                             </span>
                         </div>
@@ -486,7 +486,7 @@ export default function Activity() {
                             <div className="text-white ml-3">No activity found</div>
                         </div>
                     )}
-                    {activities.length > 0 && (
+                    {activities && activities.length > 0 && (
                         <>
                         <div className="h-fit border border-border-gray rounded-md text-white text-sm overflow-hidden min-w-[1150px]">
                             <table className="table-auto">
