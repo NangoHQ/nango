e04c35c5d696039a0473a493eb40223c689231ee
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-02-29 23:20:46 +0100

    missing async

--- packages/shared/lib/db/migrations/20240229220040_records_id-index.cjs
+++ packages/shared/lib/db/migrations/20240229220040_records_id-index.cjs
@@ -1,9 +1,9 @@
 exports.config = { transaction: false };
 
-exports.up = function(knex) {
-  await knex.schema.raw('CREATE UNIQUE INDEX CONCURRENTLY "idx_records_id_pkey" ON "nango"."_nango_sync_data_records" USING BTREE ("id")');
+exports.up = async function (knex) {
+    await knex.schema.raw('CREATE UNIQUE INDEX CONCURRENTLY "idx_records_id_pkey" ON "_nango_sync_data_records" USING BTREE ("id")');
 };
 
-exports.down = function(knex) {
-  await knex.schema.raw('DROP INDEX CONCURRENTLY idx_records_id_pkey');
+exports.down = async function (knex) {
+    await knex.schema.raw('DROP INDEX CONCURRENTLY idx_records_id_pkey');
 };
