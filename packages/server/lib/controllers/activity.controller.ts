NangoHQ/nango

commit 60a0b437035c0bc0a578985f0e572259347ca972

feat(activity):connection and integration filters in activities (#1648)
* feat(activity):connection and integration filters only present in activities

* review

* prettier

packages/server/lib/controllers/activity.controller.ts
@@ -2,7 +2,7 @@ import type { Request, Response } from 'express';
 import type { NextFunction } from 'express';
 
 import { getUserAccountAndEnvironmentFromSession } from '../utils/utils.js';
-import { connectionService, configService, getAllSyncAndActionNames, getTopLevelLogByEnvironment, getLogMessagesForLogs, errorManager } from '@nangohq/shared';
+import { activityFilter, getAllSyncAndActionNames, getTopLevelLogByEnvironment, getLogMessagesForLogs, errorManager } from '@nangohq/shared';
 
 class ActivityController {
     public async retrieve(req: Request, res: Response, next: NextFunction) {
@@ -56,8 +56,8 @@ class ActivityController {
             const { environment } = response;
 
             const scripts = await getAllSyncAndActionNames(environment.id);
-            const integrations = await configService.getAllNames(environment.id);
-            const connections = await connectionService.getAllNames(environment.id);
+            const integrations = await activityFilter(environment.id, 'connection_id');
+            const connections = await activityFilter(environment.id, 'provider');
             res.send({ scripts, integrations, connections });
         } catch (error) {
             next(error);

packages/shared/lib/services/activity/activity.service.ts
@@ -255,6 +255,25 @@ export async function getTopLevelLogByEnvironment(
     return logs || [];
 }
 
+export async function activityFilter(environment_id: number, filterColumn: 'connection_id' | 'provider'): Promise<string[]> {
+    const logsQuery = db.knex
+        .withSchema(db.schema())
+        .from<ActivityLog>('_nango_activity_logs')
+        .where({ environment_id })
+        .whereNotNull(filterColumn)
+        .groupBy(filterColumn)
+        .select(filterColumn)
+        .orderBy(filterColumn, 'asc');
+
+    const logs = await logsQuery;
+
+    const distinctValues: string[] = logs
+        .map((log: { [key: string]: string }) => log[filterColumn] as string)
+        .filter((value: string | undefined): value is string => typeof value === 'string');
+
+    return distinctValues;
+}
+
 /**
  * Retrieves log messages and organizes them by log ID using raw SQL.
  * @desc Iterates over an array of log IDs, fetching the corresponding log messages

packages/webapp/src/pages/Activity.tsx
@@ -353,40 +353,45 @@ export default function Activity() {
         const value = e.target.value;
         setStatus(value);
         setLoaded(false);
+        setOffset(0);
 
-        navigate(location.pathname + '?' + queryString.stringify({ ...queryParams, status: value }));
+        navigate(location.pathname + '?' + queryString.stringify({ ...queryParams, status: value, offset: 0 }));
     }
 
     const handleScriptChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         const value = e.target.value;
         setSelectedScript(value);
         setLoaded(false);
+        setOffset(0);
 
-        navigate(location.pathname + '?' + queryString.stringify({ ...queryParams, script: value }));
+        navigate(location.pathname + '?' + queryString.stringify({ ...queryParams, script: value, offset: 0 }));
     }
 
     const handleIntegrationChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         const value = e.target.value;
         setSelectedIntegration(value);
         setLoaded(false);
+        setOffset(0);
 
-        navigate(location.pathname + '?' + queryString.stringify({ ...queryParams, integration: value }));
+        navigate(location.pathname + '?' + queryString.stringify({ ...queryParams, integration: value, offset: 0 }));
     }
 
     const handleConnectionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         const value = e.target.value;
         setSelectedConnection(value);
         setLoaded(false);
+        setOffset(0);
 
-        navigate(location.pathname + '?' + queryString.stringify({ ...queryParams, connection: value }));
+        navigate(location.pathname + '?' + queryString.stringify({ ...queryParams, connection: value, offset: 0 }));
     }
 
     const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const value = e.target.value;
         setDate(value);
         setLoaded(false);
+        setOffset(0);
 
-        navigate(location.pathname + '?' + queryString.stringify({ ...queryParams, date: value }));
+        navigate(location.pathname + '?' + queryString.stringify({ ...queryParams, date: value, offset: 0 }));
     }
 
     const onRemoveFilter = (action: (val: string) => void, prop: string) => {

commit 6a1cf9985942cc94c7665961b1401191fb7fa294

Khaliq/nan 525 upfort logs filter bug (#1825)
## Describe your changes
Integration filter should use the provider config key instead of the
provider. This bug was introduced by
https://github.com/NangoHQ/nango/pull/1648

## Issue ticket number and link
NAN-525

## Checklist before requesting a review (skip if just adding/editing
APIs & templates)
- [ ] I added tests, otherwise the reason is: 
- [ ] I added observability, otherwise the reason is:
- [ ] I added analytics, otherwise the reason is:

packages/server/lib/controllers/activity.controller.ts
@@ -73,7 +73,7 @@ class ActivityController {
             const { environment } = response;
 
             const scripts = await getAllSyncAndActionNames(environment.id);
-            const integrations = await activityFilter(environment.id, 'provider');
+            const integrations = await activityFilter(environment.id, 'provider_config_key');
             const connections = await activityFilter(environment.id, 'connection_id');
             res.send({ scripts, integrations, connections });
         } catch (error) {

packages/shared/lib/services/activity/activity.service.ts
@@ -248,10 +248,16 @@ export async function getTopLevelLogByEnvironment(
     return logs || [];
 }
 
-export async function activityFilter(environment_id: number, filterColumn: 'connection_id' | 'provider'): Promise<string[]> {
+export async function activityFilter(environment_id: number, filterColumn: 'connection_id' | 'provider_config_key'): Promise<string[]> {
     const logsQuery = db.knex
         .from<ActivityLog>('_nango_activity_logs')
-        .where({ environment_id })
+        .where({
+            environment_id
+        })
+        .andWhereNot({
+            [filterColumn]: '',
+            action: 'sync deploy'
+        })
         .whereNotNull(filterColumn)
         .groupBy(filterColumn)
         .select(filterColumn)