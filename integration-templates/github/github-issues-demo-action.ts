e38f5c3d6103c55e7219ed1723773d10e615f3bb
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-03-11 11:41:15 +0100

    add action too

---
+++ integration-templates/github/github-issues-demo-action.ts
@@ -0,0 +1,25 @@
+import type { NangoSync } from './models';
+
+interface GithubIssueInput {
+    title: string;
+}
+
+export default async function runAction(nango: NangoSync, input: GithubIssueInput): Promise<{ status: boolean}> {
+    // Fetch issues from GitHub
+    const res = await nango.post({
+        endpoint: '/repos/NangoHQ/interactive-demo/issues',
+        data: {
+            title: input.title,
+            body: `This Issue was created automatically using Nango's Action system.
+
+Action can be triggered with an HTTP call.
+Take a look at our [Documentation](https://docs.nango.dev/integrate/guides/perform-workflows-with-an-api)`,
+            labels: ['automatic']
+        }
+    });
+
+
+    return {
+        status: res.status === 201,
+    };
+}
