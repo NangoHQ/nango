NangoHQ/nango

commit ce8e53481a86bf713d94104c3a4f13d0f7fa0d6c

fix(ui): use hooks for meta/env/user (#1844)
## Describe your changes

Fixes NAN-558
Contributes to NAN-516

We were spamming a bit /meta /env /user because we were using straight
http call in the components.
This is tentative to start refactoring those calls using SWR and hooks.
I also need to add stuff to `/meta` for the new interactive demo and it
was a good time to have something stable to do it later.

- Start exporting some types from the Server
We are duplicating types from server to webapp, which creates
discrepancy. Start small by exposing `GetMeta` to see if the forms is
fitting.

- Introduce `useEnvironment` `useMeta` `useUser` 
This should remove the biggest offender that fetch on every pages. This
also reduce the boilerplate needed on some pages, but could be reduced
further by removing some useless setState.

packages/webapp/src/pages/Activity.tsx
@@ -3,7 +3,7 @@ import useSWR from 'swr';
 import { Helmet } from 'react-helmet';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Loading } from '@geist-ui/core';
-import { requestErrorToast } from '../utils/api';
+import { requestErrorToast, swrFetcher } from '../utils/api';
 import {
     ChevronsLeft,
     Clock,
@@ -80,11 +80,9 @@ const JsonPrettyPrint: React.FC<Props> = ({ data }): ReactElement<any, any> => {
 export default function Activity() {
     const navigate = useNavigate();
 
-    const [activities, setActivities] = useState<ActivityResponse[] | null>(null);
     const [expandedRow, setExpandedRow] = useState(-1);
     const [limit] = useState(20);
     const [logIds, setLogIds] = useState<number[]>([]);
-    const [updateLogs, setUpdateLogs] = useState(false);
 
     const [scripts, setScripts] = useState<string[]>([]);
     const [integrations, setIntegrations] = useState<string[]>([]);
@@ -108,45 +106,48 @@ export default function Activity() {
     const [selectedIntegration, setSelectedIntegration] = useState(queryParams.integration || '');
     const [selectedConnection, setSelectedConnection] = useState(queryParams.connection || '');
     const [selectedDate, setDate] = useState(queryParams.date || '');
-    const [filterSet, setFilterSet] = useState(false);
+    const [loading, setLoading] = useState(false);
 
-    const { error } = useSWR(
+    const { data: activities, error } = useSWR<ActivityResponse[]>(
         `/api/v1/activity?env=${env}&limit=${limit}&offset=${offset}` +
             `${status ? `&status=${status}` : ''}` +
             `${selectedScript ? `&script=${selectedScript}` : ''}` +
             `${selectedIntegration ? `&integration=${selectedIntegration}` : ''}` +
             `${selectedConnection ? `&connection=${selectedConnection}` : ''}` +
             `${selectedDate ? `&date=${selectedDate}` : ''}`,
+        swrFetcher,
         {
-            onSuccess: (data) => {
-                setFilterSet(false);
-                setActivities(data);
-                setLogIds(data.map((activity: ActivityResponse) => activity.id));
-                setActivityRefs(
-                    data.reduce((acc: Record<number, React.RefObject<HTMLTableRowElement>>, activity: ActivityResponse) => {
-                        acc[activity.id] = createRef<HTMLTableRowElement>();
-                        return acc;
-                    }, {})
-                );
-                setUpdateLogs(!updateLogs);
-            }
+            refreshInterval: 15000,
+            keepPreviousData: false
+        }
+    );
+    useEffect(() => {
+        if (activities) {
+            setLogIds(activities.map((activity: ActivityResponse) => activity.id));
+            setActivityRefs(
+                activities.reduce((acc: Record<number, React.RefObject<HTMLTableRowElement>>, activity: ActivityResponse) => {
+                    acc[activity.id] = createRef<HTMLTableRowElement>();
+                    return acc;
+                }, {})
+            );
+            setLoading(false);
+        }
+        if (error) {
+            setLoading(false);
+        }
+    }, [activities, error]);
+
+    const { data: msgs, error: logActivitiesError } = useSWR<ActivityMessageResponse>(
+        () => (logIds.length > 0 ? `/api/v1/activity-messages?logIds=${logIds.join(',')}` : null),
+        swrFetcher,
+        {
+            refreshInterval: expandedRow !== -1 ? 5000 : 60000
         }
     );
 
-    const { error: logActivitiesError } = useSWR(
-        () => (logIds.length > 0 ? `/api/v1/activity-messages?updateLogs=${updateLogs}&logIds=${logIds.join(',')}` : null),
-        {
-            onSuccess: (data: ActivityMessageResponse) => {
-                const updatedActivities = activities?.map((activity) => {
-                    const messages = data[activity.id];
-                    return { ...activity, messages: messages ? messages.reverse() : [] };
-                });
-                setActivities(updatedActivities as ActivityResponse[]);
-            }
-        }
-    );
-
-    const { data: activityFilters } = useSWR(`/api/v1/activity-filters?env=${env}`);
+    const { data: activityFilters } = useSWR<Record<string, any>>(`/api/v1/activity-filters?env=${env}`, swrFetcher, {
+        refreshInterval: 60000
+    });
 
     useEffect(() => {
         if (queryParams.offset) setOffset(parseInt(queryParams.offset as string, 10));
@@ -233,8 +234,7 @@ export default function Activity() {
     };
 
     const handleStatusChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        setActivities([]);
-        setFilterSet(true);
+        setLoading(true);
         const value = e.target.value;
         setStatus(value);
         setOffset(0);
@@ -243,8 +243,7 @@ export default function Activity() {
     };
 
     const handleScriptChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        setActivities([]);
-        setFilterSet(true);
+        setLoading(true);
         const value = e.target.value;
         setSelectedScript(value);
         setOffset(0);
@@ -253,8 +252,7 @@ export default function Activity() {
     };
 
     const handleIntegrationChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        setActivities([]);
-        setFilterSet(true);
+        setLoading(true);
         const value = e.target.value;
         setSelectedIntegration(value);
         setOffset(0);
@@ -263,8 +261,7 @@ export default function Activity() {
     };
 
     const handleConnectionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        setActivities([]);
-        setFilterSet(true);
+        setLoading(true);
         const value = e.target.value;
         setSelectedConnection(value);
         setOffset(0);
@@ -273,8 +270,7 @@ export default function Activity() {
     };
 
     const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setActivities([]);
-        setFilterSet(true);
+        setLoading(true);
         const value = e.target.value;
         setDate(value);
         setOffset(0);
@@ -283,8 +279,7 @@ export default function Activity() {
     };
 
     const onRemoveFilter = (action: (val: string) => void, prop: string) => {
-        setActivities([]);
-        setFilterSet(true);
+        setLoading(true);
         action('');
         const url = window.location.pathname;
         const searchParams = new URLSearchParams(window.location.search);
@@ -354,7 +349,7 @@ export default function Activity() {
         );
     }
 
-    if (!filterSet && activities === null) {
+    if (!activities) {
         return (
             <DashboardLayout selectedItem={LeftNavBarItems.Activity} marginBottom={60}>
                 <Loading spaceRatio={2.5} className="-top-36" />
@@ -528,7 +523,7 @@ export default function Activity() {
                     </div>
                 )}
 
-                {filterSet ? (
+                {loading ? (
                     <Loading spaceRatio={2.5} className="top-48" />
                 ) : (
                     <>
@@ -797,25 +792,28 @@ export default function Activity() {
                                                                     )}
                                                                 </Link>
                                                                 <p className="text-gray-500 w-40">{formatTimestamp(Number(activity.timestamp))}</p>
-                                                                {activity.messages && activity.messages.length > 0 && activity.messages[0] !== null && (
-                                                                    <button
-                                                                        className="flex h-8 mr-2 rounded-md pl-2 pr-3 pt-1.5 text-sm text-white bg-gray-800 hover:bg-hover-gray"
-                                                                        onClick={() => setExpandedRow(activity.id === expandedRow ? -1 : activity.id)}
-                                                                    >
-                                                                        <p>{activity.id === expandedRow ? 'Hide Logs' : 'Show Logs'}</p>
-                                                                    </button>
-                                                                )}
-                                                                {activity.messages && activity.messages.length > 0 && activity.messages[0] && (
-                                                                    <CopyButton icontype="link" dark text={copyActivityLogUrl(activity)} />
-                                                                )}
+                                                                {msgs &&
+                                                                    activity.id in msgs &&
+                                                                    msgs[activity.id].length > 0 &&
+                                                                    msgs[activity.id][0] !== null && (
+                                                                        <>
+                                                                            <button
+                                                                                className="flex h-8 mr-2 rounded-md pl-2 pr-3 pt-1.5 text-sm text-white bg-gray-800 hover:bg-hover-gray"
+                                                                                onClick={() => setExpandedRow(activity.id === expandedRow ? -1 : activity.id)}
+                                                                            >
+                                                                                <p>{activity.id === expandedRow ? 'Hide Logs' : 'Show Logs'}</p>
+                                                                            </button>
+                                                                            <CopyButton icontype="link" dark text={copyActivityLogUrl(activity)} />
+                                                                        </>
+                                                                    )}
                                                             </div>
-                                                            {activity.id === expandedRow && activity.messages && activity.messages[0] && (
+                                                            {activity.id === expandedRow && msgs && msgs[expandedRow] && (
                                                                 <>
                                                                     <div className="flex flex-col space-y-4 mt-6 font-mono">
-                                                                        {activity.messages.length >= 1000 && (
+                                                                        {msgs[expandedRow].length >= 1000 && (
                                                                             <div className="text-center text-gray-500">[only showing the last 1000 logs]</div>
                                                                         )}
-                                                                        {activity.messages.map((message, index: number) => (
+                                                                        {msgs[expandedRow].map((message, index: number) => (
                                                                             <div key={index} className="flex flex-col max-w-7xl">
                                                                                 <div className="whitespace-normal break-all overflow-wrap">
                                                                                     <span className="text-gray-500">

packages/server/lib/controllers/environment.controller.ts
@@ -10,13 +10,22 @@ import {
     getOauthCallbackUrl,
     getGlobalWebhookReceiveUrl,
     packageJsonFile,
-    getEnvironmentId
+    getEnvironmentId,
+    Environment
 } from '@nangohq/shared';
 import { getUserAccountAndEnvironmentFromSession } from '../utils/utils.js';
 import { NANGO_ADMIN_UUID } from './account.controller.js';
 
+export interface GetMeta {
+    environments: Environment[];
+    email: string;
+    version: string;
+    baseUrl: string;
+    debugMode: boolean;
+}
+
 class EnvironmentController {
-    async meta(req: Request, res: Response, next: NextFunction) {
+    async meta(req: Request, res: Response<GetMeta>, next: NextFunction) {
         try {
             const { success: sessionSuccess, error: sessionError, response } = await getUserAccountAndEnvironmentFromSession(req);
             if (!sessionSuccess || response === null) {

packages/server/lib/index.ts
@@ -1,0 +1,1 @@
+export { GetMeta } from './controllers/environment.controller.js';

packages/webapp/src/App.tsx
@@ -6,7 +6,7 @@ import * as Sentry from '@sentry/react';
 import { useSignout } from './utils/user';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { isCloud, isEnterprise, isLocal } from './utils/utils';
+import { AUTH_ENABLED, isCloud, isLocal } from './utils/utils';
 import { fetcher } from './utils/api';
 import { useStore } from './store';
 
@@ -100,6 +100,11 @@ const App = () => {
         >
             <SWRConfig
                 value={{
+                    refreshInterval: 15 * 60000,
+                    // Our server is not well configured if we enable that it will just fetch all the time
+                    revalidateIfStale: false,
+                    revalidateOnFocus: false,
+                    revalidateOnReconnect: true,
                     fetcher,
                     onError: (error) => {
                         if (error.status === 401) {
@@ -110,43 +115,31 @@ const App = () => {
             >
                 <SentryRoutes>
                     <Route path="/" element={<Navigate to={correctPage()} replace />} />
-                    {showInteractiveDemo && (
-                        <Route path="/dev/interactive-demo" element={<PrivateRoute />}>
-                            <Route path="/dev/interactive-demo" element={<InteractiveDemo />} />
-                        </Route>
-                    )}
-                    <Route path="/:env/integrations" element={<PrivateRoute />}>
+                    <Route element={<PrivateRoute />}>
+                        {showInteractiveDemo && (
+                            <Route path="/dev/interactive-demo" element={<PrivateRoute />}>
+                                <Route path="/dev/interactive-demo" element={<InteractiveDemo />} />
+                            </Route>
+                        )}
                         <Route path="/:env/integrations" element={<IntegrationList />} />
-                    </Route>
-                    <Route path="/:env/integration/create" element={<PrivateRoute />}>
                         <Route path="/:env/integration/create" element={<CreateIntegration />} />
-                    </Route>
-                    <Route path="/:env/integration/:providerConfigKey" element={<PrivateRoute />}>
                         <Route path="/:env/integration/:providerConfigKey" element={<ShowIntegration />} />
-                    </Route>
-                    <Route path="/:env/connections" element={<PrivateRoute />}>
                         <Route path="/:env/connections" element={<ConnectionList />} />
-                    </Route>
-                    <Route path="/:env/connections/create" element={<PrivateRoute />}>
                         <Route path="/:env/connections/create" element={<ConnectionCreate />} />
-                    </Route>
-                    <Route path="/:env/connections/create/:providerConfigKey" element={<PrivateRoute />}>
                         <Route path="/:env/connections/create/:providerConfigKey" element={<ConnectionCreate />} />
-                    </Route>
-                    <Route path="/:env/connections/:providerConfigKey/:connectionId" element={<PrivateRoute />}>
                         <Route path="/:env/connections/:providerConfigKey/:connectionId" element={<Connection />} />
-                    </Route>
-                    <Route path="/:env/activity" element={<PrivateRoute />}>
                         <Route path="/:env/activity" element={<Activity />} />
-                    </Route>
-                    <Route path="/:env/project-settings" element={<PrivateRoute />}>
                         <Route path="/:env/project-settings" element={<ProjectSettings />} />
+                        {AUTH_ENABLED && (
+                            <>
+                                <Route path="/:env/account-settings" element={<AccountSettings />} />
+                                <Route path="/:env/user-settings" element={<UserSettings />} />
+                            </>
+                        )}
                     </Route>
                     <Route path="/auth-link" element={<AuthLink />} />
-                    {(isCloud() || isEnterprise() || isLocal()) && (
+                    {AUTH_ENABLED && (
                         <>
-                            <Route path="/:env/account-settings" element={<AccountSettings />} />
-                            <Route path="/:env/user-settings" element={<UserSettings />} />
                             <Route path="/signin" element={<Signin />} />
                             <Route path="/signup/:token" element={<InviteSignup />} />
                             <Route path="/forgot-password" element={<ForgotPassword />} />

packages/webapp/src/components/LeftNavBar.tsx
@@ -14,6 +14,7 @@ import {
 
 import { useStore } from '../store';
 import { isLocal, isCloud, isEnterprise } from '../utils/utils';
+import { useMeta } from '../hooks/useMeta';
 import { useSignout } from '../utils/user';
 
 export enum LeftNavBarItems {
@@ -36,49 +37,11 @@ const navActiveBg = 'bg-active-gray';
 const navHoverBg = 'hover:bg-hover-gray';
 
 export default function LeftNavBar(props: LeftNavBarProps) {
-    const [version, setVersion] = useState<string>('');
     const [showUserSettings, setShowUserSettings] = useState<boolean>(false);
     const navigate = useNavigate();
-
     const signout = useSignout();
-
-    const storedEnvs = useStore((state) => state.envs);
-    const [envs, setEnvs] = useState<{ name: string }[]>(storedEnvs);
+    const { meta } = useMeta();
     const showInteractiveDemo = useStore((state) => state.showInteractiveDemo);
-    const setStoredEnvs = useStore((state) => state.setEnvs);
-    const setBaseUrl = useStore((state) => state.setBaseUrl);
-    const setEmail = useStore((state) => state.setEmail);
-    const setDebugMode = useStore((state) => state.setDebugMode);
-
-    useEffect(() => {
-        fetch('/api/v1/meta')
-            .then((res) => {
-                if (res.status === 401) {
-                    return signout();
-                }
-                return res.json();
-            })
-            .then((data) => {
-                if (!data) {
-                    return;
-                }
-
-                if (JSON.stringify(data.environments) !== JSON.stringify(envs)) {
-                    setEnvs(data.environments);
-                    setStoredEnvs(data.environments);
-                    setBaseUrl(data.baseUrl);
-                }
-                if (data.email !== email) {
-                    setEmail(data.email);
-                }
-                setDebugMode(data.debugMode === true);
-                setVersion(data.version);
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
 
     useEffect(() => {
         const closeUserSettings = (e: MouseEvent) => {
@@ -128,6 +91,10 @@ export default function LeftNavBar(props: LeftNavBarProps) {
         window.history.pushState({}, '', newPath);
     };
 
+    if (!meta) {
+        return null;
+    }
+
     return (
         <div className="bg-pure-black">
             <div className="flex-1 ml-3 pr-4 h-full border-r border-border-gray flex flex-col w-60 bg-pure-black z-20 justify-between">
@@ -135,14 +102,14 @@ export default function LeftNavBar(props: LeftNavBarProps) {
                     <div className="flex items-center mb-8">
                         <img className="h-6" src="/logo-dark.svg" alt="Nango" />
                         <img className="mt-1 h-5 ml-1" src="/logo-text.svg" alt="Nango" />
-                        <span className="ml-3 text-xs text-black mono">{version}</span>
+                        <span className="ml-3 text-xs text-black mono">{meta.version}</span>
                     </div>
-                    {envs.length === 0 && (
+                    {meta.environments.length === 0 && (
                         <div className="mb-8">
                             <select className="border-border-gray bg-active-gray text-text-light-gray block w-full appearance-none rounded-md border px-3 py-2 shadow-sm active:outline-none focus:outline-none active:border-white focus:border-white"></select>
                         </div>
                     )}
-                    {envs.length > 0 && (
+                    {meta.environments.length > 0 && (
                         <div className="mb-6">
                             <select
                                 id="environment"
@@ -151,7 +118,7 @@ export default function LeftNavBar(props: LeftNavBarProps) {
                                 onChange={handleEnvChange}
                                 value={env}
                             >
-                                {envs.map((env) => (
+                                {meta.environments.map((env) => (
                                     <option key={env.name} value={env.name}>
                                         {env.name.slice(0, 1).toUpperCase() + env.name.slice(1)}
                                     </option>
@@ -223,7 +190,7 @@ export default function LeftNavBar(props: LeftNavBarProps) {
                                 <div className="flex items-center justify-center w-6 h-6 rounded-full bg-transparent text-sm border border-gray-400 text-gray-400 mr-3">
                                     {email.slice(0, 1).toUpperCase()}
                                 </div>
-                                <span className="items-center w-32 text-center text-gray-400 justify-center text-left text-sm truncate">{email}</span>
+                                <span className="items-center w-32 text-gray-400 justify-center text-left text-sm truncate">{email}</span>
                             </div>
                             <EllipsisHorizontalIcon className="flex h-5 w-5 ml-3 text-gray-400 cursor-pointer" />
                             {(isCloud() || isEnterprise() || isLocal()) && showUserSettings && (

packages/webapp/src/components/PrivateRoute.tsx
@@ -1,9 +1,37 @@
 import { Outlet, Navigate } from 'react-router-dom';
-import { isCloud, isEnterprise } from '../utils/utils';
-import { isSignedIn } from '../utils/user';
+import { useMeta } from '../hooks/useMeta';
+import { useEffect } from 'react';
+import { useStore } from '../store';
 
-const PrivateRoute = (_: any) => {
-    return <>{(!isCloud() && !isEnterprise()) || isSignedIn() ? <Outlet /> : <Navigate to="/signin" replace />}</>;
+const PrivateRoute: React.FC = () => {
+    const { meta, error, loading } = useMeta();
+
+    const setStoredEnvs = useStore((state) => state.setEnvs);
+    const setBaseUrl = useStore((state) => state.setBaseUrl);
+    const setEmail = useStore((state) => state.setEmail);
+    const setDebugMode = useStore((state) => state.setDebugMode);
+
+    useEffect(() => {
+        if (!meta || error) {
+            return;
+        }
+
+        setStoredEnvs(meta.environments);
+        setBaseUrl(meta.baseUrl);
+        setEmail(meta.email);
+        setDebugMode(meta.debugMode);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [meta, error]);
+
+    if (loading) {
+        return null;
+    }
+
+    if (error) {
+        return <Navigate to="/signin" replace />;
+    }
+
+    return <Outlet />;
 };
 
 export default PrivateRoute;

packages/webapp/src/hooks/useEnvironment.tsx
@@ -1,0 +1,15 @@
+import useSWR from 'swr';
+import { Account } from '../types';
+import { swrFetcher } from '../utils/api';
+
+export function useEnvironment() {
+    const { data, error } = useSWR<{ account: Account }>('/api/v1/environment', swrFetcher);
+
+    const loading = !data && !error;
+
+    return {
+        loading,
+        error,
+        environment: data?.account
+    };
+}

packages/webapp/src/hooks/useMeta.tsx
@@ -1,0 +1,15 @@
+import useSWR from 'swr';
+import type { GetMeta } from '@nangohq/server';
+import { swrFetcher } from '../utils/api';
+
+export function useMeta() {
+    const { data, error } = useSWR<GetMeta>('/api/v1/meta', swrFetcher);
+
+    const loading = !data && !error;
+
+    return {
+        loading,
+        error,
+        meta: data
+    };
+}

commit efe4e6ba707a043160ddf3b4fd2c9902b6fbd8b3

fix(activity): logs order (#1861)
## Describe your changes

Logs order has been broken after #1844 I forgot about the reverse trick.
I'll note to improve this behavior with a long term fix.

- Reup logs order

packages/webapp/src/pages/Activity.tsx
@@ -98,6 +98,7 @@ export default function Activity() {
     const env = useStore((state) => state.cookieValue);
 
     const isInitialMount = useRef(true);
+    const [msgs, setMsgs] = useState<ActivityMessageResponse>([]);
     const [activityRefs, setActivityRefs] = useState<Record<number, React.RefObject<HTMLTableRowElement>>>({});
 
     const [offset, setOffset] = useState(queryParams.offset || 0);
@@ -137,13 +138,23 @@ export default function Activity() {
         }
     }, [activities, error]);
 
-    const { data: msgs, error: logActivitiesError } = useSWR<ActivityMessageResponse>(
+    const { data: rawMsgs, error: logActivitiesError } = useSWR<ActivityMessageResponse>(
         () => (logIds.length > 0 ? `/api/v1/activity-messages?logIds=${logIds.join(',')}` : null),
         swrFetcher,
         {
             refreshInterval: expandedRow !== -1 ? 5000 : 60000
         }
     );
+    useEffect(() => {
+        if (!rawMsgs) {
+            return;
+        }
+        const tmp: ActivityMessageResponse = {};
+        for (const [key, list] of Object.entries(rawMsgs)) {
+            tmp[key as unknown as number] = list.reverse();
+        }
+        setMsgs(tmp);
+    }, [rawMsgs]);
 
     const { data: activityFilters } = useSWR<Record<string, any>>(`/api/v1/activity-filters?env=${env}`, swrFetcher, {
         refreshInterval: 60000