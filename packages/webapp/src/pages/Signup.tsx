59d64f2ffa39decfef316f0c46e8ae4d7cba454d
Khaliq <khaliq@nango.dev>
2024-03-20 20:06:45 +0200

    [NAN-578] Implement Google Social Login/Signup (#1872)
    ## Describe your changes
    Adds in google auth using workos
    
    
    ![image](https://github.com/NangoHQ/nango/assets/1724137/8deeef6c-bf77-4677-a62e-5b929a7ce331)
    
    
    ## Issue ticket number and link
    NAN-578
    
    ## Checklist before requesting a review (skip if just adding/editing
    APIs & templates)
    - [ ] I added tests, otherwise the reason is: 
    - [ ] I added observability, otherwise the reason is:
    - [ ] I added analytics, otherwise the reason is:

--- packages/webapp/src/pages/Signup.tsx
+++ packages/webapp/src/pages/Signup.tsx
@@ -2,9 +2,12 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 import { useAnalyticsTrack } from '../utils/analytics';
+import { MANAGED_AUTH_ENABLED } from '../utils/utils';
 import { useSignupAPI } from '../utils/api';
-import { useSignin, User } from '../utils/user';
+import type { User } from '../utils/user';
+import { useSignin } from '../utils/user';
 import DefaultLayout from '../layout/DefaultLayout';
+import GoogleButton from '../components/ui/button/Auth/Google';
 
 export default function Signup() {
     const [serverErrorMessage, setServerErrorMessage] = useState('');
@@ -45,14 +48,11 @@ export default function Signup() {
     return (
         <>
             <DefaultLayout>
-                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-md">
-                    <div className="bg-bg-dark-gray py-8 px-4 shadow sm:rounded-lg sm:px-10">
-                        <h2 className="mt-2 text-center text-3xl font-semibold tracking-tight text-white">Sign up</h2>
+                <div className="flex flex-col justify-center">
+                    <div className="flex flex-col justify-center w-80 mx-4">
+                        <h2 className="mt-4 text-center text-[20px] text-white">Sign up to Nango</h2>
                         <form className="mt-6 space-y-6" onSubmit={handleSubmit}>
                             <div>
-                                <label htmlFor="name" className="text-text-light-gray block text-sm font-semibold">
-                                    Name
-                                </label>
                                 <div className="mt-1">
                                     <input
                                         id="name"
@@ -61,42 +61,39 @@ export default function Signup() {
                                         autoComplete="name"
                                         required
                                         minLength={1}
+                                        placeholder="Name"
                                         maxLength={100}
-                                        className="border-border-gray bg-bg-black text-text-light-gray focus:border-blue focus:ring-blue block h-11 w-full appearance-none rounded-md border px-3 py-2 text-base placeholder-gray-400 shadow-sm focus:outline-none"
+                                        className="border-border-gray bg-dark-600 placeholder-dark-500 text-text-light-gray block h-11 w-full appearance-none rounded-md border px-3 py-2 text-[14px] placeholder-gray-400 shadow-sm focus:outline-none"
                                     />
                                 </div>
                             </div>
 
                             <div>
-                                <label htmlFor="email" className="text-text-light-gray block text-sm font-semibold">
-                                    Email
-                                </label>
                                 <div className="mt-1">
                                     <input
                                         id="email"
                                         name="email"
                                         type="email"
                                         autoComplete="email"
+                                        placeholder="Email"
                                         required
-                                        className="border-border-gray bg-bg-black text-text-light-gray focus:ring-blue block h-11 w-full appearance-none rounded-md border px-3 py-2 text-base placeholder-gray-400 shadow-sm focus:border-blue-500 focus:outline-none"
+                                        className="border-border-gray bg-dark-600 placeholder-dark-500 text-text-light-gray block h-11 w-full appearance-none rounded-md border px-3 py-2 text-[14px] placeholder-gray-400 shadow-sm focus:outline-none"
                                     />
                                 </div>
                             </div>
 
                             <div>
-                                <label htmlFor="password" className="text-text-light-gray block text-sm font-semibold">
-                                    Password
-                                </label>
                                 <div className="mt-1">
                                     <input
                                         id="password"
                                         name="password"
                                         type="password"
                                         autoComplete="new-password"
+                                        placeholder="Password"
                                         required
                                         minLength={8}
                                         maxLength={50}
-                                        className="border-border-gray bg-bg-black text-text-light-gray block h-11 w-full appearance-none rounded-md border px-3 py-2 text-base placeholder-gray-400 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500"
+                                        className="border-border-gray bg-dark-600 placeholder-dark-500 text-text-light-gray block h-11 w-full appearance-none rounded-md border px-3 py-2 text-[14px] placeholder-gray-400 shadow-sm focus:outline-none"
                                     />
                                 </div>
                             </div>
@@ -104,38 +101,45 @@ export default function Signup() {
                             <div className="grid">
                                 <button
                                     type="submit"
-                                    className="border-border-blue bg-bg-dark-blue active:ring-border-blue mt-4 flex h-12 place-self-center rounded-md border px-4 pt-3 text-base font-semibold text-blue-500 shadow-sm hover:border-2 active:ring-2 active:ring-offset-2"
+                                    className="bg-white flex h-11 justify-center rounded-md border px-4 pt-3 text-[14px] text-black shadow hover:border-2 active:ring-2 active:ring-offset-2"
                                 >
                                     Sign up
                                 </button>
                                 {serverErrorMessage && <p className="mt-6 place-self-center text-sm text-red-600">{serverErrorMessage}</p>}
                             </div>
                         </form>
+                        {MANAGED_AUTH_ENABLED && (
+                            <>
+                                <div className="flex items-center justify-center my-4 text-xs">
+                                    <div className="border-t border-gray-600 flex-grow mr-7"></div>
+                                    <span className="text-dark-500">or continue with</span>
+                                    <div className="border-t border-gray-600 flex-grow ml-7"></div>
+                                </div>
+                                <GoogleButton text="Sign up with Google" setServerErrorMessage={setServerErrorMessage} />
+                            </>
+                        )}
                     </div>
-                    <div className="grid">
-                        <div className="mt-4 flex place-self-center text-sm">
-                            <p className="text-text-light-gray">Already have an account?</p>
-                            <Link to="/signin" className="text-text-blue hover:text-text-light-blue ml-1">
-                                Sign in
+                    <div className="grid text-xs">
+                        <div className="mt-7 flex place-self-center">
+                            <p className="text-dark-500">Already have an account?</p>
+                            <Link to="/signin" className="text-white ml-1">
+                                Sign in.
                             </Link>
                         </div>
                     </div>
-                    <div className="grid">
-                        <div className="mt-4 flex place-self-center text-sm">
-                            <p className="text-text-light-gray">By signing up, you agree to our</p>
-                            <a href="https://www.nango.dev/terms" target="_blank" rel="noreferrer" className="text-text-blue hover:text-text-light-blue ml-1">
-                                Terms of Service
-                            </a>
-                            <p className="text-text-light-gray ml-1">and</p>
-                            <a
-                                href="https://www.nango.dev/privacy-policy"
-                                target="_blank"
-                                rel="noreferrer"
-                                className="text-text-blue hover:text-text-light-blue ml-1"
-                            >
-                                Privacy Policy
-                            </a>
-                            <p className="text-text-light-gray">.</p>
+                    <div className="grid w-full">
+                        <div className="mt-8 flex text-xs">
+                            <p className="text-dark-500">
+                                By signing in, you agree to our
+                                <a href="https://www.nango.dev/terms" target="_blank" rel="noreferrer" className="text-white ml-1">
+                                    Terms of Service
+                                </a>
+                                <span className="text-dark-500 ml-1">and</span>
+                                <a href="https://www.nango.dev/privacy-policy" target="_blank" rel="noreferrer" className="text-white ml-1">
+                                    Privacy Policy
+                                </a>
+                                <span className="text-dark-500">.</span>
+                            </p>
                         </div>
                     </div>
                 </div>
