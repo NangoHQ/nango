bf74650b633e035d0106ea51f2f6fd9b3a5975fa
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-03-21 09:30:05 +0100

    fix(shared): proper ts rootDir (#1887)
    ## Describe your changes
    
    Fixes NAN-617
    
    - rootDir was not pointing to lib
    It currently output everything to dist/lib which is inconvenient but
    also makes the paths unreliable since it changes from ts to js
    
    - Fix package.json loading (and cache it)
    This package.json is loaded on every call to `/meta`. 
    And it was spamming errors in tests

--- packages/shared/lib/utils/utils.ts
+++ packages/shared/lib/utils/utils.ts
@@ -163,8 +163,8 @@ export function isValidHttpUrl(str: string) {
     }
 }
 
-export function dirname() {
-    return path.dirname(fileURLToPath(import.meta.url));
+export function dirname(thisFile?: string) {
+    return path.dirname(fileURLToPath(thisFile || import.meta.url));
 }
 
 export function parseTokenExpirationDate(expirationDate: any): Date {
@@ -423,9 +423,15 @@ export function getConnectionConfig(queryParams: any): Record<string, string> {
     return Object.fromEntries(arr) as Record<string, string>;
 }
 
+let packageJsonCache: PackageJson | undefined;
 export function packageJsonFile(): PackageJson {
-    const localPath = process.env['SERVER_RUN_MODE'] === 'DOCKERIZED' ? 'packages/shared/package.json' : '../shared/package.json';
-    return JSON.parse(readFileSync(resolve(process.cwd(), localPath)).toString('utf-8'));
+    if (packageJsonCache) {
+        return packageJsonCache;
+    }
+
+    const localPath = '../../package.json';
+    packageJsonCache = JSON.parse(readFileSync(resolve(dirname(), localPath)).toString('utf-8')) as PackageJson;
+    return packageJsonCache;
 }
 
 export function safeStringify(obj: any): string {
