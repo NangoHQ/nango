22dba587c0532fb80121e59c91d5f3760b367755
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-03-25 15:33:33 +0100

    fix(ui): improve cache for session, integrations

--- packages/server/lib/index.ts
+++ packages/server/lib/index.ts
@@ -1,2 +1,3 @@
 export type { GetMeta } from './controllers/environment.controller.js';
 export type { GetUser } from './controllers/user.controller.js';
+export type { ListIntegration, Integration } from './controllers/config.controller.js';
