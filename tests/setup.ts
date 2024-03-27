4614bb6432a97d7d1166b572caf973f0ba328910
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-03-27 20:01:17 +0100

    review

--- tests/setup.ts
+++ tests/setup.ts
@@ -4,11 +4,14 @@ import { Wait, PostgreSqlContainer } from 'testcontainers';
 const containers: StartedTestContainer[] = [];
 
 async function setupPostgres() {
+    const dbName = 'postgres';
+    const user = 'postgres';
+    const password = 'nango_test';
     const container = new PostgreSqlContainer('postgres:15.5-alpine');
     const pg = await container
-        .withDatabase('postgres')
-        .withUsername('postgres')
-        .withPassword('nango_test')
+        .withDatabase(dbName)
+        .withUsername(user)
+        .withPassword(password)
         .withExposedPorts(5432)
         .withName('pg-test')
         .withWaitStrategy(Wait.forLogMessage('database system is ready to accept connections'))
@@ -17,13 +20,11 @@ async function setupPostgres() {
     containers.push(pg);
     const port = pg.getMappedPort(5432);
 
-    const testCred = 'nango_test';
-
-    process.env['NANGO_DB_PASSWORD'] = testCred;
+    process.env['NANGO_DB_PASSWORD'] = password;
     process.env['NANGO_DB_HOST'] = 'localhost';
-    process.env['NANGO_DB_USER'] = 'postgres';
+    process.env['NANGO_DB_USER'] = user;
     process.env['NANGO_DB_PORT'] = port.toString();
-    process.env['NANGO_DB_NAME'] = 'postgres';
+    process.env['NANGO_DB_NAME'] = dbName;
     process.env['NANGO_DB_MIGRATION_FOLDER'] = './packages/shared/lib/db/migrations';
     process.env['TELEMETRY'] = 'false';
 }
