edff2d88239eaeb6dc81bf3d13921abceec0f89c
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-03-27 18:00:00 +0100

    fix(env): pin Postgres

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,18 +1,21 @@
-import { GenericContainer, StartedTestContainer, TestContainer, Wait } from 'testcontainers';
+import type { StartedTestContainer } from 'testcontainers';
+import { Wait, PostgreSqlContainer } from 'testcontainers';
 
-let startedContainer: StartedTestContainer;
+const containers: StartedTestContainer[] = [];
 
-export const setup = async () => {
-    const container: TestContainer = new GenericContainer('postgres');
-    startedContainer = await container
-        .withEnvironment({ POSTGRES_USER: 'postgres' })
-        .withEnvironment({ POSTGRES_PASSWORD: 'nango_test' })
-        .withEnvironment({ POSTGRES_DB: 'postgres' })
+async function setupPostgres() {
+    const container = new PostgreSqlContainer('postgres:15.5-alpine');
+    const pg = await container
+        .withDatabase('postgres')
+        .withUsername('postgres')
+        .withPassword('nango_test')
         .withExposedPorts(5432)
+        .withName('pg-test')
         .withWaitStrategy(Wait.forLogMessage('database system is ready to accept connections'))
         .start();
 
-    const port = startedContainer.getMappedPort(5432);
+    containers.push(pg);
+    const port = pg.getMappedPort(5432);
 
     const testCred = 'nango_test';
 
@@ -23,10 +26,20 @@ export const setup = async () => {
     process.env['NANGO_DB_NAME'] = 'postgres';
     process.env['NANGO_DB_MIGRATION_FOLDER'] = './packages/shared/lib/db/migrations';
     process.env['TELEMETRY'] = 'false';
-};
+}
+
+export async function setup() {
+    await Promise.all([setupPostgres()]);
+}
 
 export const teardown = async () => {
-    if (startedContainer) {
-        startedContainer.stop();
-    }
+    await Promise.all(
+        containers.map(async (container) => {
+            try {
+                await container.stop();
+            } catch (err) {
+                console.error(err);
+            }
+        })
+    );
 };
