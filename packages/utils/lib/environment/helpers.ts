4b16d7a6ab7a37272c7ea5cc798fc93665fd4625
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-03-28 18:21:45 +0100

    tmp

--- packages/utils/lib/environment/helpers.ts
+++ packages/utils/lib/environment/helpers.ts
@@ -1,52 +1,70 @@
 import { z } from 'zod';
 
-export function envErrorToString(issues: z.ZodIssue[]) {
-    return issues
-        .map((issue) => {
-            return `${issue.path.join('')} (${issue.code} ${issue.message})`;
-        })
-        .join(', ');
-}
+const ENVS = {
+    // Node ecosystem
+    NODE_ENV: z.enum(['production', 'development', 'test']).default('development'),
+    CI: z.coerce.boolean().default(false),
+    VITEST: z.coerce.boolean().default(false),
+    TZ: z.string().default('UTC'),
 
-export function initGlobalEnv() {
-    const schema = z.object({
-        // Database
-        NANGO_DATABASE_URL: z.string().url().optional(),
-        NANGO_DB_HOST: z.string().optional().default('localhost'),
-        NANGO_DB_PORT: z.coerce.number().optional().default(5432),
-        NANGO_DB_USER: z.string().optional().default('nango'),
-        NANGO_DB_NAME: z.string().optional().default('nango'),
-        NANGO_DB_PASSWORD: z.string().optional().default('nango'),
-        NANGO_DB_SSL: z.coerce.boolean().default(false),
-        NANGO_ENCRYPTION_KEY: z.string().optional(),
+    // Postgres
+    NANGO_DATABASE_URL: z.string().url().optional(),
+    NANGO_DB_HOST: z.string().optional().default('localhost'),
+    NANGO_DB_PORT: z.coerce.number().optional().default(5432),
+    NANGO_DB_USER: z.string().optional().default('nango'),
+    NANGO_DB_NAME: z.string().optional().default('nango'),
+    NANGO_DB_PASSWORD: z.string().optional().default('nango'),
+    NANGO_DB_SSL: z.coerce.boolean().default(false),
+    NANGO_DB_CLIENT: z.string().optional(),
+    NANGO_ENCRYPTION_KEY: z.string().optional(),
+    NANGO_DB_MIGRATION_FOLDER: z.string().optional(),
 
-        // env
-        NODE_ENV: z.enum(['production', 'development', 'test']).default('development'),
-        CI: z.coerce.boolean().default(false),
-        VITEST: z.coerce.boolean().default(false),
+    // Redis
+    NANGO_REDIS_URL: z.string().url().optional(),
 
-        // demo
-        DEFAULT_GITHUB_CLIENT_ID: z.string().optional(),
-        DEFAULT_GITHUB_CLIENT_SECRET: z.string().optional(),
+    // Render
+    RENDER_API_KEY: z.string().optional(),
+    IS_RENDER: z.coerce.boolean().default(false),
 
-        // auth
-        WORKOS_API_KEY: z.string().optional(),
-        WORKOS_CLIENT_ID: z.string().optional(),
-        NANGO_DASHBOARD_USERNAME: z.string().optional(),
-        NANGO_DASHBOARD_PASSWORD: z.string().optional(),
+    // Slack
+    NANGO_ADMIN_CONNECTION_ID: z.string().optional(),
+    NANGO_SLACK_INTEGRATION_KEY: z.string().optional(),
+    NANGO_ADMIN_UUID: z.string().uuid().optional(),
 
-        // aws
-        AWS_REGION: z.string().optional(),
-        AWS_BUCKET_NAME: z.string().optional(),
+    // Sentry
+    SENTRY_DNS: z.string().url().optional(),
 
-        // others
-        SERVER_RUN_MODE: z.enum(['DOCKERIZED', '']).optional(),
-        NANGO_CLOUD: z.coerce.boolean().optional().default(false),
-        NANGO_ENTERPRISE: z.coerce.boolean().optional().default(false),
-        NANGO_SERVER_URL: z.string().url().optional(),
-        NANGO_TELEMETRY_SDK: z.coerce.boolean().default(false).optional(),
-        LOG_LEVEL: z.enum(['info', 'debug', 'warn', 'error']).optional().default('info')
-    });
+    // Temporal
+    TEMPORAL_NAMESPACE: z.string().optional(),
+    TEMPORAL_ADDRESS: z.string().optional(),
+    TEMPORAL_WORKER_MAX_CONCURRENCY: z.coerce.number().default(500),
+
+    // ----- Others
+    SERVER_RUN_MODE: z.enum(['DOCKERIZED', '']).optional(),
+    NANGO_CLOUD: z.coerce.boolean().optional().default(false),
+    NANGO_ENTERPRISE: z.coerce.boolean().optional().default(false),
+    NANGO_TELEMETRY_SDK: z.coerce.boolean().default(false).optional(),
+    NANGO_ADMIN_KEY: z.string().optional(),
+    NANGO_INTEGRATIONS_FULL_PATH: z.string().optional(),
+    TELEMETRY: z.coerce.boolean().default(true),
+    LOG_LEVEL: z.enum(['info', 'debug', 'warn', 'error']).optional().default('info')
+};
+
+type EnvKeys = keyof typeof ENVS;
+
+/**
+ * Parse and type process.env
+ */
+export function getEnvs<const TKey extends EnvKeys[]>({ required }: { required: TKey }) {
+    const schema = z.object({ ...ENVS }).required(
+        required.reduce(
+            (prev, curr) => {
+                prev[curr as TKey[0]] = true;
+                return prev;
+            },
+            {} as Record<TKey[0], true>
+        )
+    );
 
     const res = schema.safeParse(process.env);
     if (!res.success) {
@@ -55,3 +73,15 @@ export function initGlobalEnv() {
 
     return res.data;
 }
+
+export function envErrorToString(issues: z.ZodIssue[]) {
+    return issues
+        .map((issue) => {
+            return `${issue.path.join('')} (${issue.code} ${issue.message})`;
+        })
+        .join(', ');
+}
+
+const envs = getEnvs({ required: ['NANGO_DATABASE_URL'] });
+envs.NANGO_ENCRYPTION_KEY;
+envs.NANGO_DATABASE_URL;
