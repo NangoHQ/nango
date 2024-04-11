dd5297eb663a195a5e75f95c0fad3716a8fbeede
Khaliq <khaliqgant@gmail.com>
2024-04-11 16:50:28 +0200

    client id and secret can be null

--- packages/shared/lib/models/Provider.ts
+++ packages/shared/lib/models/Provider.ts
@@ -8,8 +8,8 @@ export interface Config extends TimestampsAndDeleted {
     id?: number;
     unique_key: string;
     provider: string;
-    oauth_client_id: string;
-    oauth_client_secret: string;
+    oauth_client_id: string | null;
+    oauth_client_secret: string | null;
     oauth_scopes?: string;
     environment_id: number;
     oauth_client_secret_iv?: string | null;
