29e50addfb3420c61594fe1447d67ba25bbe0dd8
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-04-18 11:27:52 +0200

    Revert "try with 0.0.0.0"
    This reverts commit 572d76348c0760126912b71f73b017e1c2e36445.

--- packages/runner/lib/app.ts
+++ packages/runner/lib/app.ts
@@ -1,17 +1,14 @@
 import './tracer.js';
-import type { AddressInfo } from 'node:net';
 import { server } from './server.js';
 import { getLogger } from '@nangohq/utils';
 
 const logger = getLogger('Runner');
-const SERVER_RUN_MODE = process.env['SERVER_RUN_MODE'];
 
 try {
     const port = parseInt(process.argv[2] || '') || 3006;
     const id = process.argv[3] || process.env['RUNNER_ID'] || 'unknown-id';
-    const tmp = server.listen(port, SERVER_RUN_MODE === 'DOCKERIZED' ? '0.0.0.0' : 'localhost', () => {
-        const addr = tmp.address() as AddressInfo;
-        logger.info(`🚀 '${id}' ready at ${addr?.address}:${addr?.port}`);
+    server.listen(port, () => {
+        logger.info(`🚀 '${id}' ready at http://localhost:${port}`);
     });
 } catch (err) {
     logger.error(`Unable to start runner: ${JSON.stringify(err)}`);
