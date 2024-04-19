4eb27e9b443fb172be2db2f09b8db02e458a4bc5
Thomas Bonnin <233326+TBonnin@users.noreply.github.com>
2024-04-19 09:40:12 -0400

    fetch new records and compare with legacy ones (still returning legacy) (#2015)
    ## Describe your changes
    Next step in the records migration: reading from new db. First, we read
    both legacy and new db, comparing the output and logging discrepancy,
    still returning the legacy output.
    
    Fetching new records is behind a feature flag so it can easily be
    enabled/disabled
    
    Once we have confirmed there is no discrepancy, I will make a PR to
    return the output of new db

--- packages/server/lib/controllers/sync.controller.ts
+++ packages/server/lib/controllers/sync.controller.ts
@@ -42,10 +42,13 @@ import {
     setFrequency,
     getEnvironmentAndAccountId,
     getSyncAndActionConfigsBySyncNameAndConfigId,
-    createActivityLogMessage
+    createActivityLogMessage,
+    featureFlags
 } from '@nangohq/shared';
-import { isErr, isOk } from '@nangohq/utils';
+import { isErr, isOk, getLogger } from '@nangohq/utils';
 import { records as recordsService } from '@nangohq/records';
+
+const logger = getLogger('sync.controller');
 
 class SyncController {
     public async deploySync(req: Request, res: Response, next: NextFunction) {
@@ -176,7 +179,11 @@ class SyncController {
                 return;
             }
 
-            const { success, error, response } = await syncDataService.getAllDataRecords(
+            const {
+                success,
+                error: legacyError,
+                response
+            } = await syncDataService.getAllDataRecords(
                 connectionId,
                 providerConfigKey,
                 environmentId,
@@ -188,11 +195,61 @@ class SyncController {
             );
 
             if (!success || !response) {
-                errorManager.errResFromNangoErr(res, error);
-
+                errorManager.errResFromNangoErr(res, legacyError);
                 return;
             }
 
+            const shouldFetchNewRecords = await featureFlags.isEnabled('new-records-fetch', 'global', false);
+            if (shouldFetchNewRecords) {
+                const { error, response: connection } = await connectionService.getConnection(connectionId, providerConfigKey, environmentId);
+
+                if (error || !connection) {
+                    errorManager.errResFromNangoErr(res, error);
+                    return;
+                }
+
+                const newResponse = await recordsService.getRecords({
+                    connectionId: connection.id as number,
+                    model: model as string,
+                    modifiedAfter: (delta || modified_after) as string,
+                    limit: limit as string,
+                    filter: filter as LastAction,
+                    cursor: cursor as string
+                });
+
+                if (isErr(newResponse)) {
+                    logger.error('Error fetching records from new records service', newResponse.err);
+                } else {
+                    // Compare legacy and new records
+                    // dates are set by the database and may differ
+                    // so we need to remove them before comparing
+                    const oldRecords = response.records.map((r) => {
+                        const { _nango_metadata: _ignored, ...rest } = r;
+                        return rest;
+                    });
+                    const newRecords = newResponse.res.records.map((r) => {
+                        const { _nango_metadata: _ignored, ...rest } = r;
+                        return rest;
+                    });
+                    const oldRecordsJson = JSON.stringify(oldRecords);
+                    const newRecordsJson = JSON.stringify(newRecords);
+                    if (oldRecordsJson !== newRecordsJson) {
+                        logger.error(`[RECORDS MIGRATION] Differences between legacy and new records: ${oldRecordsJson} <<<>>> ${newRecordsJson}`, {
+                            connectionId,
+                            model,
+                            modifiedAfter: delta || modified_after,
+                            limit,
+                            filter,
+                            cursor
+                        });
+                    } else {
+                        logger.info('[RECORDS MIGRATION] No differences between legacy and new records');
+                    }
+                }
+
+                // TODO: enable track fetch once the call to legacy getAllDataRecords is removed
+                // await trackFetch(connection.id as number);
+            }
             res.send(response);
         } catch (e) {
             next(e);
