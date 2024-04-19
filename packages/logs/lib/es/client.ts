e242b5dc939766df30a2a234f32e02881d30df4c
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-04-19 13:10:35 +0200

    more

--- packages/logs/lib/es/client.ts
+++ packages/logs/lib/es/client.ts
@@ -3,7 +3,7 @@ import { envs } from '../env.js';
 
 export const client = new Client({
     // Because it's loaded in CLI and runner we can't required any envs
-    nodes: envs.NANGO_LOGS_ES_URL! || 'http://localhost:1/fake',
+    nodes: envs.NANGO_LOGS_ES_URL! || 'http://localhost:0',
     requestTimeout: 5000,
     maxRetries: 1,
     auth: { username: envs.NANGO_LOGS_ES_USER!, password: envs.NANGO_LOGS_ES_PWD! },
