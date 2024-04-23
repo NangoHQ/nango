03af7d5784f1dea1716e154e2ece32bc0952a625
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-04-23 17:53:29 +0200

    I love linter <3

--- packages/logs/lib/es/client.ts
+++ packages/logs/lib/es/client.ts
@@ -3,9 +3,9 @@ import { envs } from '../env.js';
 
 export const client = new Client({
     // Because it's loaded in CLI and runner we can't required any envs
-    nodes: envs.NANGO_LOGS_ES_URL! || 'http://localhost:0',
+    nodes: envs.NANGO_LOGS_ES_URL || 'http://localhost:0',
     requestTimeout: 5000,
     maxRetries: 1,
-    auth: { username: envs.NANGO_LOGS_ES_USER!, password: envs.NANGO_LOGS_ES_PWD! },
+    auth: { username: envs.NANGO_LOGS_ES_USER, password: envs.NANGO_LOGS_ES_PWD },
     tls: { rejectUnauthorized: false }
 });
