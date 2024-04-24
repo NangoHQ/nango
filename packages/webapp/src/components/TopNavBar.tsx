ae170eadbb630d2aec142ba5e6116960a2d6db55
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-04-24 14:36:54 +0200

    feat: hn demo (#2041)
    ## Describe your changes
    
    fixes NAN-788
    
    - Introduce a temporary frontend endpoint to automatically create an
    account and redirect to the interactive demo.
    It detects if we are connected to avoid creating new accounts for
    nothing otherwise it will create an account with a random id and email
    `demo-<id>@example.com`.
    - Banner is displayed when connected to a demo account 
    - Bonus: fix the broken script to compile
    
    ---
    
    <img width="1443" alt="Screenshot 2024-04-24 at 12 38 03"
    src="https://github.com/NangoHQ/nango/assets/1637651/214c6d9f-9277-4a2d-ac0d-9da800f68d41">

--- packages/webapp/src/components/TopNavBar.tsx
+++ packages/webapp/src/components/TopNavBar.tsx
@@ -1,9 +1,34 @@
 import { ChatBubbleBottomCenterIcon, ArrowTopRightOnSquareIcon } from '@heroicons/react/24/outline';
+import { useStore } from '../store';
+import { useMemo } from 'react';
+import { useSignout } from '../utils/user';
+import Info from './ui/Info';
 
 export default function NavBar() {
+    const signout = useSignout();
+    const email = useStore((state) => state.email);
+    const isHNDemo = useMemo(() => {
+        return Boolean(email.match(/demo-[a-z0-9]+@example.com/));
+    }, [email]);
+
+    const onCreateAccount = () => {
+        signout();
+    };
+
     return (
         <div className="w-full fixed bg-pure-black z-50 left-[15.8rem]">
-            <div className="flex justify-end border-b border-border-gray py-3 mr-[15.8rem]">
+            <div className="flex justify-between border-b border-border-gray py-3 mr-[15.8rem]">
+                <div className="text-white px-6 text-sm">
+                    {isHNDemo && (
+                        <Info padding={'px-3 py-1.5'} size={15}>
+                            This is a test account. Click{' '}
+                            <button className="font-bold" onClick={onCreateAccount}>
+                                here
+                            </button>{' '}
+                            to create a real account.
+                        </Info>
+                    )}
+                </div>
                 <div className="flex items-center pr-6">
                     <a
                         href="https://nango.dev/slack"
