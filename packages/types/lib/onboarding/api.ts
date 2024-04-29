b304e5787994d2807e1a75f264b2d85aa265e524
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-04-29 16:59:47 +0200

    review

--- packages/types/lib/onboarding/api.ts
+++ packages/types/lib/onboarding/api.ts
@@ -1,9 +1,9 @@
-import type { Endpoint } from '../api';
+import type { ApiError, Endpoint } from '../api';
 import type { NangoRecord } from '../record/api';
 
 export type GetOnboardingStatus = Endpoint<{
     Params: { foo: 'bar' };
-    Error: { error: { code: 'onboarding_dev_only' | 'no_onboarding' | 'failed_to_get_records' | 'invalid_query_params' } };
+    Error: ApiError<'onboarding_dev_only'> | ApiError<'no_onboarding'> | ApiError<'failed_to_get_records'> | ApiError<'invalid_query_params'>;
     Success: {
         id: number;
         progress: number;
