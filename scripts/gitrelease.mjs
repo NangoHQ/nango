c831f5fe7426aa7a9dd2154aaddc3a4638d88814
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-05-06 11:57:28 +0200

    test

---
+++ scripts/gitrelease.mjs
@@ -0,0 +1,26 @@
+#!/usr/bin/env zx
+import 'zx/globals';
+
+const { GITHUB_TOKEN } = process.env;
+const nextVersion = process.argv[3];
+const branch = process.argv[4] || 'master';
+const nextTag = `v${nextVersion}`;
+
+const releaseMessage = `chore(release): ${nextVersion} [skip ci]`;
+await $`git add -A package.json package-lock.json packages/**/package.json `;
+await $`git commit -am ${releaseMessage}`;
+await $`git tag -a ${nextTag} HEAD -m ${releaseMessage}`;
+await $`npx git-cliff -o CHANGELOG.md`;
+await $`git add CHANGELOG.md`;
+await $`git commit --amend`;
+await $`git push --follow-tags origin HEAD:refs/heads/${branch}`;
+
+// Push GitHub release
+const releaseNotes = $`npx git-cliff --latest`;
+const releaseData = JSON.stringify({
+    name: nextTag,
+    tag_name: nextTag,
+    body: releaseNotes
+});
+
+await $`curl -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${repoName}/releases -d ${releaseData}`;
--- scripts/gitrelease.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-nextVersion=$1
-releaseMessage="chore(release): ${nextVersion} [skip ci]"
-
-git add -A package.json package-lock.json packages/**/package.json CHANGELOG.md
-git commit -am ${releaseMessage}
-git tag -a ${nextVersion} HEAD -m ${releaseMessage}
-git push --follow-tags origin HEAD:refs/heads/master
