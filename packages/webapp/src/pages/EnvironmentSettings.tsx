NangoHQ/nango

commit b0972c48a449684ab933829f8f602b2e71bb942f

fix(api): stop using env cookie (#1987)
## Describe your changes

Fixes NAN-729

We were using cookie as a way to automically detect env, because cookies
are shared across tab this could create issues when switching env in an
other tab and doing a request in an other.

- Remove use of cookie env, all (almost) path now take `env` as a query
parameter

Tested on staging but please test on your side since it's touching every
pages and endpoints (edit, deleted and stuff)

packages/webapp/src/pages/EnvironmentSettings.tsx
@@ -25,6 +25,8 @@ import Button from '../components/ui/button/Button';
 import { useEnvironment } from '../hooks/useEnvironment';
 
 export const EnvironmentSettings: React.FC = () => {
+    const env = useStore((state) => state.env);
+
     const [secretKey, setSecretKey] = useState('');
     const [secretKeyRotatable, setSecretKeyRotatable] = useState(true);
     const [hasPendingSecretKey, setHasPendingSecretKey] = useState(false);
@@ -49,19 +51,18 @@ export const EnvironmentSettings: React.FC = () => {
     const [sendAuthWebhook, setSendAuthWebhook] = useState(false);
     const [hmacEditMode, setHmacEditMode] = useState(false);
     const [envVariables, setEnvVariables] = useState<{ id?: number; name: string; value: string }[]>([]);
-    const editCallbackUrlAPI = useEditCallbackUrlAPI();
-    const editWebhookUrlAPI = useEditWebhookUrlAPI();
-    const editHmacEnabled = useEditHmacEnabledAPI();
-    const editAlwaysSendWebhook = useEditAlwaysSendWebhookAPI();
-    const editSendAuthWebhook = useEditSendAuthWebhookAPI();
-    const editHmacKey = useEditHmacKeyAPI();
-    const editEnvVariables = useEditEnvVariablesAPI();
+    const editCallbackUrlAPI = useEditCallbackUrlAPI(env);
+    const editWebhookUrlAPI = useEditWebhookUrlAPI(env);
+    const editHmacEnabled = useEditHmacEnabledAPI(env);
+    const editAlwaysSendWebhook = useEditAlwaysSendWebhookAPI(env);
+    const editSendAuthWebhook = useEditSendAuthWebhookAPI(env);
+    const editHmacKey = useEditHmacKeyAPI(env);
+    const editEnvVariables = useEditEnvVariablesAPI(env);
 
     const { setVisible, bindings } = useModal();
     const { setVisible: setSecretVisible, bindings: secretBindings } = useModal();
 
-    const env = useStore((state) => state.cookieValue);
-    const { environment, mutate } = useEnvironment();
+    const { environment, mutate } = useEnvironment(env);
 
     useEffect(() => {
         setEnvVariables(envVariables.filter((env) => env.id));
@@ -246,7 +247,7 @@ export const EnvironmentSettings: React.FC = () => {
     };
 
     const onRotateKey = async (publicKey = true) => {
-        const res = await fetch('/api/v1/environment/rotate-key', {
+        const res = await fetch(`/api/v1/environment/rotate-key?env=${env}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -272,7 +273,7 @@ export const EnvironmentSettings: React.FC = () => {
     };
 
     const onRevertKey = async (publicKey = true) => {
-        const res = await fetch('/api/v1/environment/revert-key', {
+        const res = await fetch(`/api/v1/environment/?env=${env}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -298,7 +299,7 @@ export const EnvironmentSettings: React.FC = () => {
     };
 
     const onActivateKey = async (publicKey = true) => {
-        const res = await fetch('/api/v1/environment/activate-key', {
+        const res = await fetch(`/api/v1/environment/activate-key?env=${env}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -323,7 +324,7 @@ export const EnvironmentSettings: React.FC = () => {
     };
 
     const updateSlackNotifications = async (enabled: boolean) => {
-        await fetch('/api/v1/environment/slack-notifications-enabled', {
+        await fetch(`/api/v1/environment/slack-notifications-enabled?env=${env}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -337,7 +338,7 @@ export const EnvironmentSettings: React.FC = () => {
     const disconnectSlack = async () => {
         await updateSlackNotifications(false);
 
-        const res = await fetch(`/api/v1/connection/admin/account-${accountUUID}`, {
+        const res = await fetch(`/api/v1/connection/admin/account-${accountUUID}?env=${env}`, {
             method: 'DELETE'
         });
 
@@ -353,7 +354,7 @@ export const EnvironmentSettings: React.FC = () => {
     const connectSlack = async () => {
         const connectionId = `account-${accountUUID}`;
 
-        const res = await fetch(`/api/v1/environment/admin-auth?connection_id=${connectionId}`, {
+        const res = await fetch(`/api/v1/environment/admin-auth?connection_id=${connectionId}&env=${env}`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'

packages/server/lib/controllers/auth.controller.ts
@@ -2,11 +2,11 @@ import type { Request, Response, NextFunction } from 'express';
 import { WorkOS } from '@workos-inc/node';
 import crypto from 'crypto';
 import util from 'util';
-import { resetPasswordSecret, getUserAccountAndEnvironmentFromSession } from '../utils/utils.js';
+import { resetPasswordSecret, getUserFromSession } from '../utils/utils.js';
 import jwt from 'jsonwebtoken';
 import EmailClient from '../clients/email.client.js';
 import type { User } from '@nangohq/shared';
-import { isCloud, baseUrl, basePublicUrl, getLogger, isOk, resultErr, resultOk, type Result } from '@nangohq/utils';
+import { isCloud, baseUrl, basePublicUrl, getLogger, isOk, resultErr, resultOk, type Result, isErr } from '@nangohq/utils';
 import {
     userService,
     accountService,
@@ -82,13 +82,13 @@ const createAccountIfNotInvited = async (name: string, state?: string): Promise<
 class AuthController {
     async signin(req: Request, res: Response, next: NextFunction) {
         try {
-            const { success, error, response } = await getUserAccountAndEnvironmentFromSession(req);
-            if (!success || response === null) {
-                errorManager.errResFromNangoErr(res, error);
+            const getUser = await getUserFromSession(req);
+            if (isErr(getUser)) {
+                errorManager.errResFromNangoErr(res, getUser.err);
                 return;
             }
-            const { user } = response;
 
+            const user = getUser.res;
             const webUser: WebUser = {
                 id: user.id,
                 accountId: user.account_id,

packages/server/lib/controllers/user.controller.ts
@@ -1,7 +1,7 @@
-import { getUserAccountAndEnvironmentFromSession } from '../utils/utils.js';
+import { getUserAccountAndEnvironmentFromSession, getUserFromSession } from '../utils/utils.js';
 import type { Request, Response, NextFunction } from 'express';
 import EmailClient from '../clients/email.client.js';
-import { isCloud, isEnterprise, basePublicUrl } from '@nangohq/utils';
+import { isCloud, isEnterprise, basePublicUrl, isErr } from '@nangohq/utils';
 import { errorManager, userService } from '@nangohq/shared';
 
 export interface GetUser {
@@ -16,13 +16,13 @@ export interface GetUser {
 class UserController {
     async getUser(req: Request, res: Response<GetUser>, next: NextFunction) {
         try {
-            const { success, error, response } = await getUserAccountAndEnvironmentFromSession(req);
-            if (!success || response === null) {
-                errorManager.errResFromNangoErr(res, error);
+            const getUser = await getUserFromSession(req);
+            if (isErr(getUser)) {
+                errorManager.errResFromNangoErr(res, getUser.err);
                 return;
             }
-            const { user } = response;
 
+            const user = getUser.res;
             res.status(200).send({
                 user: {
                     id: user.id,
@@ -38,12 +38,13 @@ class UserController {
 
     async editName(req: Request, res: Response, next: NextFunction) {
         try {
-            const { success, error, response } = await getUserAccountAndEnvironmentFromSession(req);
-            if (!success || response === null) {
-                errorManager.errResFromNangoErr(res, error);
+            const getUser = await getUserFromSession(req);
+            if (isErr(getUser)) {
+                errorManager.errResFromNangoErr(res, getUser.err);
                 return;
             }
-            const { user } = response;
+
+            const user = getUser.res;
             const name = req.body['name'];
 
             if (!name) {
@@ -60,12 +61,13 @@ class UserController {
 
     async editPassword(req: Request, res: Response, next: NextFunction) {
         try {
-            const { success, error, response } = await getUserAccountAndEnvironmentFromSession(req);
-            if (!success || response === null) {
-                errorManager.errResFromNangoErr(res, error);
+            const getUser = await getUserFromSession(req);
+            if (isErr(getUser)) {
+                errorManager.errResFromNangoErr(res, getUser.err);
                 return;
             }
-            const { user } = response;
+
+            const user = getUser.res;
             const oldPassword = req.body['old_password'];
             const newPassword = req.body['new_password'];
 

packages/server/lib/utils/utils.ts
@@ -1,41 +1,53 @@
-import { fileURLToPath } from 'url';
-import path from 'path';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
 import type { Request } from 'express';
 import type { User, Environment, Account, Template as ProviderTemplate, ServiceResponse } from '@nangohq/shared';
-import { getLogger } from '@nangohq/utils';
+import type { Result } from '@nangohq/utils';
+import { getLogger, resultErr, resultOk, isErr } from '@nangohq/utils';
 import type { WSErr } from './web-socket-error.js';
 import { NangoError, userService, environmentService, interpolateString } from '@nangohq/shared';
 
 const logger = getLogger('Server.Utils');
 
-export async function getUserAccountAndEnvironmentFromSession(
-    req: Request<any, any>
-): Promise<ServiceResponse<{ user: User; account: Account; environment: Environment }>> {
+export async function getUserFromSession(req: Request<any, any>): Promise<Result<User, NangoError>> {
     const sessionUser = req.user;
-    const currentEnvironment: string = req.cookies['env'] || 'dev';
-
-    if (sessionUser == null) {
+    if (!sessionUser) {
         const error = new NangoError('user_not_found');
 
-        return { success: false, error, response: null };
+        return resultErr(error);
     }
 
     const user = await userService.getUserById(sessionUser.id);
 
-    if (user == null) {
+    if (!user) {
         const error = new NangoError('user_not_found');
-        return { success: false, error, response: null };
+        return resultErr(error);
+    }
+
+    return resultOk(user);
+}
+
+export async function getUserAccountAndEnvironmentFromSession(
+    req: Request<any, any>
+): Promise<ServiceResponse<{ user: User; account: Account; environment: Environment }>> {
+    const getUser = await getUserFromSession(req);
+    if (isErr(getUser)) {
+        return { error: getUser.err, response: null, success: false };
+    }
+
+    const user = getUser.res;
+    const currentEnvironment = req.query['env'];
+    if (typeof currentEnvironment !== 'string') {
+        return { success: false, error: new NangoError('invalid_env'), response: null };
     }
 
     const environmentAndAccount = await environmentService.getAccountAndEnvironmentById(user.account_id, currentEnvironment);
-
     if (!environmentAndAccount) {
         const error = new NangoError('account_not_found');
         return { success: false, error, response: null };
     }
 
     const response = { user, ...environmentAndAccount };
-
     return { success: true, error: null, response };
 }
 

packages/shared/lib/utils/error.ts
@@ -127,6 +127,11 @@ export class NangoError extends Error {
                 this.message = 'The requested OAuth scopes are invalid. OAuth scopes should be comma separated and not an array';
                 break;
 
+            case 'invalid_env':
+                this.status = 400;
+                this.message = "Invalid param 'env'";
+                break;
+
             case 'missing_environment_id':
                 this.status = 400;
                 this.message = `Missing param 'environment_id'.`;

packages/shared/lib/utils/utils.ts
@@ -315,7 +315,10 @@ export async function getEnvironmentAndAccountId(
 
 export async function getAccountIdAndEnvironmentIdFromSession(req: Request): Promise<ServiceResponse<{ accountId: number; environmentId: number }>> {
     const sessionUser = req.user as User;
-    const currentEnvironment: string = req.cookies['env'] || 'dev';
+    const currentEnvironment = req.query['env'];
+    if (typeof currentEnvironment !== 'string') {
+        return { success: false, error: new NangoError('invalid_env'), response: null };
+    }
 
     if (sessionUser == null) {
         const error = new NangoError('user_not_found');

packages/webapp/src/App.tsx
@@ -43,7 +43,7 @@ Sentry.init({
 
 const App = () => {
     const SentryRoutes = Sentry.withSentryReactRouterV6Routing(Routes);
-    const env = useStore((state) => state.cookieValue);
+    const env = useStore((state) => state.env);
     const signout = useSignout();
     const setShowInteractiveDemo = useStore((state) => state.setShowInteractiveDemo);
     const showInteractiveDemo = useStore((state) => state.showInteractiveDemo);

packages/webapp/src/components/LeftNavBar.tsx
@@ -42,7 +42,10 @@ export default function LeftNavBar(props: LeftNavBarProps) {
     const navigate = useNavigate();
     const signout = useSignout();
     const { meta } = useMeta();
-    const { mutate } = useEnvironment();
+    const env = useStore((state) => state.env);
+    const setEnv = useStore((state) => state.setEnv);
+    const email = useStore((state) => state.email);
+    const { mutate } = useEnvironment(env);
     const showInteractiveDemo = useStore((state) => state.showInteractiveDemo);
 
     useEffect(() => {
@@ -59,13 +62,9 @@ export default function LeftNavBar(props: LeftNavBarProps) {
         };
     }, [showUserSettings]);
 
-    const env = useStore((state) => state.cookieValue);
-    const setCookieValue = useStore((state) => state.setCookieValue);
-    const email = useStore((state) => state.email);
-
     const handleEnvChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         const newEnv = e.target.value;
-        setCookieValue(newEnv);
+        setEnv(newEnv);
         void mutate();
 
         const pathSegments = window.location.pathname.split('/').filter(Boolean);

commit 2bb594ef386a9c4d84f3c130d827f1ab3cff6531

[nan-905] fix revert key and allow public keys to be able to be rotated (#2128)
## Describe your changes
Rotateable might not be set. Fixes incorrect route introduced by
https://github.com/NangoHQ/nango/pull/1987/files#diff-0199d9c354e383165f793ac468f1114e575c42728bf2a704dce1bb153e9c8288L275

## Issue ticket number and link
NAN-905

## Checklist before requesting a review (skip if just adding/editing
APIs & templates)
- [ ] I added tests, otherwise the reason is: 
- [ ] I added observability, otherwise the reason is:
- [ ] I added analytics, otherwise the reason is:

packages/webapp/src/pages/EnvironmentSettings.tsx
@@ -75,7 +75,7 @@ export const EnvironmentSettings: React.FC = () => {
         }
 
         setSecretKey(environment.pending_secret_key || environment.secret_key);
-        setSecretKeyRotatable(environment.secret_key_rotatable);
+        setSecretKeyRotatable(environment.secret_key_rotatable !== false);
         setHasPendingSecretKey(Boolean(environment.pending_secret_key));
 
         setPublicKey(environment.pending_public_key || environment.public_key);
@@ -270,7 +270,7 @@ export const EnvironmentSettings: React.FC = () => {
     };
 
     const onRevertKey = async (publicKey = true) => {
-        const res = await fetch(`/api/v1/environment/?env=${env}`, {
+        const res = await fetch(`/api/v1/environment/revert-key?env=${env}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'

packages/webapp/src/types.ts
@@ -264,7 +264,7 @@ export interface Account {
     always_send_webhook: boolean;
     slack_notifications: boolean;
     websockets_path: string;
-    secret_key_rotatable: boolean;
+    secret_key_rotatable?: boolean;
     env_variables: { id?: number; name: string; value: string }[];
     host: string;
     uuid: string;