9b26a79a5d41203acbfaa460d587372c3059069a
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-05-13 18:12:03 +0200

    feat(db): add ability to change schema name (#2126)
    ## Describe your changes
    
    Contributes NAN-900
    
    - Allow to change the default schema name
    For testing purpose, local dev, and also to be able to have multiple
    install in the same DB. @khaliqgant I believe it could also match your
    local setup.
    I need this to be able to run some specific integration tests

--- packages/shared/lib/db/migrations/20240222091347_sync-jobs-index.cjs
+++ packages/shared/lib/db/migrations/20240222091347_sync-jobs-index.cjs
@@ -2,7 +2,7 @@ exports.config = { transaction: false };
 
 exports.up = function (knex) {
     return knex.schema.raw(
-        'CREATE INDEX CONCURRENTLY "idx_jobs_id_status_type_where_delete" ON "nango"."_nango_sync_jobs" USING BTREE ("sync_id","status","type") WHERE deleted = false'
+        'CREATE INDEX CONCURRENTLY "idx_jobs_id_status_type_where_delete" ON "_nango_sync_jobs" USING BTREE ("sync_id","status","type") WHERE deleted = false'
     );
 };
 
