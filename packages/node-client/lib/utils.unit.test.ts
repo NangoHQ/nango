6fa4f0f2c6ff9a9f5d1228d926a78f30c5424a94
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-05-14 14:38:59 +0200

    regex

--- packages/node-client/lib/utils.unit.test.ts
+++ packages/node-client/lib/utils.unit.test.ts
@@ -3,9 +3,9 @@ import { getUserAgent } from './utils.js';
 
 describe('getUserAgent', () => {
     it('should output default user agent', () => {
-        expect(getUserAgent()).toMatch(/nango-node-client\/[0-9.]+ darwin.23.2.0; Node.js 20.12.2/);
+        expect(getUserAgent()).toMatch(/nango-node-client\/[0-9.]+ [a-z]+.[0-9.]+; Node.js 20.12.2/);
     });
     it('should output additional user agent ', () => {
-        expect(getUserAgent('cli')).toMatch(/nango-node-client\/[0-9.]+ darwin.23.2.0; Node.js 20.12.2; cli/);
+        expect(getUserAgent('cli')).toMatch(/nango-node-client\/[0-9.]+ [a-z]+.[0-9.]+; Node.js 20.12.2; cli/);
     });
 });
