2be4bc9f8dfb08704b8c8622ad69ae2ac2efc3e7
Kevin Descamps <kevindescamps78@gmail.com>
2024-05-21 09:41:45 +0200

    feat: allow upsert existing basic connection

--- packages/shared/lib/services/connection.service.ts
+++ packages/shared/lib/services/connection.service.ts
@@ -251,12 +251,6 @@ class ConnectionService {
         credentials: BasicApiCredentials | ApiKeyCredentials,
         logContextGetter: LogContextGetter
     ) {
-        const connection = await this.checkIfConnectionExists(connection_id, provider_config_key, environmentId);
-
-        if (connection) {
-            throw new NangoError('connection_already_exists');
-        }
-
         const [importedConnection] = await this.upsertApiConnection(connection_id, provider_config_key, provider, credentials, {}, environmentId, accountId);
 
         if (importedConnection) {
