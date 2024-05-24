f8ecd9e7c193f0bb23c94328662ba0ff9d304ea0
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-05-24 16:24:28 +0200

    feat(logs): opensearch -> elasticsearch (#2196)
    ## Describe your changes
    
    Fixes NAN-1000
    
    Due do lack of tooling, outdated sdk and minimal cloud support, we have
    decided to use elasticsearch instead of opensearch.
    
    - Use elasticsearch 
    I haven't changed the env var to avoid creating more trouble but not
    sure if it's a good idea. Let me know
    
    - Disabled elasticsearch in self-hosted docker compose
    Decision was made to have console log and empty state in the UI if this
    feature is disabled. So that's it's not required in the critical path.

--- packages/logs/lib/es/client.ts
+++ packages/logs/lib/es/client.ts
@@ -1,13 +1,12 @@
-import { Client } from '@opensearch-project/opensearch';
+import { Client } from '@elastic/elasticsearch';
 import { envs } from '../env.js';
 
 export const client = new Client({
-    nodes: envs.NANGO_LOGS_OS_URL || 'http://localhost:0',
+    nodes: envs.NANGO_LOGS_ES_URL || 'http://localhost:0',
     requestTimeout: 5000,
     maxRetries: 1,
     auth: {
-        username: envs.NANGO_LOGS_OS_USER!, // ggignore
-        password: envs.NANGO_LOGS_OS_PWD! // ggignore
-    },
-    ssl: { rejectUnauthorized: false }
+        username: envs.NANGO_LOGS_ES_USER!, // ggignore
+        password: envs.NANGO_LOGS_ES_PWD! // ggignore
+    }
 });
--- packages/logs/lib/utils.ts
+++ packages/logs/lib/utils.ts
@@ -1,5 +1,5 @@
 import { getLogger } from '@nangohq/utils';
 
-export const logger = getLogger('opensearch');
+export const logger = getLogger('elasticsearch');
 
 export const isCli = process.argv.find((value) => value.includes('/bin/nango') || value.includes('cli/dist/index'));
