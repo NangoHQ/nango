e92b2bda750d6663310b4313cf35810d6637fc16
Thomas Bonnin <233326+TBonnin@users.noreply.github.com>
2024-05-30 12:12:36 -0400

    chore(persist): make NANGO_ENCRYPTION_KEY required in persist service (#2226)
    Instead of failing when persisting records, persist will fail to start
    if the encryption key is not setup (with a error message mentioning the
    doc url)
    Also adding to the docs how to generate a encryption key
    ![Screenshot 2024-05-30 at 10 23
    52](https://github.com/NangoHQ/nango/assets/233326/70933c96-49c3-4768-a5ba-7299f832fb2c)
    
    
    https://linear.app/nango/issue/NAN-922/improve-error-when-no-encryption-key
    
    ## Checklist before requesting a review (skip if just adding/editing
    APIs & templates)
    - [ ] I added tests, otherwise the reason is: 
    - [ ] I added observability, otherwise the reason is:
    - [ ] I added analytics, otherwise the reason is:

--- packages/persist/lib/app.ts
+++ packages/persist/lib/app.ts
@@ -2,11 +2,12 @@ import './tracer.js';
 import { getLogger } from '@nangohq/utils';
 import { server } from './server.js';
 import { db } from '@nangohq/shared';
+import { envs } from './env.js';
 
 const logger = getLogger('Persist');
 
 try {
-    const port = parseInt(process.env['NANGO_PERSIST_PORT'] || '') || 3007;
+    const port = envs.NANGO_PERSIST_PORT;
     server.listen(port, () => {
         logger.info(`🚀 API ready at http://localhost:${port}`);
 
