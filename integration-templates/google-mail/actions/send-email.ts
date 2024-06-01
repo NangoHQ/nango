b1f4b7a4379669dc59912dcdcc4cfdb4776a6dd6
Shoaib Wani <wanishoaib@protonmail.com>
2024-06-01 16:14:01 +0530

    feat(google-mail): action script to send an email using Gmail integration

---
+++ integration-templates/google-mail/actions/send-email.ts
@@ -0,0 +1,38 @@
+import type { NangoAction, GmailEmailSentOutput, GmailEmailInput } from '../../models';
+
+export default async function runAction(nango: NangoAction, input: GmailEmailInput): Promise<GmailEmailSentOutput> {
+    try {
+        // generate a base64 representation of input
+        const email = `From: ${input.from}\nTo: ${input.to}\nSubject: ${input.subject}\n\n${input.body}`;
+
+        const base64EncodedEmail = Buffer.from(email).toString('base64');
+
+        // send the email using nango proxy
+        const sentEmailResponse = await nango.proxy({
+            method: 'POST',
+            endpoint: '/gmail/v1/users/me/messages/send',
+            data: {
+                raw: base64EncodedEmail
+            }
+        });
+
+        return mapEmail(sentEmailResponse.data);
+    } catch (error: any) {
+        throw new nango.ActionError({
+            message: 'Failed to fetch fields in the gmail-send action script.',
+            details: {
+                message: error?.message,
+                method: error?.config?.method,
+                url: error?.config?.url,
+                code: error?.code
+            }
+        });
+    }
+}
+
+function mapEmail(record: any): GmailEmailSentOutput {
+    return {
+        id: record.id,
+        threadId: record.threadId
+    };
+}
