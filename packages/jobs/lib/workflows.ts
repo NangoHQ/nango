e13e10d2d48a4a94848bf03a5be0acdbe5b3f810
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-06-03 16:03:54 +0200

    jesus

--- packages/jobs/lib/workflows.ts
+++ packages/jobs/lib/workflows.ts
@@ -2,7 +2,13 @@ import type { ActivityFailure } from '@temporalio/workflow';
 import { CancellationScope, proxyActivities, isCancellation } from '@temporalio/workflow';
 import type * as activities from './activities.js';
 import type { WebhookArgs, PostConnectionScriptArgs, ContinuousSyncArgs, InitialSyncArgs, ActionArgs, RunnerOutput } from '@nangohq/shared';
-import { ACTION_MAX_ATTEMPTS, ACTION_TIMEOUT, SYNC_MAX_ATTEMPTS, SYNC_TIMEOUT, WEBHOOK_MAX_ATTEMPTS, WEBHOOK_TIMEOUT } from '@nangohq/utils';
+
+const SYNC_TIMEOUT = '24h';
+const SYNC_MAX_ATTEMPTS = 3;
+const ACTION_TIMEOUT = '15m';
+const ACTION_MAX_ATTEMPTS = 1; // no retry
+const WEBHOOK_TIMEOUT = '15m';
+const WEBHOOK_MAX_ATTEMPTS = 3;
 
 const { routeSync, scheduleAndRouteSync } = proxyActivities<typeof activities>({
     // 1 hour to start so syncs are not evicted from the queue too soon
