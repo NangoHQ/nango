7aeca14da1168d085b9bb119b25fd5ea97e77237
Thomas Bonnin <233326+TBonnin@users.noreply.github.com>
2024-06-07 13:26:38 -0400

    chore: recurring schedules can be created in paused state

--- packages/scheduler/lib/scheduler.integration.test.ts
+++ packages/scheduler/lib/scheduler.integration.test.ts
@@ -147,7 +147,7 @@ describe('Scheduler', () => {
 async function recurring({ scheduler, state = 'PAUSED' }: { scheduler: Scheduler; state?: ScheduleState }): Promise<Schedule> {
     const recurringProps = {
         name: nanoid(),
-        state,
+        state: 'STARTED' as const,
         startsAt: new Date(),
         frequencyMs: 900_000,
         payload: { foo: 'bar' },
