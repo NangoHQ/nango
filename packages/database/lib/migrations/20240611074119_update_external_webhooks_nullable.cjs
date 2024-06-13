8e5c9e92c2334b40b16776ec6bc6223d6b510e60
Khaliq <khaliqgant@gmail.com>
2024-06-13 13:56:35 +0300

    [nan-1142] rename column

--- packages/database/lib/migrations/20240611074119_update_external_webhooks_nullable.cjs
+++ packages/database/lib/migrations/20240611074119_update_external_webhooks_nullable.cjs
@@ -4,6 +4,7 @@ exports.up = function (knex) {
     return knex.schema.alterTable(TABLE, function (t) {
         t.string('primary_url').nullable().alter();
         t.string('secondary_url').nullable().alter();
+        t.renameColumn('on_auth_refesh_error', 'on_auth_refresh_error');
         t.unique(['environment_id']);
     });
 };
@@ -12,6 +13,7 @@ exports.down = function (knex) {
     return knex.schema.alterTable(TABLE, function (t) {
         t.string('primary_url').notNullable().alter();
         t.string('secondary_url').notNullable().alter();
+        t.renameColumn('on_auth_refresh_error', 'on_auth_refesh_error');
         t.dropUnique(['environment_id']);
     });
 };
