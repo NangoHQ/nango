4373c23baf60a2a8136618d7ce4f1c60197c66a5
Khaliq <khaliqgant@gmail.com>
2024-06-20 15:15:05 +0300

    [nan-1188] add calendly syncs

---
+++ integration-templates/calendly/syncs/event-types.ts
@@ -0,0 +1,30 @@
+import type { NangoSync, EventType } from '../../models';
+
+export default async function fetchData(nango: NangoSync): Promise<void> {
+    const connection = await nango.getConnection();
+
+    const userId = connection.connection_config['owner'];
+
+    if (!userId) {
+        throw new Error('No user id found');
+    }
+
+    for await (const eventTypes of nango.paginate({
+        endpoint: '/event_types',
+        params: {
+            user: userId
+        },
+        paginate: {
+            response_path: 'collection'
+        }
+    })) {
+        const eventTypeData = eventTypes.map((eventType) => {
+            return {
+                ...eventType,
+                id: eventType.uri.split('/').pop()
+            };
+        });
+
+        await nango.batchSave<EventType>(eventTypeData, 'EventType');
+    }
+}
