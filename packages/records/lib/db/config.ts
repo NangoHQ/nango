dcf0879c0fc49c7e6440034e22d070b1c3ef85a2
Thomas Bonnin <233326+TBonnin@users.noreply.github.com>
2024-07-01 09:00:19 -0400

    chore: RECORDS_DATABASE_URL defaults to main db (#2431)
    Defaulting the records database to the main db instead of localhost.
    self-hosting nango now doesn't require to set the RECORDS_DATABASE_URL
    but only if one wants/needs to persist records in a dedicated db.
    
    Also adding mention of RECORDS_DATABASE_URL in self-hosting docs
    
    ## Issue ticket number and link
    
    https://linear.app/nango/issue/NAN-897/update-self-hosting-docs-to-mention-records-database-url
    
    ## Checklist before requesting a review (skip if just adding/editing
    APIs & templates)
    - [ ] I added tests, otherwise the reason is: 
    - [ ] I added observability, otherwise the reason is:
    - [ ] I added analytics, otherwise the reason is:

--- packages/records/lib/db/config.ts
+++ packages/records/lib/db/config.ts
@@ -2,13 +2,17 @@ import { envs } from '../env.js';
 import type { Knex } from 'knex';
 
 export const schema = envs.RECORDS_DATABASE_SCHEMA;
+const databaseUrl =
+    envs.RECORDS_DATABASE_URL ||
+    envs.NANGO_DATABASE_URL ||
+    `postgres://${envs.NANGO_DB_USER}:${envs.NANGO_DB_PASSWORD}@${envs.NANGO_DB_HOST}:${envs.NANGO_DB_PORT}/${envs.NANGO_DB_NAME}`;
 const runningMigrationOnly = process.argv.some((v) => v === 'migrate:latest');
 const isJS = !runningMigrationOnly;
 
 const config: Knex.Config = {
     client: 'postgres',
     connection: {
-        connectionString: envs.RECORDS_DATABASE_URL,
+        connectionString: databaseUrl,
         statement_timeout: 60000
     },
     searchPath: schema,
