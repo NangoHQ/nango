4b6cd16ce32cc3a0cd80fcc489bd95a6d9d69f5a
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-07-02 15:48:07 +0200

    cleanup

--- packages/shared/lib/index.ts
+++ packages/shared/lib/index.ts
@@ -29,7 +29,6 @@ export * from './services/sync/config/config.service.js';
 export * from './services/sync/config/endpoint.service.js';
 export * from './services/sync/config/deploy.service.js';
 export * from './services/onboarding.service.js';
-export * from './services/sync/run.utils.js';
 
 export * as oauth2Client from './clients/oauth2.client.js';
 
