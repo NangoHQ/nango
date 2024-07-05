1914a80089723e8ca433af83099bdd2138b1302d
Khaliq <khaliqgant@gmail.com>
2024-07-05 19:16:00 +0300

    [nan-1343] remove debug

--- scripts/run-integration-template.bash
+++ scripts/run-integration-template.bash
@@ -7,8 +7,6 @@ NANGO_SECRET_KEY_DEV=$(grep NANGO_SECRET_KEY_DEV .env | cut -d '=' -f2)
 
 cd integration-templates/$INTEGRATION
 
-echo $NANGO_SECRET_KEY_DEV
-
 NANGO_SECRET_KEY_DEV=$NANGO_SECRET_KEY_DEV NANGO_HOSTPORT=http://localhost:3003 _NANGO_IN_REPO=true npx nango compile
 cp models.ts ../
 NANGO_SECRET_KEY_DEV=$NANGO_SECRET_KEY_DEV NANGO_HOSTPORT=http://localhost:3003 _NANGO_IN_REPO=true npx nango $@
