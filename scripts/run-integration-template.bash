6129383ba339065a74eb928b7ab43bb570be10a0
Khaliq <khaliqgant@gmail.com>
2024-07-05 14:14:07 +0300

    [nan-1343] remove useless comment

--- scripts/run-integration-template.bash
+++ scripts/run-integration-template.bash
@@ -5,7 +5,6 @@ shift
 
 cd integration-templates/$INTEGRATION
 
-# grab the value from the .env file for NANGO_SECRET_KEY_DEV
 NANGO_SECRET_KEY_DEV=$(grep NANGO_SECRET_KEY_DEV .env | cut -d '=' -f2)
 
 NANGO_SECRET_KEY_DEV=$NANGO_SECRET_KEY_DEV NANGO_HOSTPORT=http://localhost:3003 _NANGO_IN_REPO=true npx nango compile
