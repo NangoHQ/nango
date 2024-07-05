4de68a6f003b6ea3511cb6092731d98b4315b142
Khaliq <khaliqgant@gmail.com>
2024-07-05 14:27:29 +0300

    [nan-1343] logic tweaks

--- scripts/run-integration-template.bash
+++ scripts/run-integration-template.bash
@@ -3,9 +3,11 @@
 INTEGRATION=$1
 shift
 
+NANGO_SECRET_KEY_DEV=$(grep NANGO_SECRET_KEY_DEV .env | cut -d '=' -f2)
+
 cd integration-templates/$INTEGRATION
 
-NANGO_SECRET_KEY_DEV=$(grep NANGO_SECRET_KEY_DEV .env | cut -d '=' -f2)
+echo $NANGO_SECRET_KEY_DEV
 
 NANGO_SECRET_KEY_DEV=$NANGO_SECRET_KEY_DEV NANGO_HOSTPORT=http://localhost:3003 _NANGO_IN_REPO=true npx nango compile
 cp models.ts ../
