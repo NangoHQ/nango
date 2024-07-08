29483a83cb04dc3c2650056ff772ebf7f4d2ddd4
Khaliq <khaliq@nango.dev>
2024-07-08 09:50:09 +0300

    feat(internal-integration-templates): [nan-1343] allow integration templates to be run from the repo (#2473)
    ## Describe your changes
    Makes for an easier workflow to be able to run and test integration
    templates directly from the repo instead of making another directory and
    then copying and pasting the files back and forth.
    
    So a command would look like this:
    
    ```
    bash scripts/run-integration-template.bash asana dryrun ${sync|action} ${connection_id}
    ```
    
    ## Issue ticket number and link
    NAN-1343
    
    ## Checklist before requesting a review (skip if just adding/editing
    APIs & templates)
    - [ ] I added tests, otherwise the reason is: 
    - [ ] I added observability, otherwise the reason is:
    - [ ] I added analytics, otherwise the reason is:

---
+++ scripts/run-integration-template.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
+TEMP_DIRECTORY=tmp-run-integration-template
+INTEGRATION=$1
+shift
+
+NANGO_SECRET_KEY_DEV=$(grep NANGO_SECRET_KEY_DEV .env | cut -d '=' -f2)
+
+rm -rf $TEMP_DIRECTORY
+mkdir -p $TEMP_DIRECTORY/nango-integrations
+cp -r integration-templates/$INTEGRATION $TEMP_DIRECTORY/nango-integrations
+
+mv $TEMP_DIRECTORY/nango-integrations/$INTEGRATION/nango.yaml $TEMP_DIRECTORY/nango-integrations/nango.yaml
+
+pushd $TEMP_DIRECTORY/nango-integrations
+
+NANGO_SECRET_KEY_DEV=$NANGO_SECRET_KEY_DEV NANGO_HOSTPORT=http://localhost:3003 npx nango $@
+popd
+rm -rf $TEMP_DIRECTORY
