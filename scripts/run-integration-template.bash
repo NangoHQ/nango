f3627683d9d39301904bf9dbcbe4f5e051d16e8b
Khaliq <khaliqgant@gmail.com>
2024-07-08 09:39:51 +0300

    [nan-1343] use named temp directory

--- scripts/run-integration-template.bash
+++ scripts/run-integration-template.bash
@@ -9,19 +9,20 @@ popd () {
     command popd "$@" > /dev/null
 }
 
+TEMP_DIRECTORY=tmp/run-integration-template
 INTEGRATION=$1
 shift
 
 NANGO_SECRET_KEY_DEV=$(grep NANGO_SECRET_KEY_DEV .env | cut -d '=' -f2)
 
-rm -rf temp
-mkdir -p temp/nango-integrations
-cp -r integration-templates/$INTEGRATION temp/nango-integrations
+rm -rf $TEMP_DIRECTORY
+mkdir -p $TEMP_DIRECTORY/nango-integrations
+cp -r integration-templates/$INTEGRATION $TEMP_DIRECTORY/nango-integrations
 
-mv temp/nango-integrations/$INTEGRATION/nango.yaml temp/nango-integrations/nango.yaml
+mv $TEMP_DIRECTORY/nango-integrations/$INTEGRATION/nango.yaml $TEMP_DIRECTORY/nango-integrations/nango.yaml
 
-pushd temp/nango-integrations
+pushd $TEMP_DIRECTORY/nango-integrations
 
 NANGO_SECRET_KEY_DEV=$NANGO_SECRET_KEY_DEV NANGO_HOSTPORT=http://localhost:3003 npx nango $@
 popd
-rm -rf temp
+rm -rf $TEMP_DIRECTORY
