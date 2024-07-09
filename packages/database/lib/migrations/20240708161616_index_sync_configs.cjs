1bd9ee463c786351ef50a9ae8c86ccb73e02add7
Thomas Bonnin <233326+TBonnin@users.noreply.github.com>
2024-07-09 08:36:28 -0400

    chore: tweak sync_configs index

--- packages/database/lib/migrations/20240708161616_index_sync_configs.cjs
+++ packages/database/lib/migrations/20240708161616_index_sync_configs.cjs
@@ -2,10 +2,12 @@ exports.config = { transaction: false };
 
 exports.up = async function (knex, _) {
     await knex.schema.raw(
-        `CREATE INDEX CONCURRENTLY idx_nango_sync_configs_sync_name_active ON _nango_sync_configs USING BTREE (sync_name) WHERE active = true`
+        `CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_nango_sync_configs_active
+            ON _nango_sync_configs USING BTREE (type, sync_name, nango_config_id)
+            WHERE active = true`
     );
 };
 
 exports.down = async function (knex, _) {
-    await knex.schema.raw('DROP INDEX CONCURRENTLY idx_nango_sync_configs_sync_name_active');
+    await knex.schema.raw('DROP INDEX CONCURRENTLY IF EXISTS idx_nango_sync_configs_active');
 };
