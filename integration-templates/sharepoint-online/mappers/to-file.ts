8ffe2bb137f5b4a9accfe445dcdac2b149ea8b45
Hassan_Wari <85742599+hassan254-prog@users.noreply.github.com>
2024-07-15 17:19:48 +0300

    feat(integration-templates): improve sharepoint templates (#2495)
    ## Describe your changes
    
    - Added mapper functions to both templates.
    - Removed the logic of saving `deltaTokens` for incremental sync.
    - Since we no longer need the paginate helper function, I adopted the
    built-in `nango.paginate` function to replace it.
    
    ## Issue ticket number and link
    [EXT-95](https://linear.app/nango/issue/EXT-95/add-sharepoint-template)
    ## Checklist before requesting a review (skip if just adding/editing
    APIs & templates)
    - [ ] I added tests, otherwise the reason is: 
    - [ ] I added observability, otherwise the reason is:
    - [ ] I added analytics, otherwise the reason is:

---
+++ integration-templates/sharepoint-online/mappers/to-file.ts
@@ -0,0 +1,26 @@
+import type { FileMetadata } from '../../models';
+import type { DriveItem } from '../types';
+
+/**
+ * Converts a DriveItem object to a slim FileMetadata object.
+ * Only includes essential properties mapped from DriveItem.
+ * @param file The DriveItem object to convert.
+ * @returns FileMetadata object representing file metadata.
+ */
+export function toFile(file: DriveItem): FileMetadata {
+    const fileMetadata: FileMetadata = {
+        id: file.id,
+        etag: file.eTag,
+        cTag: file.cTag,
+        name: file.name,
+        is_folder: 'folder' in file,
+        mime_type: file.file?.mimeType ?? null,
+        path: file.parentReference.path,
+        raw_source: file,
+        updated_at: new Date(file.lastModifiedDateTime).toISOString(),
+        download_url: file['@microsoft.graph.downloadUrl'] ?? null,
+        created_at: new Date(file.createdDateTime).toISOString(),
+        blob_size: file.size
+    };
+    return fileMetadata;
+}
