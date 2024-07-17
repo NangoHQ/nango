5011afac8e617b9c827e5a56b89bed0ed2ee218b
Thomas Bonnin <233326+TBonnin@users.noreply.github.com>
2024-07-17 10:16:02 -0400

    chore: add start endpoint to runner

--- packages/runner/lib/utils.ts
+++ packages/runner/lib/utils.ts
@@ -1,3 +1,21 @@
-import { getLogger } from '@nangohq/utils';
+import { getLogger, stringifyError } from '@nangohq/utils';
 
 export const logger = getLogger('Runner');
+
+export async function httpFetch({ method, url, data }: { method: string; url: string; data: string }): Promise<void> {
+    try {
+        const res = await fetch(url, {
+            method: method,
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: data
+        });
+        if (res.status > 299) {
+            logger.error(`Error (status=${res.status}) sending '${data}' to '${url}': ${JSON.stringify(await res.json())}`);
+        }
+    } catch (err) {
+        logger.error(`Error sending '${data}' to '${url}': ${stringifyError(err)}`);
+    }
+}
