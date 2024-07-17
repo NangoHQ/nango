8205d34f5d313d60ca92e4afba8471806c6eba5c
Khaliq <khaliq@nango.dev>
2024-07-16 21:55:45 -0400

    fix(xero-integration-template): [nan-1438] xero updates to match the API better (#2519)
    ## Describe your changes
    Update the xero create-invoice to better match the API. Mappings are as
    follows:
    ```
    # fees
    # Xero => Nango
    Quantity => units
    UnitAmount => precise_unit_amount
    LineAmount => amount_cents
    TaxAmount => taxes_amount_cents
    DiscountAmount => discount_amount_cents
    DiscountRate => discount_rate
    ```
    
    ## Issue ticket number and link
    
    ## Checklist before requesting a review (skip if just adding/editing
    APIs & templates)
    - [ ] I added tests, otherwise the reason is: 
    - [ ] I added observability, otherwise the reason is:
    - [ ] I added analytics, otherwise the reason is:

--- scripts/run-integration-template.bash
+++ scripts/run-integration-template.bash
@@ -13,13 +13,14 @@ TEMP_DIRECTORY=tmp-run-integration-template
 INTEGRATION=$1
 shift
 
-NANGO_SECRET_KEY_DEV=$(grep NANGO_SECRET_KEY_DEV .env | cut -d '=' -f2)
+NANGO_SECRET_KEY_DEV=$(grep -v '^#' .env | grep NANGO_SECRET_KEY_DEV | cut -d '=' -f2)
 
 rm -rf $TEMP_DIRECTORY
 mkdir -p $TEMP_DIRECTORY/nango-integrations
 cp -r integration-templates/$INTEGRATION $TEMP_DIRECTORY/nango-integrations
 
 mv $TEMP_DIRECTORY/nango-integrations/$INTEGRATION/nango.yaml $TEMP_DIRECTORY/nango-integrations/nango.yaml
+[ -d $TEMP_DIRECTORY/nango-integrations/$INTEGRATION/fixtures ] && mv $TEMP_DIRECTORY/nango-integrations/$INTEGRATION/fixtures $TEMP_DIRECTORY/nango-integrations/fixtures
 
 pushd $TEMP_DIRECTORY/nango-integrations
 
