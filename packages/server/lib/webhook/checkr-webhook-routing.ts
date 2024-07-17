e22f1f61dcd4e25e01698cdc9d3e41981bb0c145
Khaliq <khaliq@nango.dev>
2024-07-17 11:28:16 -0400

    feat(webhooks): [nan-1432] add checkr webhook forwarding possibility (#2516)
    ## Describe your changes
    Support checkr webhooks. TBD on how to verify the webhook signature.
    [Docs](https://docs.checkr.com/#section/Webhooks/Securing-webhooks)
    
    ## Issue ticket number and link
    NAN-1432
    
    ## Checklist before requesting a review (skip if just adding/editing
    APIs & templates)
    - [ ] I added tests, otherwise the reason is: 
    - [ ] I added observability, otherwise the reason is:
    - [ ] I added analytics, otherwise the reason is:

---
+++ packages/server/lib/webhook/checkr-webhook-routing.ts
@@ -0,0 +1,48 @@
+import crypto from 'node:crypto';
+
+import type { Config as ProviderConfig } from '@nangohq/shared';
+import { getLogger } from '@nangohq/utils';
+import type { WebhookHandler } from './types.js';
+import type { LogContextGetter } from '@nangohq/logs';
+
+const logger = getLogger('Webhook.Checkr');
+
+interface CheckrBody {
+    id: string;
+    object: string;
+    type: string;
+    created_at: string;
+    webhook_url: string;
+    data: Record<string, unknown>;
+    createdAt: string;
+}
+
+function validate(integration: ProviderConfig, headerSignature: string, rawBody: string): boolean {
+    if (!integration.custom?.['webhookSecret']) {
+        return false;
+    }
+
+    const signature = crypto.createHmac('sha256', integration.custom['webhookSecret']).update(rawBody).digest('hex');
+    return crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(headerSignature));
+}
+
+const route: WebhookHandler = async (nango, integration, headers, body, rawBody, logContextGetter: LogContextGetter) => {
+    const signature = headers['x-checkr-signature'];
+
+    logger.info('received', { configId: integration.id });
+
+    if (!validate(integration, signature, rawBody)) {
+        logger.error('invalid signature', { configId: integration.id });
+        // TODO the verification should use the API key
+        //return;
+    }
+
+    const parsedBody = body as CheckrBody;
+    logger.info(`valid ${parsedBody.type}`, { configId: integration.id });
+
+    const response = await nango.executeScriptForWebhooks(integration, parsedBody, 'type', 'account_id', logContextGetter, 'checkr_account_id');
+
+    return { parsedBody, connectionIds: response?.connectionIds || [] };
+};
+
+export default route;
