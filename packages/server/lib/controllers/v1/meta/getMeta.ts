da9f525883597dc4c2e1ba85054ac1ad0fe3018e
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-07-22 18:18:48 +0200

    more

---
+++ packages/server/lib/controllers/v1/meta/getMeta.ts
@@ -0,0 +1,35 @@
+import { asyncWrapper } from '../../../utils/asyncWrapper.js';
+import { baseUrl, flagHasScripts, requireEmptyQuery, zodErrorToHTTP } from '@nangohq/utils';
+import { NANGO_VERSION, environmentService, getOnboardingProgress } from '@nangohq/shared';
+import { envs } from '@nangohq/logs';
+import type { GetMeta } from '@nangohq/types';
+
+export const getMeta = asyncWrapper<GetMeta>(async (req, res) => {
+    const emptyQuery = requireEmptyQuery(req);
+    if (emptyQuery) {
+        res.status(400).send({ error: { code: 'invalid_query_params', errors: zodErrorToHTTP(emptyQuery.error) } });
+        return;
+    }
+
+    const sessionUser = req.user;
+    if (!sessionUser) {
+        res.status(400).send({ error: { code: 'user_not_found' } });
+        return;
+    }
+
+    const environments = await environmentService.getEnvironmentsByAccountId(sessionUser.account_id);
+    const onboarding = await getOnboardingProgress(sessionUser.id);
+    res.status(200).send({
+        data: {
+            environments,
+            version: NANGO_VERSION,
+            baseUrl,
+            debugMode: req.session.debugMode === true,
+            onboardingComplete: onboarding?.complete || false,
+            features: {
+                logs: envs.NANGO_LOGS_ENABLED,
+                scripts: flagHasScripts
+            }
+        }
+    });
+});
