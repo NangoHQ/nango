db8ce3ecfd40cdb950e5c536667580060df0a2c2
Khaliq <khaliqgant@gmail.com>
2024-07-30 16:08:38 -0400

    [nan-1527] add anrok actions

---
+++ integration-templates/anrok/actions/void-transaction.ts
@@ -0,0 +1,32 @@
+import type { NangoAction, TransactionToDelete, TransactionDeletionActionResponse } from '../../models';
+
+export default async function runAction(nango: NangoAction, rawInput: TransactionToDelete[]): Promise<TransactionDeletionActionResponse> {
+    const response: TransactionDeletionActionResponse = {
+        succeeded: [],
+        failed: []
+    };
+    const input = Array.isArray(rawInput) ? rawInput : [rawInput];
+    for (const transaction of input) {
+        await nango
+            .post({
+                endpoint: `v1/seller/transactions/id:${transaction.id}/void`,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                data: {}
+            })
+            .then(() => {
+                const successTransaction = {
+                    ...transaction
+                };
+                response.succeeded.push(successTransaction);
+            })
+            .catch((error) => {
+                response.failed.push({
+                    ...transaction,
+                    validation_errors: error.response.data
+                });
+            });
+    }
+    return response;
+}
