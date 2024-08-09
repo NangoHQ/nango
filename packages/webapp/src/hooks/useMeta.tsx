bb51797316309afb136b9102752a8922864c54c8
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-08-09 12:32:16 +0200

    fix(ui): various

--- packages/webapp/src/hooks/useMeta.tsx
+++ packages/webapp/src/hooks/useMeta.tsx
@@ -1,9 +1,10 @@
 import useSWR from 'swr';
+import type { SWRError } from '../utils/api';
 import { swrFetcher } from '../utils/api';
 import type { GetMeta } from '@nangohq/types';
 
 export function useMeta() {
-    const { data, error } = useSWR<GetMeta['Success'], GetMeta['Errors']>('/api/v1/meta', swrFetcher);
+    const { data, error } = useSWR<GetMeta['Success'], SWRError<GetMeta['Errors']>>('/api/v1/meta', swrFetcher);
 
     const loading = !data && !error;
 
