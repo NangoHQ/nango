abd6972cc98471ab6ee0a107f1fb5f45ade2805c
Khaliq <khaliqgant@gmail.com>
2024-09-04 21:48:21 +0300

    debug

--- tests/integration-templates/generate-and-run-tests.bash
+++ tests/integration-templates/generate-and-run-tests.bash
@@ -11,6 +11,7 @@ INTEGRATION=$1
 
 # if no mocks exist for this integration then bail early
 if [ ! -d "../../integration-templates/$INTEGRATION/mocks" ]; then
+    pwd
     echo "No mocks found for $INTEGRATION"
     exit 0
 fi
