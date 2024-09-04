f7abe399097a78ad4bae3a3c3f2e3fcef6f67cb5
Khaliq <khaliqgant@gmail.com>
2024-09-04 21:59:15 +0300

    [nan-1643] check for mocks in the action

--- tests/integration-templates/generate-and-run-tests.bash
+++ tests/integration-templates/generate-and-run-tests.bash
@@ -9,13 +9,6 @@ popd () {
 
 INTEGRATION=$1
 
-# if no mocks exist for this integration then bail early
-if [ ! -d "../../integration-templates/$INTEGRATION/mocks" ]; then
-    pwd
-    echo "No mocks found for $INTEGRATION"
-    exit 0
-fi
-
 mkdir -p nango-integrations
 pushd nango-integrations
 cp -r ../../../integration-templates/$INTEGRATION .
