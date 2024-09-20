4e82a81ff35772464691e6367d8e2f0c5a5fd732
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-09-20 17:36:55 +0200

    feat(connect): integrations list (#2749)
    ## Describe your changes
    
    Contributes to https://linear.app/nango/issue/NAN-1703/create-ui
    
    - Create the integrations list page
    [[figma](https://www.figma.com/design/wBPwGMlopg8tcMFRekYLgE/Auth-UI?node-id=4107-456&node-type=canvas&m=dev)]
    
    - Setup react correctly: error boundary, router, fetcher, 
    
    <img width="488" alt="Screenshot 2024-09-20 at 13 54 26"
    src="https://github.com/user-attachments/assets/9954f917-c3b4-4846-9849-8506341abd45">
    
    
    ## 🧪 Tests
    ```
    npm run dw
    npm run dwa
    
    # temp to be able to use API
    touch packages/connect-ui/.env 
    VITE_LOCAL_SECRET_KEY=__SECRET_KEY__
    
    npm run connect-ui:dev:watch
    ```

---
+++ packages/connect-ui/src/lib/api.ts
@@ -0,0 +1,89 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-redundant-type-constituents */
+import type { ApiError, Endpoint, GetPublicListIntegrations, GetPublicProvider } from '@nangohq/types';
+
+const API_HOSTNAME = 'http://localhost:3003'; // TODO: remove hardcoded value
+
+function uriParamsReplacer(tpl: string, data: Record<string, any>) {
+    let res = tpl;
+    for (const [key, value] of Object.entries(data)) {
+        res = res.replace(`:${key}`, value);
+    }
+    return res;
+}
+
+export async function fetchApi<TEndpoint extends Endpoint<{ Path: any; Success: any; Method: any; Querystring?: any }>>(
+    path: TEndpoint['Path'],
+    opts: (TEndpoint['Method'] extends 'GET' ? { method?: TEndpoint['Method'] } : { method: TEndpoint['Method'] }) &
+        (TEndpoint['Body'] extends never ? { body?: undefined } : { body: TEndpoint['Body'] }) &
+        (TEndpoint['Querystring'] extends never ? { query?: undefined } : { query: TEndpoint['Querystring'] }) &
+        (TEndpoint['Params'] extends never ? { params?: never } : { params: TEndpoint['Params'] }),
+    method?: RequestInit['method']
+): Promise<TEndpoint['Success']> {
+    const url = new URL(API_HOSTNAME);
+    url.pathname = opts.params ? uriParamsReplacer(path, opts.params) : path;
+
+    if (opts?.query) {
+        for (const key in opts.query) {
+            if (typeof opts.query[key] === 'undefined' || (typeof opts.query[key] === 'string' && opts.query[key] === '')) {
+                continue;
+            }
+            if (Array.isArray(opts.query[key])) {
+                for (const val of opts.query[key] as any[]) {
+                    url.searchParams.set(key, val);
+                }
+            } else {
+                url.searchParams.set(key, opts.query[key]);
+            }
+        }
+    }
+
+    const headers = new Headers();
+    if (opts?.body) {
+        headers.append('content-type', 'application/json');
+    }
+
+    headers.append('Authorization', `Bearer ${import.meta.env.VITE_LOCAL_SECRET_KEY}`);
+
+    const res = await fetch(url, {
+        method: method || 'GET',
+        body: opts?.body && JSON.stringify(opts.body),
+        headers
+    });
+
+    let json: TEndpoint['Reply'] | undefined;
+    if (res.status !== 204) {
+        json = (await res.json()) as TEndpoint['Reply'];
+    }
+    if (res.status !== 200 || isError(json)) {
+        throw new APIError({ res, json });
+    }
+
+    return json || {};
+}
+
+export function isError<TEndpoint extends Endpoint<{ Path: any; Success: any; Method: any }>['Reply']>(
+    json: TEndpoint
+): json is Exclude<TEndpoint, { data: any }> {
+    return json && 'error' in (json as any);
+}
+
+export class APIError extends Error {
+    details;
+    constructor(details: { res: Response; json: Record<string, any> | ApiError<any> | undefined }) {
+        super('err');
+        this.details = details;
+    }
+}
+
+/******************
+ * ENDPOINTS
+ ********************/
+
+export async function getIntegrations() {
+    return await fetchApi<GetPublicListIntegrations>('/integrations', {});
+}
+
+export async function getProvider(params: GetPublicProvider['Params']) {
+    return await fetchApi<GetPublicProvider>(`/providers/:provider`, { params });
+}
