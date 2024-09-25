cb0b6385626ac0662d03eece9f56b7d538ad6b3d
Thomas Bonnin <233326+TBonnin@users.noreply.github.com>
2024-09-25 15:42:28 -0400

    fix(keystore): hash should actually be hashed o_O

--- packages/keystore/lib/utils/encryption.ts
+++ packages/keystore/lib/utils/encryption.ts
@@ -1,15 +1,32 @@
+import utils from 'node:util';
+import crypto from 'crypto';
 import { Encryption } from '@nangohq/utils';
 import { envs } from './env.js';
 
+const pbkdf2 = utils.promisify(crypto.pbkdf2);
+
 let encryption: Encryption | null = null;
+
+function getEncryptionKey(): string | undefined {
+    return envs.NANGO_ENCRYPTION_KEY;
+}
 
 export function getEncryption(): Encryption {
     if (!encryption) {
-        const encryptionKey = envs.NANGO_ENCRYPTION_KEY;
+        const encryptionKey = getEncryptionKey();
         if (!encryptionKey) {
             throw new Error('NANGO_ENCRYPTION_KEY is not set');
         }
         encryption = new Encryption(encryptionKey);
     }
     return encryption;
+}
+
+export async function hashValue(val: string): Promise<string> {
+    const encryptionKey = getEncryptionKey();
+    if (!encryptionKey) {
+        throw new Error('NANGO_ENCRYPTION_KEY is not set');
+    }
+
+    return (await pbkdf2(val, encryptionKey, 310000, 32, 'sha256')).toString('base64');
 }
