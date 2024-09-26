93de4fa64e4b6e9c2079acfcf306b8e32eebc842
Khaliq <khaliqgant@gmail.com>
2024-09-26 16:06:07 +0300

    [nan-1170] remove integration templates

--- integration-templates/anrok/utils.ts
+++
@@ -1,9 +0,0 @@
-export function errorToObject(err: unknown) {
-    if ('response' in (err as any)) {
-        return (err as any).response.data;
-    }
-    if (err instanceof Error) {
-        return JSON.parse(JSON.stringify(err, ['name', 'message']));
-    }
-    return err;
-}
--- integration-templates/unanet/actions/get-leads.ts
+++
@@ -1,9 +0,0 @@
-import type { NangoAction } from '../../models';
-
-export default async function runAction(nango: NangoAction, _input?: void): Promise<void> {
-    const response = await nango.get({
-        endpoint: '/api/leads'
-    });
-
-    return response.data;
-}
