02e9ceacbe0a6a66366f315e8b5c40f41eb6cb3d
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-10-03 12:30:36 +0200

    global api_hostname

--- packages/connect-ui/src/lib/nango.ts
+++ packages/connect-ui/src/lib/nango.ts
@@ -1,17 +1,17 @@
 import Nango from '@nangohq/frontend';
 import { useMemo } from 'react';
 
+import { API_HOSTNAME } from './api';
+
 export function useNango(sessionToken: string | null) {
     const nango = useMemo(() => {
         if (!sessionToken) {
             return;
         }
 
-        // Temp solution, ideally we can launch this without having to rebuild the whole UI
-        const hostname = import.meta.env.VITE_API_HOSTNAME || 'http://localhost:3003';
         return new Nango({
             connectSessionToken: sessionToken,
-            host: hostname
+            host: API_HOSTNAME
         });
     }, [sessionToken]);
 
