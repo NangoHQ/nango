8dd404400ad7bcba60bd0011885b2018b573b305
vladimir.gorobets <vladimir.gorobets@gmail.com>
2024-10-04 18:44:38 +0300

    add new token_url_encode parameter to provider config

--- packages/types/lib/providers/provider.ts
+++ packages/types/lib/providers/provider.ts
@@ -56,12 +56,12 @@ export interface BaseProvider {
     };
     authorization_url?: string;
     authorization_url_encode?: boolean;
-    token_url_encode?: boolean;
     access_token_url?: string;
     authorization_params?: Record<string, string>;
     scope_separator?: string;
     default_scopes?: string[];
     token_url?: string | TokenUrlObject;
+    token_url_encode?: boolean;
     token_params?: Record<string, string>;
     authorization_url_replacements?: Record<string, string>;
     redirect_uri_metadata?: string[];
