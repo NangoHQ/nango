3fb46ae3fad417629a79384f8fd65696815c1741
Hassan_Wari <thebigstormtech@gmail.com>
2024-10-21 09:26:18 +0300

    [ghost-admin-support] improve

--- packages/connect-ui/src/views/Go.tsx
+++ packages/connect-ui/src/views/Go.tsx
@@ -40,10 +40,15 @@ const formSchema: Record<AuthModeType, z.AnyZodObject> = {
         content_url: z.string().min(1)
     }),
     JWT: z.object({
-        api_key: z.string().optional(),
         privateKeyId: z.string().optional(),
         issuerId: z.string().optional(),
-        privateKey: z.string().optional()
+        privateKey: z.union([
+            z.object({
+                id: z.string(),
+                secret: z.string()
+            }),
+            z.string()
+        ])
     }),
     TBA: z.object({
         oauth_client_id_override: z.string().min(1),
