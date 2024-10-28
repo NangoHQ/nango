0c3290e8becd17a954be5cbb2b9c906788b5e8d3
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-10-28 16:18:33 +0100

    feat(connect): allow optional params, cosmetic fixes (#2909)
    ## Describe your changes
    
    Fixes
    https://linear.app/nango/issue/NAN-1947/allow-some-fields-to-be-optional
    
    - Allow optional params
    - Fix: do not display back on a single integration scenario
    - Fix: move help at the bottom of the screen
    - Fix: change button wording when error in credentials

--- packages/connect-ui/src/lib/store.ts
+++ packages/connect-ui/src/lib/store.ts
@@ -8,12 +8,14 @@ interface State {
     provider: GetPublicProvider['Success']['data'] | null;
     integration: GetPublicIntegration['Success']['data'] | null;
     isDirty: boolean;
+    isSingleIntegration: boolean;
     session: ConnectSessionPayload | null;
     nango: Nango | null;
     setSessionToken: (value: string) => void;
     setSession: (value: ConnectSessionPayload) => void;
     setNango: (value: Nango) => void;
     setIsDirty: (value: boolean) => void;
+    setIsSingleIntegration: (value: boolean) => void;
     set: (provider: GetPublicProvider['Success']['data'], integration: GetPublicIntegration['Success']['data']) => void;
     reset: () => void;
 }
@@ -23,12 +25,14 @@ export const useGlobal = create<State>((set) => ({
     provider: null,
     integration: null,
     isDirty: false,
+    isSingleIntegration: false,
     session: null,
     nango: null,
     setSessionToken: (value) => set({ sessionToken: value }),
     setSession: (value) => set({ session: value }),
     setNango: (value) => set({ nango: value }),
     setIsDirty: (value) => set({ isDirty: value }),
+    setIsSingleIntegration: (value) => set({ isSingleIntegration: value }),
     set: (provider, integration) => {
         set({ provider, integration });
     },
