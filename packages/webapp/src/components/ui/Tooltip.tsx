1419097c2cff74d56627d0574f6fa51332b0a2ac
Alan Johnson <nalanj@fastmail.com>
2024-10-29 12:04:35 -0400

    Add tooltip wrapper component for simple use cases

--- packages/webapp/src/components/ui/Tooltip.tsx
+++ packages/webapp/src/components/ui/Tooltip.tsx
@@ -23,4 +23,23 @@ const TooltipContent = React.forwardRef<React.ElementRef<typeof TooltipPrimitive
 );
 TooltipContent.displayName = TooltipPrimitive.Content.displayName;
 
-export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider };
+interface SimpleTooltipProps {
+    tooltipContent: React.ReactElement | string | undefined;
+}
+
+function SimpleTooltip(props: React.PropsWithChildren<SimpleTooltipProps>) {
+    if (!props.tooltipContent) {
+        return <>{props.children}</>;
+    }
+
+    return (
+        <TooltipProvider>
+            <Tooltip>
+                <TooltipContent>{props.tooltipContent}</TooltipContent>
+                <TooltipTrigger>{props.children}</TooltipTrigger>
+            </Tooltip>
+        </TooltipProvider>
+    );
+}
+
+export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider, SimpleTooltip };
