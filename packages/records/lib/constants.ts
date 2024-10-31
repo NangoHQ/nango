b8e53fdf1300313a6923a4c073422e4f07f189cb
Michael Biddle <36382405+mbiddle153@users.noreply.github.com>
2024-10-31 09:50:36 -0500

    feat: Add support for record count (#2874)
    ## Describe your changes
    Adds method to get count of records for a given connection and model (I
    believe has to be on a per-model basis due to the records table
    partitioning).
    
    Aggregates and attaches that count to the returned `SyncResponse` object
    and displays it in the `SyncRow` component.
    
    ## Issue ticket number and link
    
    ## Checklist before requesting a review (skip if just adding/editing
    APIs & templates)
    - [ ] I added tests, otherwise the reason is: 
    - [ ] I added observability, otherwise the reason is:
    - [ ] I added analytics, otherwise the reason is:
    
    ---------
    
    Co-authored-by: Michael Biddle <michaelbiddle@Michaels-MBP.attlocal.net>
    Co-authored-by: Thomas Bonnin <233326+TBonnin@users.noreply.github.com>

--- packages/records/lib/constants.ts
+++ packages/records/lib/constants.ts
@@ -1,1 +1,2 @@
 export const RECORDS_TABLE = 'records';
+export const RECORD_COUNTS_TABLE = 'record_counts';
--- packages/webapp/src/types.ts
+++ packages/webapp/src/types.ts
@@ -59,6 +59,7 @@ export interface SyncResponse {
         models: string[];
     };
     active_logs: Pick<ActiveLog, 'log_id'> | null;
+    record_count: number;
 }
 
 export type RunSyncCommand = 'PAUSE' | 'UNPAUSE' | 'RUN' | 'RUN_FULL' | 'CANCEL';
