4204309da3d96320f716ccfee7f7b519ebea4a20
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-11-06 10:36:18 +0100

    log only

--- packages/server/lib/middleware/cliVersionCheck.ts
+++ packages/server/lib/middleware/cliVersionCheck.ts
@@ -1,10 +1,13 @@
+import { getLogger } from '@nangohq/utils';
 import type { Request, Response, NextFunction } from 'express';
 
 import semver from 'semver';
 
+const logger = getLogger('CliVersionCheck');
+
 const VERSION_REGEX = /nango-cli\/([0-9.]+)/;
 export function cliMinVersion(minVersion: string) {
-    return (req: Request, res: Response, next: NextFunction) => {
+    return (req: Request, _: Response, next: NextFunction) => {
         const userAgent = req.headers['user-agent'];
         if (!userAgent) {
             // Could be strictly enforced
@@ -20,10 +23,11 @@ export function cliMinVersion(minVersion: string) {
         }
 
         if (semver.gt(minVersion, match[1])) {
-            res.status(400).send({
-                error: { code: 'invalid_cli_version', message: `This endpoint requires a CLI version >= ${minVersion} (current: ${match[0]})` }
-            });
-            return;
+            logger.info(`This endpoint requires a CLI version >= ${minVersion} (current: ${match[0]})`);
+            // res.status(400).send({
+            //     error: { code: 'invalid_cli_version', message: `This endpoint requires a CLI version >= ${minVersion} (current: ${match[0]})` }
+            // });
+            // return;
         }
 
         next();
