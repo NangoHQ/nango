8b9d2543913e277ba86a20500196e33b64a0ec49
Hassan_Wari <85742599+hassan254-prog@users.noreply.github.com>
2024-11-13 17:51:02 +0300

    feat(authmode): add a new signature authmode (#2942)
    ## Describe your changes
    
    - Add WSSE auth mode
    
    ## Issue ticket number and link
    [EXT-206](https://linear.app/nango/issue/EXT-206/add-emarsys-support)
    
    With the new changes we can now comfortably support
    [emarsys](https://dev.emarsys.com/docs/core-api-reference/b3c3a1eba8515-authentication#wsse-formata-namewsse-formata)
    and also found [oroinc](https://doc.oroinc.com/api/authentication/wsse/)
    that also supports WSSE auth mode.

--- packages/types/lib/providers/provider.ts
+++ packages/types/lib/providers/provider.ts
@@ -137,4 +137,12 @@ export interface ProviderTwoStep extends BaseProvider {
     token_expires_in_ms?: number;
     proxy_header_authorization?: string;
 }
-export type Provider = BaseProvider | ProviderOAuth1 | ProviderOAuth2 | ProviderJwt | ProviderTwoStep;
+export interface ProviderSignature extends BaseProvider {
+    signature: {
+        protocol: 'WSSE';
+    };
+    token: {
+        expires_in_ms: number;
+    };
+}
+export type Provider = BaseProvider | ProviderOAuth1 | ProviderOAuth2 | ProviderJwt | ProviderTwoStep | ProviderSignature;
