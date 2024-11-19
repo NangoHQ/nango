7a5937c7a73db5c7a0b5e1b4d9079cf765fd9984
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-11-19 11:06:09 +0100

    more

--- packages/webapp/src/pages/GettingStarted/Show.tsx
+++ packages/webapp/src/pages/GettingStarted/Show.tsx
@@ -1,4 +1,4 @@
-import { IconChevronRight, IconLockOpen2, IconPencil, IconRefresh, IconTool } from '@tabler/icons-react';
+import { IconChevronRight, IconLockOpen2, IconPencil, IconPlayerPlay, IconRefresh, IconTool } from '@tabler/icons-react';
 import { LeftNavBarItems } from '../../components/LeftNavBar';
 import Button from '../../components/ui/button/Button';
 import { Tag } from '../../components/ui/label/Tag';
@@ -6,12 +6,15 @@ import DashboardLayout from '../../layout/DashboardLayout';
 import { useAnalyticsTrack } from '../../utils/analytics';
 import { Helmet } from 'react-helmet';
 import { useScript } from '@uidotdev/usehooks';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { cn } from '../../utils/utils';
 
 export const GettingStarted: React.FC = () => {
-    const analyticsTrack = useAnalyticsTrack();
-
     const ytLoaded = useScript('https://www.youtube.com/iframe_api');
+    const analyticsTrack = useAnalyticsTrack();
+    const [hasVideo, setHasVideo] = useState(false);
+    const [apiReady, setApiReady] = useState(false);
+
     useEffect(() => {
         if (ytLoaded !== 'ready') {
             return;
@@ -20,37 +23,7 @@ export const GettingStarted: React.FC = () => {
         // The API will call this function when page has finished downloading
         // @ts-expect-error yes I want this
         window.onYouTubeIframeAPIReady = () => {
-            try {
-                const player = window.YT.Player('player', {
-                    height: '100%',
-                    width: '100%',
-                    videoId: 'oTpWlmnv7dM',
-                    playerVars: {
-                        playsinline: 1
-                    },
-                    events: {
-                        onStateChange: (event: { data: number }) => {
-                            switch (event.data) {
-                                case 0:
-                                    console.log('event on end');
-                                    break;
-                                case 1:
-                                    console.log('event on playing');
-                                    break;
-                                case 2:
-                                    console.log('event on playing');
-                                    break;
-                                default:
-                            }
-                        }
-                    }
-                });
-                player.on('stateChange', (e) => {
-                    console.log('vt', e);
-                });
-            } catch {
-                // do nothing
-            }
+            setApiReady(true);
         };
         // @ts-expect-error yes I want this
         window.onPlayerStateChange = (event) => {
@@ -58,13 +31,73 @@ export const GettingStarted: React.FC = () => {
         };
     }, [ytLoaded]);
 
+    const triggerVideo = () => {
+        if (hasVideo) {
+            return;
+        }
+        if (!apiReady) {
+            // adblock
+            return;
+        }
+
+        setHasVideo(true);
+        try {
+            const player = window.YT.Player('player', {
+                height: '100%',
+                width: '100%',
+                videoId: 'oTpWlmnv7dM',
+                playerVars: {
+                    playsinline: 1,
+                    autoplay: true
+                },
+                events: {
+                    onReady: () => {
+                        console.log('salut cest ready');
+                    },
+                    onStateChange: (event: { data: number }) => {
+                        switch (event.data) {
+                            case 0:
+                                console.log('event on end');
+                                break;
+                            case 1:
+                                console.log('event on playing');
+                                break;
+                            case 2:
+                                console.log('event on playing');
+                                break;
+                            default:
+                        }
+                    }
+                }
+            });
+            player.on('stateChange', (e) => {
+                console.log('stateChange', e);
+            });
+        } catch {
+            // do nothing
+        }
+    };
+
     return (
         <DashboardLayout selectedItem={LeftNavBarItems.GettingStarted} className="flex flex-col gap-9">
             <Helmet>
                 <title>Getting Started - Nango</title>
             </Helmet>
-            <div className="border rounded-lg border-grayscale-700">
-                <div id="player" style={{ aspectRatio: '16 / 9' }}></div>
+            <div
+                className={cn(
+                    'border rounded-lg border-grayscale-700 group hover:border-gray-600 hover:shadow-card focus:shadow-card focus:border-gray-600 focus:outline-0',
+                    !hasVideo && 'cursor-pointer'
+                )}
+                onClick={!hasVideo ? triggerVideo : undefined}
+            >
+                <div id="player" style={{ aspectRatio: '16 / 9' }} className="rounded-lg relative">
+                    <img src="/images/demo_thumbnail.png" alt="" className="rounded-lg" />
+                    <div className="absolute w-full h-full top-0 left-0 flex items-center justify-center z-10 text-black">
+                        <div className="transition-transform bg-white p-2 rounded-full shadow-[0_1px_100px_50px_black] group-hover:animate-pulse">
+                            <IconPlayerPlay size={50} fill="#000" />
+                        </div>
+                    </div>
+                </div>
             </div>
             <h1 className="text-xl font-semibold text-white">Build your first integration from here:</h1>
             <div className="grid grid-cols-2 text-white gap-7">
