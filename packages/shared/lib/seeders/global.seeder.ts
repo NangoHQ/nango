948191f2b66e2320711771ee56ae00e25a8b40b1
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-11-19 17:55:51 +0100

    fix(models): move User to DBUser

--- packages/shared/lib/seeders/global.seeder.ts
+++ packages/shared/lib/seeders/global.seeder.ts
@@ -1,10 +1,9 @@
-import type { User } from '../models/index.js';
 import { seedUser } from './user.seeder.js';
 import { createEnvironmentSeed } from './environment.seeder.js';
 import { createAccount } from './account.seeder.js';
-import type { DBEnvironment, DBTeam } from '@nangohq/types';
+import type { DBEnvironment, DBTeam, DBUser } from '@nangohq/types';
 
-export async function seedAccountEnvAndUser(): Promise<{ account: DBTeam; env: DBEnvironment; user: User }> {
+export async function seedAccountEnvAndUser(): Promise<{ account: DBTeam; env: DBEnvironment; user: DBUser }> {
     const account = await createAccount();
     const env = await createEnvironmentSeed(account.id, 'dev');
     const user = await seedUser(account.id);
