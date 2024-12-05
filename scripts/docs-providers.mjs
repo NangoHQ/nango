715c4c715d792d6778de194981f2a6b862667d7b
Alan Johnson <alan@nango.dev>
2024-12-05 11:46:20 -0500

    Add script for generating providers snippet for docs

---
+++ scripts/docs-providers.mjs
@@ -0,0 +1,14 @@
+import fs from 'node:fs';
+import yaml from 'js-yaml';
+
+const providersPath = 'packages/shared/providers.yaml';
+const providersMdxPath = 'docs-v2/snippets/providers.mdx';
+
+if (!fs.existsSync(providersPath)) {
+    console.error(`Providers file not found: ${providersPath}`);
+    process.exit(1);
+}
+
+const providers = yaml.load(fs.readFileSync(providersPath, 'utf-8'));
+
+fs.writeFileSync(providersMdxPath, `export const providers = ${JSON.stringify(providers, null, 4)}`, 'utf-8');
