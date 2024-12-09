4147bf552553f0d1cf51f452de33e4f85bdcc2a6
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-12-09 15:08:21 +0100

    tests

--- packages/server/lib/controllers/auth/postUnauthenticated.integration.test.ts
+++ packages/server/lib/controllers/auth/postUnauthenticated.integration.test.ts
@@ -1,4 +1,4 @@
-import { seeders } from '@nangohq/shared';
+import { connectionService, seeders } from '@nangohq/shared';
 import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 import { isError, isSuccess, runServer } from '../../utils/tests.js';
 
@@ -120,5 +120,57 @@ describe(`GET ${endpoint}`, () => {
                 errors: [{ code: 'custom', message: 'connection_id is forbidden when using session token', path: ['connection_id'] }]
             }
         });
+    });
+
+    it('should be allowed to reconnect', async () => {
+        const { env } = await seeders.seedAccountEnvAndUser();
+        const config = await seeders.createConfigSeed(env, 'unauthenticated', 'unauthenticated');
+
+        // Initial session token
+        const resSession = await api.fetch('/connect/sessions', {
+            method: 'POST',
+            token: env.secret_key,
+            body: { end_user: { id: '1', email: 'john@example.com' }, allowed_integrations: ['unauthenticated'] }
+        });
+        isSuccess(resSession.json);
+
+        // Initial connect
+        const resConnect = await api.fetch(endpoint, {
+            method: 'POST',
+            query: { connect_session_token: resSession.json.data.token },
+            params: { providerConfigKey: config.unique_key }
+        });
+        isSuccess(resConnect.json);
+
+        const connectionBefore = await connectionService.checkIfConnectionExists(resConnect.json.connectionId, resConnect.json.providerConfigKey, env.id);
+
+        // Reconnect session token
+        const resSessionReconnect = await api.fetch('/connect/sessions/reconnect', {
+            method: 'POST',
+            token: env.secret_key,
+            body: {
+                connection_id: resConnect.json.connectionId,
+                integration_id: resConnect.json.providerConfigKey,
+                end_user: { id: '1', email: 'john.updated@example.com' }
+            }
+        });
+        isSuccess(resSessionReconnect.json);
+
+        // Reconnect connect
+        const resReconnect = await api.fetch(endpoint, {
+            method: 'POST',
+            query: { connect_session_token: resSessionReconnect.json.data.token },
+            params: { providerConfigKey: config.unique_key }
+        });
+        isSuccess(resReconnect.json);
+
+        // Check that the connection was updated
+        // Nothing is different except the date
+        const connectionAfter = await connectionService.checkIfConnectionExists(resConnect.json.connectionId, resConnect.json.providerConfigKey, env.id);
+        expect(connectionAfter).toMatchObject({
+            ...connectionBefore,
+            updated_at: expect.toBeIsoDate()
+        });
+        expect(connectionBefore?.updated_at.toISOString()).not.toBe(connectionAfter?.updated_at.toISOString());
     });
 });
