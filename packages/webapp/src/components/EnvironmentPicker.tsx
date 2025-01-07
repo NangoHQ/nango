4e2bf62d7f209d0195001493ccc25cfb6ad70c7c
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2025-01-07 17:11:36 +0100

    search after 5

--- packages/webapp/src/components/EnvironmentPicker.tsx
+++ packages/webapp/src/components/EnvironmentPicker.tsx
@@ -62,7 +62,7 @@ export const EnvironmentPicker: React.FC = () => {
                 className="bg-grayscale-900 w-full px-0 border-grayscale-700 py-0"
             >
                 <Command>
-                    {meta.environments.length > 2 && (
+                    {meta.environments.length > 5 && (
                         <CommandInput
                             placeholder="Search..."
                             className="text-white ring-0 focus:ring-0 focus-visible:outline-none border-transparent border-b-grayscale-700 rounded-b-none px-2.5    "
