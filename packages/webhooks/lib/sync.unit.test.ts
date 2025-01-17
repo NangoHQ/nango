608ca8a273792cb816d5eb329279381752d234f8
Alan Johnson <alan@nango.dev>
2025-01-17 14:55:56 -0500

    Fix up tests

--- packages/webhooks/lib/sync.unit.test.ts
+++ packages/webhooks/lib/sync.unit.test.ts
@@ -2,10 +2,58 @@
 import { vi, expect, describe, it, beforeEach } from 'vitest';
 import { sendSync } from './sync.js';
 import { axiosInstance } from '@nangohq/utils';
-import type { NangoSyncWebhookBodySuccess, Connection, ExternalWebhook, DBEnvironment } from '@nangohq/types';
-import * as logPackage from '@nangohq/logs';
+import type { NangoSyncWebhookBodySuccess, Connection, ExternalWebhook, DBEnvironment, DBTeam, DBSyncConfig } from '@nangohq/types';
+import type { Config } from '@nangohq/shared';
 
 const spy = vi.spyOn(axiosInstance, 'post');
+
+const account: DBTeam = {
+    id: 1,
+    name: 'team',
+    uuid: 'uuid',
+    is_capped: false,
+    created_at: new Date(),
+    updated_at: new Date()
+};
+
+const providerConfig: Config = {
+    id: 1,
+    provider: 'provider',
+    unique_key: 'unique_key',
+    oauth_client_id: '',
+    oauth_client_secret: '',
+    environment_id: 1,
+    missing_fields: [],
+    created_at: new Date(),
+    updated_at: new Date()
+};
+
+const syncConfig: DBSyncConfig = {
+    id: 1,
+    sync_name: 'a_sync',
+    nango_config_id: 1,
+    file_location: 'file_location',
+    version: '0.0.1',
+    models: [], // TODO: remove nullable NAN-2527
+    active: true,
+    runs: null,
+    model_schema: null,
+    environment_id: 1,
+    track_deletes: true,
+    type: 'sync',
+    auto_start: false,
+    attributes: {},
+    pre_built: true,
+    is_public: false,
+    metadata: {},
+    input: null,
+    sync_type: 'full',
+    webhook_subscriptions: null,
+    enabled: true,
+    models_json_schema: null,
+    created_at: new Date(),
+    updated_at: new Date()
+};
 
 const connection: Pick<Connection, 'id' | 'connection_id' | 'provider_config_key'> = {
     id: 1,
@@ -26,8 +74,6 @@ const webhookSettings: ExternalWebhook = {
     updated_at: new Date()
 };
 
-const getLogCtx = () => new logPackage.LogContext({ parentId: '1', operation: {} as any }, { dryRun: true, logToConsole: false });
-
 describe('Webhooks: sync notification tests', () => {
     beforeEach(() => {
         vi.resetAllMocks();
@@ -37,8 +83,11 @@ describe('Webhooks: sync notification tests', () => {
         const responseResults = { added: 10, updated: 0, deleted: 0 };
 
         await sendSync({
+            account,
             connection,
             environment: { name: 'dev', id: 1, secret_key: 'secret' } as DBEnvironment,
+            providerConfig,
+            syncConfig,
             webhookSettings: {
                 ...webhookSettings,
                 primary_url: '',
@@ -55,168 +104,153 @@ describe('Webhooks: sync notification tests', () => {
     });
 
     it('Should not send a sync webhook if the webhook url is not present even if always send is checked', async () => {
-        const logCtx = getLogCtx();
-
         const responseResults = { added: 10, updated: 0, deleted: 0 };
         await sendSync({
+            account,
             connection,
-            syncName: 'syncName',
+            environment: { name: 'dev', id: 1, secret_key: 'secret' } as DBEnvironment,
+            providerConfig,
+            syncConfig,
             model: 'model',
             responseResults,
             success: true,
             operation: 'INCREMENTAL',
             now: new Date(),
-            logCtx,
             webhookSettings: {
                 ...webhookSettings,
                 primary_url: '',
                 secondary_url: '',
                 on_sync_completion_always: true
-            },
-            environment: { name: 'dev', id: 1, secret_key: 'secret' } as DBEnvironment
+            }
         });
         expect(axiosInstance.post).not.toHaveBeenCalled();
     });
 
     it('Should not send a sync webhook if the webhook url is present but if always send is not checked and there were no sync changes', async () => {
-        const logCtx = getLogCtx();
-
         const responseResults = { added: 0, updated: 0, deleted: 0 };
         await sendSync({
+            account,
             connection,
-            syncName: 'syncName',
+            environment: { name: 'dev', id: 1, secret_key: 'secret' } as DBEnvironment,
+            providerConfig,
+            syncConfig,
             model: 'model',
             responseResults,
             operation: 'INCREMENTAL',
             success: true,
             now: new Date(),
-            logCtx,
             webhookSettings: {
                 ...webhookSettings,
                 secondary_url: '',
                 on_sync_completion_always: false
-            },
-            environment: { name: 'dev', id: 1, secret_key: 'secret' } as DBEnvironment
+            }
         });
         expect(spy).not.toHaveBeenCalled();
     });
 
     it('Should send a sync webhook if the webhook url is present and if always send is not checked and there were sync changes', async () => {
-        const logCtx = getLogCtx();
-
         const responseResults = { added: 10, updated: 0, deleted: 0 };
         await sendSync({
+            account,
             connection,
-            syncName: 'syncName',
+            environment: { name: 'dev', id: 1, secret_key: 'secret' } as DBEnvironment,
+            providerConfig,
+            syncConfig,
             model: 'model',
             responseResults,
             operation: 'INCREMENTAL',
             success: true,
             now: new Date(),
-            logCtx,
             webhookSettings: {
                 ...webhookSettings,
                 secondary_url: '',
                 on_sync_completion_always: false
-            },
-            environment: { name: 'dev', id: 1, secret_key: 'secret' } as DBEnvironment
+            }
         });
         expect(spy).toHaveBeenCalled();
     });
 
     it('Should send a sync webhook if the webhook url is present and if always send is checked and there were sync changes', async () => {
-        const logCtx = getLogCtx();
-
         const responseResults = { added: 10, updated: 0, deleted: 0 };
         await sendSync({
+            account,
             connection,
-            syncName: 'syncName',
+            environment: { name: 'dev', id: 1, secret_key: 'secret' } as DBEnvironment,
+            providerConfig,
+            syncConfig,
             model: 'model',
             responseResults,
             operation: 'INCREMENTAL',
             success: true,
             now: new Date(),
-            logCtx,
             webhookSettings: {
                 ...webhookSettings,
                 secondary_url: '',
                 on_sync_completion_always: true
-            },
-            environment: { name: 'dev', id: 1, secret_key: 'secret' } as DBEnvironment
+            }
         });
         expect(spy).toHaveBeenCalled();
     });
 
     it('Should send an sync webhook if the webhook url is present and if always send is checked and there were no sync changes', async () => {
-        const logCtx = getLogCtx();
-
         const responseResults = { added: 0, updated: 0, deleted: 0 };
         await sendSync({
+            account,
             connection,
-            syncName: 'syncName',
+            environment: { name: 'dev', id: 1, secret_key: 'secret' } as DBEnvironment,
+            providerConfig,
+            syncConfig,
             model: 'model',
             responseResults,
             operation: 'INCREMENTAL',
             now: new Date(),
-            logCtx,
             success: true,
             webhookSettings: {
                 ...webhookSettings,
                 secondary_url: '',
                 on_sync_completion_always: true
-            },
-            environment: { name: 'dev', id: 1, secret_key: 'secret' } as DBEnvironment
+            }
         });
         expect(spy).toHaveBeenCalled();
     });
 
     it('Should send an sync webhook twice if the webhook url and secondary are present and if always send is checked and there were no sync changes', async () => {
-        const logCtx = getLogCtx();
-
         const responseResults = { added: 0, updated: 0, deleted: 0 };
         await sendSync({
+            account,
             connection,
-            syncName: 'syncName',
+            environment: { name: 'dev', id: 1, secret_key: 'secret' } as DBEnvironment,
+            providerConfig,
+            syncConfig,
             model: 'model',
             responseResults,
             operation: 'INCREMENTAL',
             now: new Date(),
             success: true,
-            logCtx,
             webhookSettings: {
                 ...webhookSettings,
                 on_sync_completion_always: true
-            },
-            environment: {
-                name: 'dev',
-                id: 1,
-                secret_key: 'secret'
-            } as DBEnvironment
+            }
         });
         expect(spy).toHaveBeenCalledTimes(2);
     });
 
     it('Should send a webhook with the correct body on sync success', async () => {
-        const logCtx = getLogCtx();
-
         const now = new Date();
 
         const responseResults = { added: 10, updated: 0, deleted: 0 };
         await sendSync({
+            account,
             connection,
-            syncName: 'syncName',
+            environment: { name: 'dev', id: 1, secret_key: 'secret' } as DBEnvironment,
+            providerConfig,
+            syncConfig,
             model: 'model',
             responseResults,
             operation: 'INCREMENTAL',
             now,
             success: true,
-            logCtx,
-            webhookSettings: webhookSettings,
-            environment: {
-                name: 'dev',
-                id: 1,
-                secret_key: 'secret'
-            } as DBEnvironment
+            webhookSettings: webhookSettings
         });
 
         const body: NangoSyncWebhookBodySuccess = {
@@ -258,54 +292,52 @@ describe('Webhooks: sync notification tests', () => {
     });
 
     it('Should not send an error webhook if the option is not checked', async () => {
-        const logCtx = getLogCtx();
-
         const error = {
             type: 'error',
             description: 'error description'
         };
 
         await sendSync({
+            account,
             connection,
-            syncName: 'syncName',
+            environment: { name: 'dev', id: 1, secret_key: 'secret' } as DBEnvironment,
+            providerConfig,
+            syncConfig,
             model: 'model',
             success: false,
             error,
             operation: 'INCREMENTAL',
             now: new Date(),
-            logCtx,
             webhookSettings: {
                 ...webhookSettings,
                 on_sync_error: false
-            },
-            environment: { name: 'dev', id: 1, secret_key: 'secret' } as DBEnvironment
+            }
         });
 
         expect(spy).not.toHaveBeenCalled();
     });
 
     it('Should send an error webhook if the option is checked with the correct body', async () => {
-        const logCtx = getLogCtx();
-
         const error = {
             type: 'error',
             description: 'error description'
         };
 
         await sendSync({
+            account,
             connection,
-            syncName: 'syncName',
+            environment: { name: 'dev', id: 1, secret_key: 'secret' } as DBEnvironment,
+            providerConfig,
+            syncConfig,
             model: 'model',
             success: false,
             error,
             operation: 'INCREMENTAL',
             now: new Date(),
-            logCtx,
             webhookSettings: {
                 ...webhookSettings,
                 on_sync_error: true
-            },
-            environment: { name: 'dev', id: 1, secret_key: 'secret' } as DBEnvironment
+            }
         });
 
         expect(spy).toHaveBeenCalled();
