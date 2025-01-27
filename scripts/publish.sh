3380cda0a033b6eac27e7257954e53b06b59275f
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2025-01-27 12:17:52 +0100

    fix(npm): correct publish command

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -23,7 +23,7 @@ function bump_other_pkg {
     folder=$1
     package=$2
     pushd "$GIT_ROOT_DIR/packages/$folder"
-    npm install -E @nangohq/$package@$VERSION
+    npm install --save --save-exact @nangohq/$package@$VERSION
     popd
 }
 
@@ -105,7 +105,7 @@ bump_other_pkg "shared" "providers"
 # Node client
 bump_and_npm_publish "@nangohq/node" "$VERSION"
 pushd "$GIT_ROOT_DIR/packages/shared"
-npm install @nangohq/node@$VERSION
+npm install --save --save-exact @nangohq/node@$VERSION
 popd
 
 # Shared
@@ -124,7 +124,7 @@ bump_and_npm_publish "nango" "$VERSION"
 # Frontend
 bump_and_npm_publish "@nangohq/frontend" "$VERSION"
 pushd "$GIT_ROOT_DIR/packages/webapp"
-npm install @nangohq/frontend@$VERSION
+npm install --save --save-exact @nangohq/frontend@$VERSION
 popd
 
 # clean up
