65851d7bc8fbf20785f94a93912b2ba2dc26c88c
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2025-01-28 17:44:51 +0100

    automatic bump

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -1,11 +1,12 @@
 #!/usr/bin/env zx
-import { $, echo, chalk, glob, spinner, fs, path, minimist } from 'zx';
+import { $, echo, chalk, glob, spinner, fs, minimist } from 'zx';
 import figures from 'figures';
 
 // npx zx ./publish.mjs --version=0.0.1
 const argv = minimist(process.argv.slice(2));
 const nextVersion = argv.version;
 const skipCli = argv['skip-cli'] === true;
+const dryRun = argv['dry-run'] === true;
 
 if (!nextVersion) {
     echo`${chalk.red(`Please specify a version: "node publish.mjs 0.0.1"`)}`;
@@ -22,38 +23,36 @@ if (!nextVersion.match(versionRegex)) {
     process.exit(1);
 }
 
-const here = import.meta.dirname;
-
 // Append version to file because it's easier to get the version
-await modifyVersionTs();
+await bumpVersionTs();
 
 // Rebuild with modified version.ts
 await tsBuild();
 
 // ---- Publish
 await npmPublish('@nangohq/types');
-await bumpPackageFor('@nangohq/types', ['cli', 'frontend', 'nango-yaml', 'node-client', 'runner-sdk', 'providers']);
+await bumpReference('@nangohq/types');
 await npmInstall();
 
 await npmPublish('@nangohq/nango-yaml');
-await bumpPackageFor('@nangohq/nango-yaml', ['cli']);
+await bumpReference('@nangohq/nango-yaml');
 await npmInstall();
 
 await npmPublish('@nangohq/providers');
-await bumpPackageFor('@nangohq/providers', ['runner-sdk']);
+await bumpReference('@nangohq/providers');
 await npmInstall();
 
 await npmPublish('@nangohq/node');
-await bumpPackageFor('@nangohq/node', ['cli', 'runner-sdk']);
+await bumpReference('@nangohq/node');
 await npmInstall();
 
 await npmPublish('@nangohq/runner-sdk');
-await bumpPackageFor('@nangohq/runner-sdk', ['cli']);
+await bumpReference('@nangohq/runner-sdk');
 await npmInstall();
 
 // TODO: to delete maybe, seems unnecessary
 await npmPublish('@nangohq/frontend');
-await bumpPackageFor('webapp', ['frontend']);
+await bumpReference('@nangohq/frontend');
 await npmInstall();
 
 if (!skipCli) {
@@ -83,15 +82,14 @@ async function tsBuild() {
     echo(chalk.green(`${figures.tick} npm run ts-build`));
 }
 
-async function modifyVersionTs() {
-    await spinner('Modifying version.ts..', async () => {
-        const versionFiles = await glob('packages/*/lib/version.ts');
-        for (const file of versionFiles) {
-            const content = (await fs.readFile(file)).toString();
-            await fs.writeFile(file, content.replace(versionRegex, nextVersion));
-        }
-    });
-    echo(chalk.green(`${figures.tick} Modifying version.ts`));
+async function bumpVersionTs() {
+    echo`Bumping version.ts..`;
+    const versionFiles = await glob('packages/*/lib/version.ts');
+    for (const file of versionFiles) {
+        const content = (await fs.readFile(file)).toString();
+        await fs.writeFile(file, content.replace(versionRegex, nextVersion));
+        echo(chalk.grey(`  ${figures.tick} Bumped ${nextVersion} in ${file}`));
+    }
 }
 
 async function npmPublish(packageName) {
@@ -113,18 +111,26 @@ async function npmPublish(packageName) {
         }
 
         await $`npm version ${nextVersion} -w "${packageName}"`;
-        await $`npm publish --access public -w "${packageName}"`;
+        if (!dryRun) {
+            await $`npm publish --access public -w "${packageName}"`;
+        }
 
         echo(chalk.green(`${figures.tick} Published ${packageName}      `));
     });
 }
 
-async function bumpPackageFor(packageName, folders) {
+async function bumpReference(packageName) {
+    const packagesJson = await glob('packages/*/package.json');
     // We don't use npm install, because it behaves incoherently with workspaces and different terminals
-    for (const folder of folders) {
-        const fp = path.join(here, '..', 'packages', folder, 'package.json');
+    for (const packageJson of packagesJson) {
+        const fp = packageJson;
         const content = (await fs.readFile(fp)).toString();
-        await fs.writeFile(fp, content.replace(new RegExp(`${packageName}": ".*"`), `${packageName}": "${nextVersion}"`));
+        const matchReg = new RegExp(`${packageName}": "[0-9]+.[0-9]+.[0-9]+"`);
+        if (!matchReg.test(content)) {
+            continue;
+        }
+
+        await fs.writeFile(fp, content.replace(matchReg, `${packageName}": "${nextVersion}"`));
         echo(chalk.grey(`  ${figures.tick} Bumped ${packageName} to ${nextVersion} in ${fp}`));
     }
 }
