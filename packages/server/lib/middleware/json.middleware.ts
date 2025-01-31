71855adc9f44bae26d2d782db158cdd4a58e95d6
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2025-02-01 00:25:30 +0100

    fix(api): req.is is not as reliable as expected

--- packages/server/lib/middleware/json.middleware.ts
+++ packages/server/lib/middleware/json.middleware.ts
@@ -1,7 +1,7 @@
 import type { RequestHandler } from 'express';
 
 export const jsonContentTypeMiddleware: RequestHandler = (req, res, next) => {
-    if (req.headers['content-type'] && !req.is('application/json')) {
+    if (req.headers['content-type'] && !req.headers['content-type'].startsWith('application/json')) {
         // Send error here
         res.status(415).json({ error: { code: 'invalid_content_type', message: 'Content-Type header must be application/json' } });
         return;
