c6760218af6e9d4b1c6f822725a9ae23a37d041e
Thomas Bonnin <233326+TBonnin@users.noreply.github.com>
2025-02-03 15:32:13 -0500

    chore: fleet improvements
    - adding 'image' column to deployment
        - it will make it easy to switch to another image without having to change
    fleet code (for example if we move runner to the unified image).
        - Note: the column is not used by fleet supervisor yet. Coming in next PR :)
    - checking if the image exist when rolling out to avoid rolling out while master is building
    - add 'notes' column to node_overrides
        - won't be used by the app but I wanted a text column where we can
          manually enter useful information like the name of the customer or 'Thomas's runner'
    - making image, cpu_mill, memory_mb, storage_mb columns nullable in the node_overrides table.
        - I will change the overiding logic: if value is null, fleet will pick default values instead of having to insert specific values when overriding, especially useful for the image that changes with each deployment

--- packages/types/lib/fleet/index.ts
+++ packages/types/lib/fleet/index.ts
@@ -3,6 +3,7 @@ export type CommitHash = string & { readonly length: 40 };
 export interface Deployment {
     readonly id: number;
     readonly commitId: CommitHash;
+    readonly image: string;
     readonly createdAt: Date;
     readonly supersededAt: Date | null;
 }
