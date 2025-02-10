60b9af45024622e6309b8811f4bf4ce508b15662
Thomas Bonnin <233326+TBonnin@users.noreply.github.com>
2025-02-10 17:39:28 -0500

    fix(persist): correct validation and types (#3483)
    fix zod validations and types in persist

--- packages/persist/lib/routes/environment/environmentId/connection/connectionId/sync/syncId/job/jobId/validate.ts
+++ packages/persist/lib/routes/environment/environmentId/connection/connectionId/sync/syncId/job/jobId/validate.ts
@@ -1,6 +1,4 @@
 import { z } from 'zod';
-import { validateRequest } from '@nangohq/utils';
-import type { Endpoint } from '@nangohq/types';
 
 const mergingStrategySchema = z.discriminatedUnion('strategy', [
     z.object({
@@ -12,28 +10,27 @@ const mergingStrategySchema = z.discriminatedUnion('strategy', [
     })
 ]);
 
-export const validateRecords = <E extends Endpoint<any>>() =>
-    validateRequest<E>({
-        parseBody: (data: unknown) =>
-            z
-                .object({
-                    model: z.string(),
-                    records: z.array(z.object({ id: z.union([z.string().max(255).min(1), z.number()]) }).catchall(z.unknown())).nonempty(),
-                    providerConfigKey: z.string(),
-                    connectionId: z.string(),
-                    activityLogId: z.string(),
-                    merging: mergingStrategySchema.default({ strategy: 'override' })
-                })
-                .strict()
-                .parse(data),
-        parseParams: (data: unknown) =>
-            z
-                .object({
-                    environmentId: z.coerce.number().int().positive(),
-                    nangoConnectionId: z.coerce.number().int().positive(),
-                    syncId: z.string(),
-                    syncJobId: z.coerce.number().int().positive()
-                })
-                .strict()
-                .parse(data)
-    });
+export const recordsRequestParser = {
+    parseBody: (data: unknown) =>
+        z
+            .object({
+                model: z.string(),
+                records: z.array(z.object({ id: z.union([z.string().max(255).min(1), z.number()]) }).catchall(z.unknown())).nonempty(),
+                providerConfigKey: z.string(),
+                connectionId: z.string(),
+                activityLogId: z.string(),
+                merging: mergingStrategySchema.default({ strategy: 'override' })
+            })
+            .strict()
+            .parse(data),
+    parseParams: (data: unknown) =>
+        z
+            .object({
+                environmentId: z.coerce.number().int().positive(),
+                nangoConnectionId: z.coerce.number().int().positive(),
+                syncId: z.string(),
+                syncJobId: z.coerce.number().int().positive()
+            })
+            .strict()
+            .parse(data)
+};
