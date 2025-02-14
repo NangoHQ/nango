50c3202acf3bf8516891192938a0502ed5375975
Alan Johnson <alan@nango.dev>
2025-02-14 11:45:47 -0500

    Got changelog for use cases/endpoints to dump

--- scripts/pre-built-integrations-changelog.ts
+++ scripts/pre-built-integrations-changelog.ts
@@ -1,6 +1,14 @@
 import { $ } from 'zx';
-import type { Provider } from '@nangohq/types';
 import yaml from 'js-yaml';
+import type { Provider } from '@nangohq/types';
+
+interface FormattedFlow {
+    integration: string;
+    name: string;
+    endpoint: string;
+    method: string;
+    type: string;
+}
 
 const primaryBranch = (await $`git remote show origin | grep \"HEAD branch\" | sed \"s/.*: //\"`.text()).trim();
 const commits = await $`git log ${primaryBranch} --date=iso-strict --format=%cd`.lines();
@@ -8,7 +16,7 @@ let date = new Date(commits[commits.length - 1] as string);
 const until = new Date(new Date().getTime());
 
 let previousFlows: Record<string, any> | undefined = undefined;
-const months: [Date, string[]][] = [];
+const months: [Date, FormattedFlow[]][] = [];
 
 while (true) {
     const sha = await $`git log ${primaryBranch} --until='${date.toISOString()}' -1 --format=format:%H`.text();
@@ -18,14 +26,26 @@ while (true) {
 
     const flows = await getFlows(sha);
 
-    if (flows && previousFlows) {
-        // const previous = Object.keys(previousFlows);
-        // const current = Object.keys(providers);
-        // const added = current.filter((provider) => !previous.includes(provider));
-        // months.unshift([date, added]);
+    let canProcess = false;
+    if (flows) {
+        const firstFlow = Object.entries(flows['integrations'])[0]?.[1] as any;
+        if (firstFlow && (firstFlow['actions'] || firstFlow['syncs'])) {
+            canProcess = true;
+        }
     }
 
-    previousFlows = flows;
+    const formattedFlows = canProcess ? formatFlows(flows) : undefined;
+
+    if (formattedFlows && previousFlows) {
+        const previous = Object.keys(previousFlows);
+        const current = Object.keys(formattedFlows);
+        const added = current.filter((provider) => !previous.includes(provider));
+        const vals = added.map((key) => formattedFlows[key]);
+
+        months.unshift([date, vals]);
+    }
+
+    previousFlows = formattedFlows;
 
     if (date === until) {
         break;
@@ -43,22 +63,36 @@ if (!previousFlows) {
     process.exit(1);
 }
 
+const latestProviders: Record<string, Provider> = yaml.load(await $`git show ${primaryBranch}:packages/providers/providers.yaml`.text()) as Record<
+    string,
+    Provider
+>;
+
 console.log('# Pre-built Integrations Changelog');
 console.log();
-//console.log(`Nango supports ${Object.keys(previousProviders).length} providers`);
+console.log(`Nango supports ${Object.keys(previousFlows).length} pre-built use cases`);
 console.log();
 
 for (const [date, added] of months) {
     console.log(`## ${date.toLocaleString('default', { timeZone: 'UTC', month: 'long', year: 'numeric' })}:`);
     console.log();
-    //console.log(`${added.length} new providers`);
-    // console.log();
-    // for (const providerKey of added) {
-    //     const provider = previousProviders[providerKey];
+    console.log(`${added.length} new endpoints`);
 
-    //     console.log(`- [${provider?.display_name || providerKey}](/integrations/all/${providerKey})`);
-    // }
-    // console.log();
+    let previousIntegration = undefined;
+    for (const endpoint of added) {
+        if (previousIntegration !== endpoint.integration) {
+            const provider = latestProviders[endpoint.integration];
+
+            console.log(``);
+            console.log(`- [${provider ? provider.display_name : endpoint.integration}](/integrations/all/${endpoint.integration})`);
+            previousIntegration = endpoint.integration;
+        }
+
+        console.log(
+            `  - [${endpoint.method} ${endpoint.endpoint}](https://github.com/NangoHQ/integration-templates/blob/main/integrations/${endpoint.integration}/${endpoint.type}/${endpoint.name}.md)`
+        );
+    }
+    console.log();
 }
 
 async function getFlows(sha: string): Promise<Record<string, any> | undefined> {
@@ -68,4 +102,51 @@ async function getFlows(sha: string): Promise<Record<string, any> | undefined> {
     }
 
     return undefined;
+}
+
+function formatFlows(flows: any) {
+    const out: Record<string, FormattedFlow> = {};
+
+    for (const name of Object.keys(flows['integrations'])) {
+        const integration = flows['integrations'][name];
+
+        if (integration['actions']) {
+            for (const actionName of Object.keys(integration['actions'])) {
+                const action = integration['actions'][actionName];
+
+                const endpoints = Array.isArray(action['endpoint']) ? action['endpoint'] : [action['endpoint']];
+                for (const endpoint of endpoints) {
+                    const val = {
+                        integration: name,
+                        name: actionName,
+                        endpoint: endpoint['path'],
+                        method: endpoint['method'],
+                        type: 'actions'
+                    };
+                    out[JSON.stringify(val)] = val;
+                }
+            }
+        }
+
+        if (integration['syncs']) {
+            for (const syncName of Object.keys(integration['syncs'])) {
+                const sync = integration['syncs'][syncName];
+
+                const endpoints = Array.isArray(sync['endpoint']) ? sync['endpoint'] : [sync['endpoint']];
+                for (const endpoint of endpoints) {
+                    const val = {
+                        integration: name,
+                        name: syncName,
+                        endpoint: endpoint['path'],
+                        method: endpoint['method'],
+                        type: 'syncs'
+                    };
+
+                    out[JSON.stringify(val)] = val;
+                }
+            }
+        }
+    }
+
+    return out;
 }
