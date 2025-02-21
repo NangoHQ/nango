626a4518962ae3b3fcc540341db4a295fd6078e0
Khaliq <khaliq@nango.dev>
2025-02-21 15:35:48 +0200

    fix(basecamp): assign accountId if only one (#3555)
    <!-- Describe the problem and your solution --> 
    
    <!-- Issue ticket number and link (if applicable) -->
    
    <!-- Testing instructions (skip if just adding/editing providers) -->

--- packages/server/lib/hooks/connection/basecamp-post-connection.ts
+++ packages/server/lib/hooks/connection/basecamp-post-connection.ts
@@ -20,5 +20,12 @@ export default async function execute(nango: Nango) {
 
     const { accounts } = data;
 
-    await nango.updateConnectionConfig({ accounts });
+    const hasAccountId = connection.connection_config['accountId'];
+
+    // if the user didn't set the optional accountId only has one then set it automatically for them
+    if (!hasAccountId && accounts && accounts.length === 1 && accounts[0] && accounts[0].id) {
+        await nango.updateConnectionConfig({ accounts, accountId: accounts[0].id });
+    } else {
+        await nango.updateConnectionConfig({ accounts });
+    }
 }
