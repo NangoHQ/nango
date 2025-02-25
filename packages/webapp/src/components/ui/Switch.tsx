1f2873ca175eb939ac42d5d0004afc0f7b8d23f6
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2025-02-25 17:38:09 +0100

    fix(ui): change switch color, scroll to settings (#3577)
    ## Changes
    
    Fixes https://linear.app/nango/issue/NAN-2755/change-switch-color
    
    - change switch color
    - fix scroll to settings
    
    
    ![Screenshot 2025-02-25 at 16 02
    25](https://github.com/user-attachments/assets/0ffc6463-ef5c-43b9-824b-2c8a95728811)

--- packages/webapp/src/components/ui/Switch.tsx
+++ packages/webapp/src/components/ui/Switch.tsx
@@ -6,7 +6,7 @@ const Switch = React.forwardRef<React.ElementRef<typeof SwitchPrimitives.Root>, 
     ({ className, ...props }, ref) => (
         <SwitchPrimitives.Root
             className={cn(
-                'peer inline-flex h-4 w-7 shrink-0 cursor-pointer items-center rounded-full border border-grayscale-400 shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-grayscale-100 data-[state=unchecked]:bg-input data-[state=checked]:border-grayscale-100',
+                'peer inline-flex h-4 w-7 shrink-0 cursor-pointer items-center rounded-full border border-grayscale-400 shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-success-400 data-[state=unchecked]:bg-input data-[state=checked]:border-success-400',
                 className
             )}
             {...props}
