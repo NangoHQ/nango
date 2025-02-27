80e2131a97739869a3d29db9794a2dda64d3374a
Thomas Bonnin <233326+TBonnin@users.noreply.github.com>
2025-02-27 14:55:22 -0500

    feat: add API endpoints to create/delete a sync variant

---
+++ packages/types/lib/sync/api.ts
@@ -0,0 +1,33 @@
+import type { ApiError, Endpoint } from '../api.js';
+
+export type PostSyncVariant = Endpoint<{
+    Method: 'POST';
+    Path: '/sync/:name/variant/:variant';
+    Body: {
+        provider_config_key: string;
+        connection_id: string;
+    };
+    Params: {
+        name: string;
+        variant: string;
+    };
+    Error: ApiError<
+        'invalid_variant' | 'unknown_connection' | 'unknown_provider_config' | 'unknown_sync' | 'sync_variant_already_exists' | 'failed_sync_variant_creation'
+    >;
+    Success: { id: string; name: string; variant: string };
+}>;
+
+export type DeleteSyncVariant = Endpoint<{
+    Method: 'DELETE';
+    Path: '/sync/:name/variant/:variant';
+    Body: {
+        provider_config_key: string;
+        connection_id: string;
+    };
+    Params: {
+        name: string;
+        variant: string;
+    };
+    Error: ApiError<'invalid_variant' | 'unknown_connection' | 'failed_sync_variant_deletion'>;
+    Success: never;
+}>;
