17c5426ddbf71c34eb8be0d1c4455f5cb2ddbbb8
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2025-03-05 13:04:42 +0100

    everything was so bad

--- packages/utils/lib/axios.ts
+++ packages/utils/lib/axios.ts
@@ -7,8 +7,18 @@ import type { Agent as HttpAgent } from 'node:http';
 import type { Agent as HttpsAgent } from 'node:https';
 import https from 'node:https';
 
-export let httpAgent: HttpProxyAgent<string> | HttpAgent = new http.Agent();
-export let httpsAgent: HttpsProxyAgent<string> | HttpsAgent = new https.Agent();
+let agentOptions: http.AgentOptions = {};
+if (process.env['HTTP_AGENT_CONFIG']) {
+    try {
+        agentOptions = JSON.parse(process.env['HTTP_AGENT_CONFIG']) as http.AgentOptions;
+        console.warn('HTTP_AGENT_CONFIG', agentOptions);
+    } catch (err) {
+        console.error('invalid HTTP_AGENT configuration', err);
+    }
+}
+
+export let httpAgent: HttpProxyAgent<string> | HttpAgent = new http.Agent(agentOptions);
+export let httpsAgent: HttpsProxyAgent<string> | HttpsAgent = new https.Agent(agentOptions);
 
 function agentToJson(this: unknown): string {
     if (this instanceof http.Agent) {
@@ -29,16 +39,15 @@ const hasHttpProxy = process.env['http_proxy'] || process.env['HTTP_PROXY'];
 const hasHttpsProxy = process.env['https_proxy'] || process.env['HTTPS_PROXY'];
 
 if (hasHttpProxy) {
-    httpAgent = new HttpProxyAgent(hasHttpProxy);
+    httpAgent = new HttpProxyAgent(hasHttpProxy, agentOptions);
 }
 if (hasHttpsProxy) {
-    httpsAgent = new HttpsProxyAgent(hasHttpsProxy);
+    httpsAgent = new HttpsProxyAgent(hasHttpsProxy, agentOptions);
 }
 
 const config: AxiosRequestConfig = {
     httpAgent: httpAgent,
-    httpsAgent: httpsAgent,
-    ...(process.env['AXIOS_HTTP_AGENT'] ? JSON.parse(process.env['AXIOS_HTTP_AGENT']) : {})
+    httpsAgent: httpsAgent
 };
 
 if (hasHttpProxy || hasHttpsProxy) {
