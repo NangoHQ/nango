3401dd8e425f6f38b74340b5b10dde22e74862f9
Khaliq <khaliqgant@gmail.com>
2025-03-07 12:36:46 +0200

    add in ids section

--- packages/cli/lib/services/docs.service.ts
+++ packages/cli/lib/services/docs.service.ts
@@ -149,6 +149,7 @@ function requestParams(endpointType: string) {
             `- **limit:** \`(optional, integer)\` The maximum number of records to return per page. Defaults to 100.`,
             `- **cursor:** \`(optional, string)\` A marker used to fetch records modified after a specific point in time.If not provided, all records are returned.Each record includes a cursor value found in _nango_metadata.cursor.Save the cursor from the last record retrieved to track your sync progress.Use the cursor parameter together with the limit parameter to paginate through records.The cursor is more precise than modified_after, as it can differentiate between records with the same modification timestamp.`,
             `- **filter:** \`(optional, added | updated | deleted)\` Filter to only show results that have been added or updated or deleted.`,
+            `- **ids:** \`(optional, string[])\` An array of string containing a list of your records IDs. The list will be filtered to include only the records with a matching ID.`,
             ``
         );
     } else {
