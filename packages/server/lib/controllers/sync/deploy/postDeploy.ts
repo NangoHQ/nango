38119a1aae6600b94479302e9212c93fb059b997
Khaliq <khaliq@nango.dev>
2025-03-07 12:43:17 +0200

    fix(cli-deploy): concurrent deployment throws an error and ruins future (#3639)
    <!-- Describe the problem and your solution --> 
    Concurrent deploys break scripts in Nango:
    ```
    ➜  nango-integrations ➜  nango-integrations npx nango deploy dev --auto-confirm & npx nango deploy dev --auto-confirm  &
    [3] 46442
    [4] 46443
    ➜  nango-integrations Sync "dumb-a" is new. Nango will start syncing the corresponding data for 1 existing connections.
    Sync "dumb-a" is new. Nango will start syncing the corresponding data for 1 existing connections.
    Error deploying the scripts with the following error: {
      "error": {
        "message": "An error occurred. Please contact support with this unique id: \"9bf968f6-4286-4e46-9b89-1403b36a5e4e\"",
        "code": "generic_error_support",
        "payload": "9bf968f6-4286-4e46-9b89-1403b36a5e4e"
      }
    }
    Error deploying the scripts with the following error: {
      "error": {
        "message": "An error occurred. Please contact support with this unique id: \"6414eac2-e3f5-4161-bf73-6195b6fd6f19\"",
        "code": "generic_error_support",
        "payload": "6414eac2-e3f5-4161-bf73-6195b6fd6f19"
      }
    }
    ```
    
    This PR adds in a lock for up to 1 minute so we can catch concurrent
    deploys and return a nice error message to the user:
    ```
    ➜  nango-integrations npx nango deploy dev --auto-confirm & npx nango deploy dev --auto-confirm  &
    [3] 95665
    [4] 95666
    ➜  nango-integrations Sync "dumb-a" is new. Nango will start syncing the corresponding data for 1 existing connections.
    Sync "dumb-b" is new. Nango will start syncing the corresponding data for 1 existing connections.
    Sync "dumb" is new. Nango will start syncing the corresponding data for 1 existing connections.
    Sync "dumb-a" is new. Nango will start syncing the corresponding data for 1 existing connections.
    Sync "dumb-b" is new. Nango will start syncing the corresponding data for 1 existing connections.
    Sync "dumb" is new. Nango will start syncing the corresponding data for 1 existing connections.
    Error deploying the scripts with the following error: {
      "error": {
        "message": "A deployment is already in progress. Please wait for the current deployment to finish.",
        "code": "concurrent_deployment",
        "payload": {}
      }
    }
    ```
    
    ![image](https://github.com/user-attachments/assets/881e6280-8968-4280-b29a-738e12fa5420)
    
    
    <!-- Issue ticket number and link (if applicable) -->
    
    <!-- Testing instructions (skip if just adding/editing providers) -->

--- packages/server/lib/controllers/sync/deploy/postDeploy.ts
+++ packages/server/lib/controllers/sync/deploy/postDeploy.ts
@@ -1,9 +1,11 @@
 import { requireEmptyQuery, zodErrorToHTTP } from '@nangohq/utils';
 import type { PostDeploy } from '@nangohq/types';
 import { asyncWrapper } from '../../../utils/asyncWrapper.js';
-import { AnalyticsTypes, analytics, cleanIncomingFlow, deploy, errorManager, getAndReconcileDifferences } from '@nangohq/shared';
+import { AnalyticsTypes, analytics, NangoError, cleanIncomingFlow, deploy, errorManager, getAndReconcileDifferences } from '@nangohq/shared';
 import { getOrchestrator } from '../../../utils/utils.js';
 import { logContextGetter } from '@nangohq/logs';
+import type { Lock } from '@nangohq/kvstore';
+import { getLocking } from '@nangohq/kvstore';
 import { validationWithNangoYaml as validation } from './validation.js';
 
 const orchestrator = getOrchestrator();
@@ -24,6 +26,26 @@ export const postDeploy = asyncWrapper<PostDeploy>(async (req, res) => {
     const body: PostDeploy['Body'] = val.data;
     const { environment, account } = res.locals;
 
+    // we don't allow concurrent deploys so we need to lock this
+    // and reject this deploy if there is already a deploy in progress
+    const locking = await getLocking();
+    const ttlMs = 60 * 1000;
+    const lockKey = `lock:deployService:deploy:${account.id}:${environment.id}`;
+    let lock: Lock | undefined;
+
+    try {
+        lock = await locking.acquire(lockKey, ttlMs);
+    } catch {
+        const logCtx = await logContextGetter.create({ operation: { type: 'deploy', action: 'custom' } }, { account, environment });
+        const error = new NangoError('concurrent_deployment');
+
+        await logCtx.error('Failed to deploy scripts', { error });
+        await logCtx.failed();
+
+        errorManager.errResFromNangoErr(res, error);
+        return;
+    }
+
     const {
         success,
         error,
@@ -41,6 +63,9 @@ export const postDeploy = asyncWrapper<PostDeploy>(async (req, res) => {
     });
 
     if (!success || !syncConfigDeployResult) {
+        if (lock) {
+            await locking.release(lock);
+        }
         errorManager.errResFromNangoErr(res, error);
         return;
     }
@@ -58,6 +83,9 @@ export const postDeploy = asyncWrapper<PostDeploy>(async (req, res) => {
             orchestrator
         });
         if (!success) {
+            if (lock) {
+                await locking.release(lock);
+            }
             res.status(500).send({
                 error: { code: 'server_error', message: 'There was an error deploying syncs, please check the activity tab and report this issue to support' }
             });
@@ -66,6 +94,10 @@ export const postDeploy = asyncWrapper<PostDeploy>(async (req, res) => {
     }
 
     void analytics.trackByEnvironmentId(AnalyticsTypes.SYNC_DEPLOY_SUCCESS, environment.id);
+
+    if (lock) {
+        await locking.release(lock);
+    }
 
     res.send(syncConfigDeployResult.result);
 });
