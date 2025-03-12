c60702ee326aa394811d4c515a0657585c5d4bfb
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2025-03-12 10:49:17 +0100

    fix(webhooks): use same retry and logging as proxy (#3662)
    ## Changes
    
    Fixes
    https://linear.app/nango/issue/NAN-2768/use-new-retry-strategy-for-webhook
    
    - Webhooks use same retry and logging as proxy
    So we have unified logging and updated exponential backoff
    
    - Bonus fix body logging

--- packages/utils/lib/express/requestLoggerMiddleware.ts
+++ packages/utils/lib/express/requestLoggerMiddleware.ts
@@ -14,10 +14,11 @@ export function requestLoggerMiddleware({ logger }: { logger: Logger }): Handler
         res.on('finish', () => {
             const route = req.route?.path || req.originalUrl;
             const msg = `${req.method} ${route} -> ${res.statusCode}`;
+            const contentType = res.getHeader('content-type') as string | undefined;
             if (res.statusCode >= 500) {
-                logger.error(colors.red(msg), res.getHeader('content-type') === 'application/json' ? resBody : '[buffer]');
+                logger.error(colors.red(msg), contentType && contentType.startsWith('application/json') ? resBody : '[buffer]');
             } else if (res.statusCode >= 400) {
-                logger.warning?.(colors.yellow(msg), res.getHeader('content-type') === 'application/json' ? resBody : '[buffer]');
+                logger.warning?.(colors.yellow(msg), contentType && contentType.startsWith('application/json') ? resBody : '[buffer]');
             } else {
                 logger.info(msg);
             }
