d4e1b6b193097bb3e8c1cad4ee02dc8fb7e91a66
Khaliq <khaliqgant@gmail.com>
2025-03-24 10:29:16 +0200

    add bitdefender verification script

---
+++ packages/server/lib/hooks/connection/providers/bitdefender/credentials-verification.ts
@@ -0,0 +1,20 @@
+import type { APIDetailsResponse } from './types.js';
+import type { InternalNango as Nango } from '../../credentials-verification-script.js';
+
+export default async function execute(nango: Nango) {
+    const { provider_config_key: providerConfigKey } = nango.getConnection();
+
+    const bitInput = {
+        jsonrpc: '2.0',
+        method: 'getApiKeyDetails',
+        params: {},
+        id: Math.floor(Math.random() * 1000000)
+    };
+
+    await nango.proxy<APIDetailsResponse>({
+        method: 'POST',
+        endpoint: '/v1.0/jsonrpc/general',
+        providerConfigKey,
+        data: bitInput
+    });
+}
