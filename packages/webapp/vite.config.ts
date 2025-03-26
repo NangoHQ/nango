fa88453c759eab1c5d15c94014e073e9db1e0dab
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2025-03-26 12:03:53 +0100

    feat(ui): webpack -> vite

---
+++ packages/webapp/vite.config.ts
@@ -0,0 +1,20 @@
+import path from 'node:path';
+
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+
+import type { PluginOption } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), svgr()] as PluginOption[],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            // https://github.com/tabler/tabler-icons/issues/1233
+            // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
+            '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs'
+        }
+    }
+});
