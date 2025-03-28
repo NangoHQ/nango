bc2e383635086a4c9cdf90594ab781cc222ea2cf
Thomas Bonnin <233326+TBonnin@users.noreply.github.com>
2025-03-28 09:49:20 -0400

    fix: do not abort an abort task :)

--- packages/orchestrator/lib/abort.ts
+++ packages/orchestrator/lib/abort.ts
@@ -10,6 +10,12 @@ export async function scheduleAbortTask({ scheduler, task }: { scheduler: Schedu
     if (aborted.isErr()) {
         return Err(aborted.error);
     }
+
+    // we don't want to abort an abort task
+    if (aborted.value.isAbort() || aborted.value.isSyncAbort()) {
+        return Err(`Task is already an abort task`);
+    }
+
     const reason = aborted.value.state === 'EXPIRED' ? 'Expired execution' : 'Execution was cancelled';
     const payload: JsonValue = {
         type: 'abort',
