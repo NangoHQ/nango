113075cdd8fc438f2987f8b2b79113c0a0496cc2
Khaliq <khaliqgant@gmail.com>
2025-03-31 13:43:56 +0300

    restore examples

--- packages/node-client/bin/examples.js
+++ packages/node-client/bin/examples.js
@@ -1,14 +1,10 @@
 import { Nango } from '../dist/index.js';
-const secretKey = '679de721-795a-4ecf-ba16-52a4f630a43f';
-let nango = new Nango({ host: 'http://localhost:3003', secretKey });
-//nango
-//.getConnection('github-dev', 2)
-//.then((connections) => {
-//console.log(connections);
-//})
-//.catch((err: unknown) => {
-//console.log(err.response?.data || err.message);
-//});
-nango.getScriptsConfig('openai').then((config) => {
-    console.log(config);
-});
+let nango = new Nango({ host: 'http://localhost:3003' });
+nango
+    .getConnection('github-dev', 2)
+    .then((connections) => {
+        console.log(connections);
+    })
+    .catch((err: unknown) => {
+        console.log(err.response?.data || err.message);
+    });
