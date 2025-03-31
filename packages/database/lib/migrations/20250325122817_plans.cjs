def86bbddc0d767e376e8b59213d4c95794f6e45
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2025-03-31 10:27:16 +0200

    feat(plans): migrations

---
+++ packages/database/lib/migrations/20250325122817_plans.cjs
@@ -0,0 +1,30 @@
+/**
+ * @param {import('knex').Knex} knex
+ */
+exports.up = async function (knex) {
+    await knex.raw(
+        `CREATE TABLE "plans" (
+	"id" serial,
+	"account_id" int4 NOT NULL REFERENCES _nango_accounts(id) ON DELETE CASCADE,
+	"created_at" timestamptz DEFAULT NOW(),
+	"updated_at" timestamptz DEFAULT NOW(),
+	"name" varchar(256) NOT NULL,
+	"trial_start_at" timestamptz,
+	"trial_end_at" timestamptz,
+	"trial_extension_count" int2 DEFAULT 0,
+	"max_connection_with_scripts" int2 DEFAULT 3,
+	"max_environments" int2 DEFAULT 2,
+	"min_sync_frequency" int4 DEFAULT 86400,
+	"has_sync_variants" bool DEFAULT true,
+	PRIMARY KEY ("id")
+)`
+    );
+    await knex.raw(`CREATE UNIQUE INDEX "idx_account_id" ON "plans" USING BTREE ("account_id")`);
+};
+
+/**
+ * @param {import('knex').Knex} knex
+ */
+exports.down = async function (knex) {
+    await knex.raw(`DROP TABLE "plans"`);
+};
