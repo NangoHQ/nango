642742f107ddebfc44330d8c88901006bdbb2cfc
Hassan_Wari <thebigstormtech@gmail.com>
2025-04-01 21:31:43 +0300

    remove var

--- packages/cli/lib/services/compile.service.ts
+++ packages/cli/lib/services/compile.service.ts
@@ -222,13 +222,11 @@ async function compile({
         return null;
     }
 
-    const syncsAndActions = [...providerConfiguration.syncs, ...providerConfiguration.actions];
-
     const onEventScripts: { name: string; type: ScriptTypeLiteral; event: string }[] = Object.entries(providerConfiguration.onEventScripts).flatMap(
         ([event, scripts]) => scripts.map((name) => ({ name, type: 'on-event' as const, event }))
     );
 
-    const config = [...syncsAndActions, ...onEventScripts].find((script) => script.name === file.baseName);
+    const config = [...providerConfiguration.syncs, ...providerConfiguration.actions, ...onEventScripts].find((script) => script.name === file.baseName);
 
     const absoluteFullPath = path.resolve(fullPath, file.inputPath);
 
