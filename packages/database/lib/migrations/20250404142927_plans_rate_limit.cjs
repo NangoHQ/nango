5903322d5977e83adfda918fc5537462b447f49d
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2025-04-07 11:16:38 +0200

    review

--- packages/database/lib/migrations/20250404142927_plans_rate_limit.cjs
+++ packages/database/lib/migrations/20250404142927_plans_rate_limit.cjs
@@ -2,12 +2,12 @@
  * @param {import('knex').Knex} knex
  */
 exports.up = async function (knex) {
-    await knex.raw(`ALTER TABLE "plans" ADD COLUMN "api_rate_limit_points" int4 NOT NULL DEFAULT 1000`);
+    await knex.raw(`ALTER TABLE "plans" ADD COLUMN "api_rate_limit_size" varchar(4) NOT NULL DEFAULT 'm'`);
 };
 
 /**
  * @param {import('knex').Knex} knex
  */
 exports.down = async function (knex) {
-    await knex.raw(`ALTER TABLE "plans" DROP COLUMN "api_rate_limit_points"`);
+    await knex.raw(`ALTER TABLE "plans" DROP COLUMN "api_rate_limit_size"`);
 };
