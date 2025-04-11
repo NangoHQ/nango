82919555d9ea76337ed13018c22f9d9f8036475e
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2025-04-11 15:32:48 +0200

    fix(ui): switch size and alignement

--- packages/webapp/src/pages/Integrations/providerConfigKey/Show.tsx
+++ packages/webapp/src/pages/Integrations/providerConfigKey/Show.tsx
@@ -149,7 +149,7 @@ export const ShowIntegration: React.FC = () => {
                     <div className="flex gap-2 items-center">
                         <div className="flex gap-3 items-center">
                             <ErrorCircle icon="clock" variant="warning" />
-                            <Tag variant={'warning'}>{isTrialOver ? 'Trial expired' : 'Trial Plan'}</Tag>
+                            <Tag variant={'warning'}>{isTrialOver ? 'Trial expired' : 'Trial'}</Tag>
                             {!isTrialOver && <span className="text-white font-semibold">{daysRemaining} days left!</span>}
                         </div>
                         <div className="text-grayscale-400 text-sm">Actions & syncs are subject to a 2-week trial</div>
