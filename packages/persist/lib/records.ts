3a7570927aa3ed9067d91af0b5b949a525155153
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2025-04-18 11:35:59 +0200

    forgot the s

--- packages/persist/lib/records.ts
+++ packages/persist/lib/records.ts
@@ -125,7 +125,7 @@ export async function persistRecords({
         const total = allModifiedKeys.size + summary.unchangedKeys.length;
 
         void logCtx.info(
-            `Successfully batched ${total} record${allModifiedKeys.size > 1 ? 's' : ''} (${allModifiedKeys.size} modified) for model ${baseModel} `,
+            `Successfully batched ${total} record${total > 1 ? 's' : ''} (${allModifiedKeys.size} modified) for model ${baseModel} `,
             { persistType },
             {
                 persistResults: {
