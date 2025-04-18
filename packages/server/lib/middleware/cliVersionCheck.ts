e17443a301d17b17fd62e13f7338261f342931ce
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2025-04-18 16:54:59 +0200

    working

--- packages/server/lib/middleware/cliVersionCheck.ts
+++ packages/server/lib/middleware/cliVersionCheck.ts
@@ -1,14 +1,15 @@
-import { NANGO_VERSION } from '@nangohq/shared';
-import { getLogger } from '@nangohq/utils';
-import type { Request, Response, NextFunction } from 'express';
-
 import semver from 'semver';
+
+import { NANGO_VERSION, getLogger } from '@nangohq/utils';
+
+import type { ApiError } from '@nangohq/types';
+import type { NextFunction, Request, Response } from 'express';
 
 const logger = getLogger('CliVersionCheck');
 
 const VERSION_REGEX = /nango-cli\/([0-9.]+)/;
 export function cliMinVersion(minVersion: string) {
-    return (req: Request, _: Response, next: NextFunction) => {
+    return (req: Request, res: Response, next: NextFunction) => {
         const userAgent = req.headers['user-agent'];
         if (!userAgent) {
             // Could be strictly enforced
@@ -25,28 +26,30 @@ export function cliMinVersion(minVersion: string) {
 
         if (semver.gt(minVersion, match[1])) {
             logger.info(`This endpoint requires a CLI version >= ${minVersion} (current: ${match[0]})`);
-            // res.status(400).send({
-            //     error: { code: 'invalid_cli_version', message: `This endpoint requires a CLI version >= ${minVersion} (current: ${match[0]})` }
-            // });
-            // return;
+            res.status(400).send({
+                error: { code: 'invalid_cli_version', message: `This endpoint requires a CLI version >= ${minVersion} (current: ${match[0]})` }
+            });
+            return;
         }
 
         next();
     };
 }
 
+/**
+ * Enforce maximum CLI version.
+ * It's only there to prevent accidental upgrade on enterprise, it's otherwise a bit unpractical in case of rollback
+ */
 export function cliMaxVersion() {
-    return (req: Request, res: Response, next: NextFunction) => {
+    return (req: Request, res: Response<ApiError<'invalid_cli_version'>>, next: NextFunction) => {
         const userAgent = req.headers['user-agent'];
         if (!userAgent) {
-            // Could be strictly enforced
             next();
             return;
         }
 
         const match = userAgent.match(VERSION_REGEX);
         if (!match || match.length <= 1 || !match[1]) {
-            // Could be strictly enforced
             next();
             return;
         }
