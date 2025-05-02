41dcf29c7e6da66f04f645ed2db6dca2da68e711
kaposke <guilhermebassa@gmail.com>
2025-05-02 12:27:16 -0300

    feat(webapp): jump to specific time in logs

--- packages/webapp/src/utils/logs.tsx
+++ packages/webapp/src/utils/logs.tsx
@@ -1,6 +1,11 @@
+import { addMilliseconds, parse } from 'date-fns';
+
 import type { SearchOperations, SearchOperationsPeriod } from '@nangohq/types';
-import { addMilliseconds } from 'date-fns';
-import type { DateRange } from 'react-day-picker';
+
+export interface DateRange {
+    from: Date;
+    to?: Date;
+}
 
 export function getLogsUrl(
     options: Omit<
@@ -34,7 +39,7 @@ export function getLogsUrl(
 
 export function slidePeriod(period: DateRange | SearchOperationsPeriod): DateRange {
     const now = new Date();
-    let from = new Date(period.from!);
+    let from = new Date(period.from);
     let to = new Date(period.to!);
     const sliding = now.getTime() - to.getTime();
     to = addMilliseconds(to, sliding);
@@ -45,12 +50,12 @@ export function slidePeriod(period: DateRange | SearchOperationsPeriod): DateRan
 
 // Define presets
 export const presets = [
-    { name: 'last5m', label: 'Last 5 minutes' },
-    { name: 'last1h', label: 'Last hour' },
-    { name: 'last24h', label: 'Last 24 hours' },
-    { name: 'last3d', label: 'Last 3 days' },
-    { name: 'last7d', label: 'Last 7 days' },
-    { name: 'last14d', label: 'Last 14 days' }
+    { name: 'last5m', label: 'Last 5 minutes', shortLabel: '5m' },
+    { name: 'last1h', label: 'Last hour', shortLabel: '1h' },
+    { name: 'last24h', label: 'Last 24 hours', shortLabel: '24h' },
+    { name: 'last3d', label: 'Last 3 days', shortLabel: '3d' },
+    { name: 'last7d', label: 'Last 7 days', shortLabel: '1w' },
+    { name: 'last14d', label: 'Last 14 days', shortLabel: '2w' }
 ] as const;
 export type PresetNames = (typeof presets)[number]['name'];
 export type Preset = (typeof presets)[number];
@@ -90,14 +95,39 @@ export function getPresetRange(preset: PresetNames): DateRange {
 }
 
 export function matchPresetFromRange(range: DateRange): Preset | null {
-    const minutes = (range.to!.getTime() - range.from!.getTime()) / 1000 / 60;
+    const minutes = (range.to!.getTime() - range.from.getTime()) / 1000 / 60;
     for (const preset of presets) {
         const tmp = getPresetRange(preset.name);
-        const tmpMinutes = (tmp.to!.getTime() - tmp.from!.getTime()) / 1000 / 60;
+        const tmpMinutes = (tmp.to!.getTime() - tmp.from.getTime()) / 1000 / 60;
         if (tmpMinutes === minutes) {
             return preset;
         }
     }
 
     return null;
+}
+
+export function parseDateRange(input: string, dateTimeFormat: string): { dateRange: DateRange | null; error: string | null } {
+    // Validate input format
+    const dateFormatRegex = /^[A-Z][a-z]{2} \d{1,2}, \d{1,2}:\d{2} - [A-Z][a-z]{2} \d{1,2}, \d{1,2}:\d{2}$/;
+    if (!dateFormatRegex.test(input)) {
+        return { dateRange: null, error: 'Invalid format' };
+    }
+
+    const [from, to] = input.split('-').map((d) => d.trim());
+
+    const range = {
+        from: parse(from, dateTimeFormat, new Date()),
+        to: parse(to, dateTimeFormat, new Date())
+    };
+
+    if (isNaN(range.from.getTime()) || isNaN(range.to.getTime())) {
+        return { dateRange: null, error: 'Invalid date' };
+    }
+
+    if (range.from > range.to) {
+        return { dateRange: null, error: 'From date must be before to date' };
+    }
+
+    return { dateRange: range, error: null };
 }
