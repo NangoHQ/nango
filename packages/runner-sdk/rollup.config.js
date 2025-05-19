aedc5fb047cb74ec71b0cfe28ca85476e27d3d25
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2025-05-19 16:47:14 +0200

    clean up

--- packages/runner-sdk/rollup.config.js
+++ packages/runner-sdk/rollup.config.js
@@ -4,6 +4,12 @@ import typescript from '@rollup/plugin-typescript';
 import { defineConfig } from 'rollup';
 import dts from 'rollup-plugin-dts';
 
+/**
+ * This script compile a special CJS bundle for the script
+ * The main issue is that we are executing scripts in CJS format, but our whole codebase is ESM
+ * So we need a CJS bundle that contains the createAction (and others function)
+ * and d.ts file to make the local compilation works.
+ */
 export default defineConfig([
     // JS bundle
     {
