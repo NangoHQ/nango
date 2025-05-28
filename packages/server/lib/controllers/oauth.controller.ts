f97243c97838b9d06ef17b23319aea03d34315e8
Hassan_Wari <85742599+hassan254-prog@users.noreply.github.com>
2025-05-28 10:22:02 +0300

    feat(authorization): skip undefined/empty query parameter

--- packages/server/lib/controllers/oauth.controller.ts
+++ packages/server/lib/controllers/oauth.controller.ts
@@ -633,6 +633,20 @@ class OAuthController {
                     });
                 }
 
+                if (provider.authorization_url_skip_empty) {
+                    const url = new URL(authorizationUri);
+                    const queryParams = new URLSearchParams(url.search);
+                    // Remove any keys with undefined or empty string values
+                    for (const [key, value] of queryParams.entries()) {
+                        if (value === '') {
+                            queryParams.delete(key);
+                        }
+                    }
+
+                    url.search = queryParams.toString();
+                    authorizationUri = url.toString();
+                }
+
                 void logCtx.info('Redirecting', {
                     authorizationUri,
                     providerConfigKey,
