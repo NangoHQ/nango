379cc3283255a0b4781b2f3e7a21a21ab9ec2b34
Ross McEwan <ross@nango.dev>
2025-07-01 09:58:51 +0100

    fix: add primary key to _nango_db_config

--- packages/database/lib/migrations/20250701094655_add_primary_key_to_oauth_sessions.cjs
+++ packages/database/lib/migrations/20250701094655_add_primary_key_to_oauth_sessions.cjs
@@ -3,6 +3,7 @@
  */
 exports.up = async function (knex) {
     await knex.raw(`ALTER TABLE _nango_oauth_sessions ADD CONSTRAINT pk_nango_oauth_sessions_id PRIMARY KEY USING INDEX _nango_oauth_sessions_id_unique;`);
+    await knex.raw('ALTER TABLE _nango_db_config ADD PRIMARY KEY (encryption_key_hash);');
 };
 
 /**
