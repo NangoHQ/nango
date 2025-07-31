487e503d620f30446ce0c23befb865b1c4cc87e3
Hassan_Wari <85742599+hassan254-prog@users.noreply.github.com>
2025-07-31 15:23:18 +0300

    fixes after merging to master

--- packages/server/lib/controllers/config.controller.ts
+++ packages/server/lib/controllers/config.controller.ts
@@ -1,6 +1,7 @@
 import crypto from 'crypto';
 
 import { configService, connectionService, errorManager, getGlobalWebhookReceiveUrl, getProvider, getProviders } from '@nangohq/shared';
+import { report } from '@nangohq/utils';
 
 import type { RequestLocals } from '../utils/express.js';
 import type { Integration as ProviderIntegration, IntegrationWithCreds } from '@nangohq/shared';
@@ -14,28 +15,33 @@ class ConfigController {
             return;
         }
 
-        const sharedCredentialsCache = await configService.loadSharedCredentialsCache();
+        try {
+            const sharedCredentialsCache = await configService.loadSharedCredentialsCache();
 
-        const list = Object.entries(providers).map((providerProperties) => {
-            const [provider, properties] = providerProperties;
-            // check if provider has nango's preconfigured credentials
-            const preConfiguredInfo = sharedCredentialsCache[provider];
-            const isPreConfigured = preConfiguredInfo ? preConfiguredInfo.preConfigured : false;
-            const preConfiguredScopes = preConfiguredInfo ? preConfiguredInfo.scopes : [];
+            const list = Object.entries(providers).map((providerProperties) => {
+                const [provider, properties] = providerProperties;
+                // check if provider has nango's preconfigured credentials
+                const preConfiguredInfo = sharedCredentialsCache[provider];
+                const isPreConfigured = preConfiguredInfo ? preConfiguredInfo.preConfigured : false;
+                const preConfiguredScopes = preConfiguredInfo ? preConfiguredInfo.scopes : [];
 
-            return {
-                name: provider,
-                displayName: properties.display_name,
-                defaultScopes: properties.default_scopes,
-                authMode: properties.auth_mode,
-                categories: properties.categories,
-                docs: properties.docs,
-                preConfigured: isPreConfigured,
-                preConfiguredScopes: preConfiguredScopes
-            };
-        });
-        const sortedList = list.sort((a, b) => a.name.localeCompare(b.name));
-        res.status(200).send(sortedList);
+                return {
+                    name: provider,
+                    displayName: properties.display_name,
+                    defaultScopes: properties.default_scopes,
+                    authMode: properties.auth_mode,
+                    categories: properties.categories,
+                    docs: properties.docs,
+                    preConfigured: isPreConfigured,
+                    preConfiguredScopes: preConfiguredScopes
+                };
+            });
+            const sortedList = list.sort((a, b) => a.name.localeCompare(b.name));
+            res.status(200).send(sortedList);
+        } catch (err) {
+            report(err);
+            res.status(500).send({ error: { code: 'server_error' } });
+        }
     }
 
     /**
