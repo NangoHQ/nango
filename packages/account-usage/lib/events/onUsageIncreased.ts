f3e935c2462af46c20f39dd7de8e6b798501e052
kaposke <guilhermebassa@gmail.com>
2025-08-01 17:45:01 -0300

    Simplify span

--- packages/account-usage/lib/events/onUsageIncreased.ts
+++ packages/account-usage/lib/events/onUsageIncreased.ts
@@ -31,71 +31,71 @@ export async function onUsageIncreased({
         }
     });
 
-    await tracer.scope().activate(span, async () => {
-        try {
-            if (delta === 0) {
-                return;
-            }
-
-            let resolvedPlan: DBPlan | undefined = plan;
-
-            if (!resolvedPlan) {
-                const planResult = await getPlan(db.knex, { accountId });
-
-                if (planResult.isErr()) {
-                    throw new Error(`Error getting plan for account ${accountId}: ${planResult.error}`);
-                }
-
-                resolvedPlan = planResult.value;
-            }
-
-            const locking = await getLocking();
-            await locking.withLock(`lock:account-usage:${accountId}:${metric}`, 10000, 10000, async () => {
-                const accountUsageTracker = await getAccountUsageTracker();
-
-                const limit = accountUsageTracker.getLimit(resolvedPlan, metric);
-                const currentUsage = await accountUsageTracker.getUsage({ accountId: accountId, metric });
-
-                if (limit === null || currentUsage === null) {
-                    return;
-                }
-
-                const crossedLimit = currentUsage - delta < limit && currentUsage >= limit;
-                const crossed80Percent = currentUsage - delta < limit * 0.8 && currentUsage >= limit * 0.8;
-
-                if (!crossedLimit && !crossed80Percent) {
-                    return;
-                }
-
-                const account = await accountService.getAccountById(db.knex, accountId);
-
-                if (!account) {
-                    throw new Error(`Account not found for accountId ${accountId}`);
-                }
-
-                const usage = await accountUsageTracker.getAccountMetricsUsage(account, resolvedPlan);
-                const users = await userService.getUsersByAccountId(accountId);
-
-                await Promise.all(
-                    users.map((user) => {
-                        // Full limit reached prioritized in case usage jumps directly to it
-                        if (crossedLimit) {
-                            return sendUsageLimitReachedEmail({ user, account, usage, triggeringMetric: metric });
-                        }
-
-                        return sendUsageNearLimitEmail({ user, account, usage, triggeringMetric: metric });
-                    })
-                );
-
-                return;
-            });
-        } catch (err) {
-            report(new Error(`Error in onUsageIncreased: ${stringifyError(err)}`), {
-                accountId,
-                metric,
-                delta,
-                plan
-            });
+    try {
+        if (delta === 0) {
+            return;
         }
-    });
+
+        let resolvedPlan: DBPlan | undefined = plan;
+
+        if (!resolvedPlan) {
+            const planResult = await getPlan(db.knex, { accountId });
+
+            if (planResult.isErr()) {
+                throw new Error(`Error getting plan for account ${accountId}: ${planResult.error}`);
+            }
+
+            resolvedPlan = planResult.value;
+        }
+
+        const locking = await getLocking();
+        await locking.withLock(`lock:account-usage:${accountId}:${metric}`, 10000, 10000, async () => {
+            const accountUsageTracker = await getAccountUsageTracker();
+
+            const limit = accountUsageTracker.getLimit(resolvedPlan, metric);
+            const currentUsage = await accountUsageTracker.getUsage({ accountId: accountId, metric });
+
+            if (limit === null || currentUsage === null) {
+                return;
+            }
+
+            const crossedLimit = currentUsage - delta < limit && currentUsage >= limit;
+            const crossed80Percent = currentUsage - delta < limit * 0.8 && currentUsage >= limit * 0.8;
+
+            if (!crossedLimit && !crossed80Percent) {
+                return;
+            }
+
+            const account = await accountService.getAccountById(db.knex, accountId);
+
+            if (!account) {
+                throw new Error(`Account not found for accountId ${accountId}`);
+            }
+
+            const usage = await accountUsageTracker.getAccountMetricsUsage(account, resolvedPlan);
+            const users = await userService.getUsersByAccountId(accountId);
+
+            await Promise.all(
+                users.map((user) => {
+                    // Full limit reached prioritized in case usage jumps directly to it
+                    if (crossedLimit) {
+                        return sendUsageLimitReachedEmail({ user, account, usage, triggeringMetric: metric });
+                    }
+
+                    return sendUsageNearLimitEmail({ user, account, usage, triggeringMetric: metric });
+                })
+            );
+
+            return;
+        });
+    } catch (err) {
+        report(new Error(`Error in onUsageIncreased: ${stringifyError(err)}`), {
+            accountId,
+            metric,
+            delta,
+            plan
+        });
+    } finally {
+        span.finish();
+    }
 }
