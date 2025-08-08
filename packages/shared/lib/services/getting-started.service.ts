9a9bf8d0414edd0d5fe20995633321165e79dfc7
kaposke <guilhermebassa@gmail.com>
2025-08-08 14:37:00 -0300

    get endpoint with on-demand creation

---
+++ packages/shared/lib/services/getting-started.service.ts
@@ -0,0 +1,173 @@
+import db from '@nangohq/database';
+import { getProvider } from '@nangohq/providers';
+import { Err, Ok } from '@nangohq/utils';
+
+import configService from './config.service.js';
+import sharedCredentialsService from './shared-credentials.service.js';
+
+import type {
+    DBConnection,
+    DBEnvironment,
+    DBGettingStartedMeta,
+    DBGettingStartedProgress,
+    DBUser,
+    GettingStartedOutput,
+    IntegrationConfig
+} from '@nangohq/types';
+import type { Result } from '@nangohq/utils';
+
+export async function createGettingStartedMeta(accountId: number, environmentId: number): Promise<Result<DBGettingStartedMeta>> {
+    try {
+        const result = await db.knex
+            .from<DBGettingStartedMeta>('getting_started_meta')
+            .insert({ account_id: accountId, environment_id: environmentId })
+            .returning('*');
+
+        if (!result[0]) {
+            return Err(new Error('failed_to_create_getting_started_meta'));
+        }
+
+        return Ok(result[0]);
+    } catch (err) {
+        return Err(new Error('failed_to_create_getting_started_meta', { cause: err }));
+    }
+}
+
+export async function createGettingStartedProgress(userId: number, metaId: number): Promise<Result<DBGettingStartedProgress>> {
+    try {
+        const result = await db.knex
+            .from<DBGettingStartedProgress>('getting_started_progress')
+            .insert({ user_id: userId, getting_started_meta_id: metaId })
+            .returning('*');
+
+        const gettingStartedProgress = result[0];
+
+        if (!gettingStartedProgress) {
+            return Err(new Error('failed_to_create_getting_started_progress'));
+        }
+
+        return Ok(gettingStartedProgress);
+    } catch (err) {
+        return Err(new Error('failed_to_create_getting_started_progress', { cause: err }));
+    }
+}
+
+export async function getByUserId(userId: number): Promise<Result<GettingStartedOutput>> {
+    const result = await db.knex
+        .select<{
+            getting_started_progress: DBGettingStartedProgress;
+            environment: DBEnvironment;
+            integration: IntegrationConfig;
+            connection: DBConnection | null;
+        }>('progress.* as progress', 'env.* as environment', 'config.* as integration', 'conn.* as connection')
+        .from('getting_started_progress as progress')
+        .leftJoin('getting_started_meta as meta', 'meta.id', 'progress.getting_started_meta_id')
+        .leftJoin('_nango_environments as env', 'env.id', 'meta.environment_id')
+        .leftJoin('_nango_configs as config', 'config.id', 'meta.integration_id')
+        .leftJoin('_nango_connections as conn', 'conn.id', 'progress.connection_id')
+        .where({ user_id: userId })
+        .first();
+
+    if (!result) {
+        return Err(new Error('getting_started_progress_not_found'));
+    }
+
+    return Ok({
+        meta: {
+            integration: result.integration,
+            environment: result.environment
+        },
+        connection: result.connection ?? null,
+        step: result.getting_started_progress.step
+    });
+}
+
+export function updateByUserId(userId: number, data: Partial<DBGettingStartedProgress>) {
+    return db.knex.from<DBGettingStartedProgress>('getting_started_progress').where({ user_id: userId }).update(data);
+}
+
+/**
+ * Gets getting started progress for the user, or creates it if it doesn't exist.
+ */
+export async function getOrCreateByUser(user: DBUser, currentEnvironmentId: number): Promise<Result<GettingStartedOutput>> {
+    const existingResult = await getByUserId(user.id);
+
+    if (existingResult.isOk()) {
+        return Ok(existingResult.value);
+    }
+
+    const gettingStartedMeta = await getOrCreateGettingStartedMeta(user.account_id, currentEnvironmentId);
+
+    if (gettingStartedMeta.isErr()) {
+        return Err(gettingStartedMeta.error);
+    }
+
+    const createdResult = await createGettingStartedProgress(user.id, gettingStartedMeta.value.id);
+
+    if (createdResult.isErr()) {
+        return Err(createdResult.error);
+    }
+
+    return getByUserId(createdResult.value.user_id);
+}
+
+async function getOrCreateGettingStartedMeta(accountId: number, currentEnvironmentId: number): Promise<Result<DBGettingStartedMeta>> {
+    const existingMeta = await db.knex.from<DBGettingStartedMeta>('getting_started_meta').where({ account_id: accountId }).first();
+
+    if (existingMeta) {
+        return Ok(existingMeta);
+    }
+
+    const googleCalendarIntegrationId = await getOrCreateGoogleCalendarIntegration(currentEnvironmentId);
+
+    if (googleCalendarIntegrationId.isErr()) {
+        return Err(googleCalendarIntegrationId.error);
+    }
+
+    try {
+        const newMeta = await createGettingStartedMeta(accountId, currentEnvironmentId);
+
+        if (newMeta.isErr()) {
+            return Err(new Error('failed_to_create_getting_started_meta'));
+        }
+
+        return Ok(newMeta.value);
+    } catch (err) {
+        return Err(new Error('failed_to_create_getting_started_meta', { cause: err }));
+    }
+}
+
+async function getOrCreateGoogleCalendarIntegration(environmentId: number): Promise<Result<number>> {
+    const existingIntegrationId = await configService.getIdByProviderConfigKey(environmentId, 'google-calendar-getting-started');
+
+    if (existingIntegrationId) {
+        return Ok(existingIntegrationId);
+    }
+
+    const PROVIDER_NAME = 'google-calendar';
+
+    const provider = getProvider(PROVIDER_NAME);
+    if (!provider) {
+        return Err(new Error('google_calendar_provider_not_found'));
+    }
+
+    const newIntegration = await sharedCredentialsService.createPreprovisionedProvider({
+        providerName: PROVIDER_NAME,
+        environment_id: environmentId,
+        provider,
+        display_name: 'Google Calendar (Getting Started)',
+        unique_key: 'google-calendar-getting-started'
+    });
+
+    if (newIntegration.isErr()) {
+        return Err(newIntegration.error);
+    }
+
+    const id = newIntegration.value.id ?? null;
+
+    if (!id) {
+        return Err(new Error('failed_to_create_google_calendar_integration'));
+    }
+
+    return Ok(id);
+}
