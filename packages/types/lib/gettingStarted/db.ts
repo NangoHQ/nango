9daeabd08c783d3f20bed81207798571b7d3a891
kaposke <guilhermebassa@gmail.com>
2025-08-08 14:04:41 -0300

    Track environment and integration per account

--- packages/types/lib/gettingStarted/db.ts
+++ packages/types/lib/gettingStarted/db.ts
@@ -1,10 +1,23 @@
 import type { Timestamps } from '../db.js';
 
+/**
+ * One per account. Holds environment and integration used for getting started flow for that account.
+ */
+export interface DBGettingStartedMeta extends Timestamps {
+    id: number;
+    account_id: number;
+    environment_id: number;
+    integration_id: number | null;
+}
+
+/**
+ * One per user. Holds progress for each user in the getting started flow.
+ */
 export interface DBGettingStartedProgress extends Timestamps {
     id: number;
+    getting_started_meta_id: number;
     user_id: number;
-    integration_id: number;
-    demo_connection_id: number | null;
+    connection_id: number | null;
     step: number;
     complete: boolean;
 }
