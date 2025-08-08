0ea8f335d622fe41f0f4ec288f3c868fb7eedf91
Thomas Bonnin <233326+TBonnin@users.noreply.github.com>
2025-08-08 11:33:09 -0400

    fix(billing): send the total connections count to Orb
    Let Orb handle the proration

--- packages/server/lib/crons/usage.ts
+++ packages/server/lib/crons/usage.ts
@@ -46,6 +46,7 @@ export async function exec(): Promise<void> {
 
         try {
             await billing.exportBillableConnections();
+            await billing.exportProratedConnections();
             await billing.exportActiveConnections();
             await observability.exportConnectionsMetrics();
             await observability.exportRecordsMetrics();
@@ -98,8 +99,31 @@ const billing = {
     exportBillableConnections: async (): Promise<void> => {
         await tracer.trace<Promise<void>>('nango.cron.exportUsage.billing.connections', async (span) => {
             try {
+                const res = await connectionService.billableConnections();
+                if (res.isErr()) {
+                    throw res.error;
+                }
+
+                const events = res.value.map<BillingMetric>(({ accountId, count }) => {
+                    return { type: 'total_connections', value: count, properties: { accountId } };
+                });
+
+                const sendRes = usageBilling.addAll(events);
+                if (sendRes.isErr()) {
+                    throw sendRes.error;
+                }
+            } catch (err) {
+                span.setTag('error', err);
+                report(new Error('cron_failed_to_export_billable_connections', { cause: err }));
+            }
+        });
+    },
+    // TODO: remove once we confirmed the total connections metric (proration done by Orb) above is correct
+    exportProratedConnections: async (): Promise<void> => {
+        await tracer.trace<Promise<void>>('nango.cron.exportUsage.billing.proratedconnections', async (span) => {
+            try {
                 const now = new Date();
-                const res = await connectionService.billableConnections(now);
+                const res = await connectionService.proratedConnections(now);
                 if (res.isErr()) {
                     throw res.error;
                 }
@@ -114,7 +138,7 @@ const billing = {
                 }
             } catch (err) {
                 span.setTag('error', err);
-                report(new Error('cron_failed_to_export_billable_connections', { cause: err }));
+                report(new Error('cron_failed_to_export_prorated_connections', { cause: err }));
             }
         });
     },
