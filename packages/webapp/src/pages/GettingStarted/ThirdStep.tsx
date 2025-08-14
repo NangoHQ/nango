f53e892784c3baab92b40360bd2dee41843d4a79
kaposke <guilhermebassa@gmail.com>
2025-08-14 14:11:48 -0300

    add analytics

--- packages/webapp/src/pages/GettingStarted/ThirdStep.tsx
+++ packages/webapp/src/pages/GettingStarted/ThirdStep.tsx
@@ -2,7 +2,12 @@ import { useEffect, useRef } from 'react';
 
 import LinkWithIcon from '../../components/LinkWithIcon';
 
-export const ThirdStep = () => {
+interface ThirdStepProps {
+    onDocumentationLinkClicked: (link: string) => void;
+    onSlackLinkClicked: () => void;
+}
+
+export const ThirdStep = ({ onDocumentationLinkClicked, onSlackLinkClicked }: ThirdStepProps) => {
     const componentRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
@@ -20,17 +25,25 @@ export const ThirdStep = () => {
             <p className="text-text-secondary text-sm">Add nango to your app in less than an hour.</p>
 
             <div className="mt-5 flex flex-col">
-                <LinkWithIcon to="https://docs.nango.dev/getting-started/quickstart/embed-in-your-app" type="external">
+                <LinkWithIcon
+                    to="https://docs.nango.dev/getting-started/quickstart/embed-in-your-app"
+                    type="external"
+                    onClick={() => onDocumentationLinkClicked('embed-in-your-app')}
+                >
                     Embed the auth flow in your app
                 </LinkWithIcon>
-                <LinkWithIcon to="https://docs.nango.dev/guides/syncs/overview" type="external">
+                <LinkWithIcon
+                    to="https://docs.nango.dev/guides/syncs/overview"
+                    type="external"
+                    onClick={() => onDocumentationLinkClicked('explore-syncs-actions-webhooks')}
+                >
                     Explore Syncs, Actions & Webhooks
                 </LinkWithIcon>
             </div>
 
             <p className="text-text-secondary text-sm mt-5 flex flex-row gap-1">
                 Questions? We&apos;re happy to help on the
-                <LinkWithIcon to="https://nango.dev/slack" type="external">
+                <LinkWithIcon to="https://nango.dev/slack" type="external" onClick={onSlackLinkClicked}>
                     Slack Community
                 </LinkWithIcon>
             </p>
