ec8f5206d71f0fbf203e148b1c365051d2efe646
kaposke <guilhermebassa@gmail.com>
2025-08-15 14:58:29 -0300

    Fix deleteIntegration test

--- packages/server/lib/controllers/integrations/uniqueKey/deleteIntegration.integration.test.ts
+++ packages/server/lib/controllers/integrations/uniqueKey/deleteIntegration.integration.test.ts
@@ -1,11 +1,8 @@
 import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 
-import db from '@nangohq/database';
 import { gettingStartedService, seeders } from '@nangohq/shared';
 
 import { isSuccess, runServer, shouldBeProtected } from '../../../utils/tests.js';
-
-import type { DBGettingStartedMeta } from '@nangohq/types';
 
 let api: Awaited<ReturnType<typeof runServer>>;
 
@@ -50,15 +47,11 @@ describe(`DELETE ${endpoint}`, () => {
 
         const metaResult = await gettingStartedService.getOrCreateMeta(account.id, env.id);
         expect(metaResult.isOk()).toBe(true);
-        const meta = metaResult.unwrap();
 
         const res = await api.fetch(endpoint, { method: 'DELETE', token: env.secret_key, params: { uniqueKey: 'google-calendar-getting-started' } });
         isSuccess(res.json);
-        expect(res.json).toStrictEqual<typeof res.json>({
-            success: true
-        });
 
-        const metaAfter = await db.knex.from<DBGettingStartedMeta>('getting_started_meta').where({ id: meta.id }).first();
-        expect(metaAfter).toBeUndefined();
+        const metaAfter = await gettingStartedService.getMetaByAccountId(account.id);
+        expect(metaAfter).toBeNull();
     });
 });
