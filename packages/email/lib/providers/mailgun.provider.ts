d49ce07d196a4b115338bcb1951acd73248cb748
Khaliq <khaliqgant@gmail.com>
2025-09-03 12:07:54 +0200

    test hard coded

--- packages/email/lib/providers/mailgun.provider.ts
+++ packages/email/lib/providers/mailgun.provider.ts
@@ -19,7 +19,7 @@ export class MailgunEmailProvider implements EmailProvider<MessagesSendResult> {
     }
 
     async send(email: string, subject: string, html: string): Promise<MessagesSendResult> {
-        return this.client.messages.create(envs.SMTP_DOMAIN, {
+        return this.client.messages.create('email.nango.dev', {
             from: envs.SMTP_FROM,
             to: [email],
             subject,
