557a2ee79dd750c74ed3fcda08a7f88fc8a60223
kaposke <guilhermebassa@gmail.com>
2025-09-11 17:06:34 -0300

    Functional showWatermark and theme

--- packages/connect-ui/src/lib/theme.ts
+++ packages/connect-ui/src/lib/theme.ts
@@ -1,9 +1,8 @@
 import type { ConnectUIThemeSettings } from '@nangohq/types';
 
 export function setTheme(theme: ConnectUIThemeSettings) {
-    return;
     const root = document.documentElement;
 
     root.style.setProperty('--color-primary', theme.light.buttonBackground);
-    root.style.setProperty('--color-primary-foreground', theme.light.buttonText);
+    root.style.setProperty('--color-on-primary', theme.light.buttonText);
 }
