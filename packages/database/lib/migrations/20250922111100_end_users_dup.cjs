800d56bbd8e4e420ed2f8c49bf0ebb32d8fc694c
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2025-09-22 11:49:16 +0200

    coms

--- packages/database/lib/migrations/20250922111100_end_users_dup.cjs
+++ packages/database/lib/migrations/20250922111100_end_users_dup.cjs
@@ -7,7 +7,7 @@ exports.config = { transaction: false };
  */
 exports.up = async function (knex) {
     while (true) {
-        // Grab duplicated end user one by one for simplicity and atomicity
+        // Grab end user one by one for simplicity and atomicity
         const endUsers = await knex.raw(`SELECT
 			end_users.id,
 			COUNT(1) as total
@@ -24,7 +24,7 @@ exports.up = async function (knex) {
             break;
         }
 
-        // Grab connections for the duplicated end user
+        // Grab all connections for the end user that needs to be duplicated
         const connections = await knex.raw(`SELECT
 	row_to_json(end_users.*) as end_user, _nango_connections.id as connection_id
 FROM
@@ -35,7 +35,7 @@ FROM
         console.log('[endUser dup] changing', endUsers.rows[0].total, 'connections, for end user', endUsers.rows[0].id);
 
         // Insert a new end user for each connection
-        // And update the related connection
+        // And update the related connection with the new end_users.id
         for (const connection of connections.rows) {
             const inserted = await knex
                 .from('end_users')
