8843e28f1e8a7b88ae6b6525fc776f78e12b89be
kaposke <guilhermebassa@gmail.com>
2025-09-24 14:28:24 -0300

    Add links to header buttons

--- packages/webapp/src/components-v2/AppHeader.tsx
+++ packages/webapp/src/components-v2/AppHeader.tsx
@@ -1,4 +1,5 @@
 import { BookOpen } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
 import { Button } from './ui/button';
 import { SlackIcon } from '@/assets/SlackIcon';
@@ -6,14 +7,18 @@ import { SlackIcon } from '@/assets/SlackIcon';
 export const AppHeader: React.FC = () => {
     return (
         <header className="h-16 px-10 py-2.5 items-center flex justify-end gap-1.5">
-            <Button variant="secondary" size="sm">
-                <BookOpen />
-                Docs
-            </Button>
-            <Button variant="secondary" size="sm">
-                <SlackIcon />
-                Help
-            </Button>
+            <Link to="https://docs.nango.dev">
+                <Button variant="secondary" size="sm">
+                    <BookOpen />
+                    Docs
+                </Button>
+            </Link>
+            <Link to="https://nango.dev/slack">
+                <Button variant="secondary" size="sm">
+                    <SlackIcon />
+                    Help
+                </Button>
+            </Link>
         </header>
     );
 };
