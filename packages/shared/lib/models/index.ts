5b89a5597fc29acb084b60c3b59e94a4279d82fd
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2025-09-30 17:20:16 +0200

    o

--- packages/shared/lib/models/index.ts
+++ packages/shared/lib/models/index.ts
@@ -1,7 +1,7 @@
 export * from './Telemetry.js';
 export type * from './Generic.js';
 export type * from './Provider.js';
-export type * from './Auth.js';
+export * from './Auth.js';
 export * from './Sync.js';
 export type * from './Flow.js';
 export type * from './NangoConfig.js';
