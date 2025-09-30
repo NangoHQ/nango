258a70e5ea1b3a5effa68c8c3df7dd222ac8570c
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2025-09-30 17:19:13 +0200

    fix: clean up proxy types

--- packages/shared/lib/models/index.ts
+++ packages/shared/lib/models/index.ts
@@ -1,8 +1,7 @@
 export * from './Telemetry.js';
 export type * from './Generic.js';
 export type * from './Provider.js';
-export * from './Auth.js';
+export type * from './Auth.js';
 export * from './Sync.js';
 export type * from './Flow.js';
 export type * from './NangoConfig.js';
-export * from './Proxy.js';
