289048745266e9ecb7a2db2fbdf217f40687fa8b
Khaliq <khaliqgant@gmail.com>
2025-10-01 15:32:43 +0200

    grab the custom message correctly

--- packages/server/lib/controllers/auth/postTba.ts
+++ packages/server/lib/controllers/auth/postTba.ts
@@ -233,7 +233,7 @@ export const postPublicTbaAuthorization = asyncWrapper<PostPublicTbaAuthorizatio
             }
 
             const payload = customValidationResponse.error?.payload;
-            const message = typeof payload['error'] === 'string' ? payload['error'] : 'Connection failed validation';
+            const message = typeof payload['message'] === 'string' ? payload['message'] : 'Connection failed validation';
 
             res.status(400).send({
                 error: {
