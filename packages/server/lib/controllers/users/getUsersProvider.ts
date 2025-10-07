7a4b53dce4169d2aeed2e868258d8c41db7dd5e9
Ross McEwan <ross@nango.dev>
2025-10-07 17:33:53 +0200

    fix(internal): unsaved file

--- packages/server/lib/controllers/users/getUsersProvider.ts
+++ packages/server/lib/controllers/users/getUsersProvider.ts
@@ -6,13 +6,13 @@ import { zodErrorToHTTP } from '@nangohq/utils';
 import { userToAPI } from '../../formatters/user.js';
 import { asyncWrapper } from '../../utils/asyncWrapper.js';
 
-import type { GetUsers } from '@nangohq/types';
+import type { InternalGetUsers } from '@nangohq/types';
 
 const validationQuery = z.object({
     accountId: z.coerce.number()
 });
 
-export const getUsersProvider = asyncWrapper<GetUsers>(async (req, res) => {
+export const getUsersProvider = asyncWrapper<InternalGetUsers>(async (req, res) => {
     const valQuery = validationQuery.safeParse(req.query);
     if (!valQuery.success) {
         res.status(400).send({ error: { code: 'invalid_query_params', errors: zodErrorToHTTP(valQuery.error) } });
@@ -20,7 +20,6 @@ export const getUsersProvider = asyncWrapper<GetUsers>(async (req, res) => {
     }
 
     const users = await userService.getUsersByAccountId(valQuery.data.accountId);
-
     const usersFormatted = users.map(userToAPI);
 
     res.status(200).send({
