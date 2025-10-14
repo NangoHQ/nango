efbf35d955313de6edcd45061da532fec9e7c42d
Thomas Bonnin <233326+TBonnin@users.noreply.github.com>
2025-10-14 14:27:23 -0400

    chore: add usage max columns to plans table
    Values will be used to cap free account

---
+++ packages/database/lib/migrations/20251014191600_plans_usagev2_columns.cjs
@@ -0,0 +1,31 @@
+exports.config = { transaction: true };
+
+/**
+ * @param {import('knex').Knex} knex
+ */
+exports.up = async function (knex) {
+    await knex.raw(`
+        ALTER TABLE plans
+        ADD COLUMN IF NOT EXISTS proxy_max INTEGER,
+        ADD COLUMN IF NOT EXISTS function_executions_max INTEGER,
+        ADD COLUMN IF NOT EXISTS function_compute_gmbs_max INTEGER,
+        ADD COLUMN IF NOT EXISTS records_max INTEGER,
+        ADD COLUMN IF NOT EXISTS external_webhooks_max INTEGER,
+        ADD COLUMN IF NOT EXISTS function_logs_max INTEGER
+    `);
+
+    // set default values for existing free plans
+    await knex.raw(`
+        UPDATE plans
+        SET
+            proxy_max = 100000,
+            function_executions_max = 100000,
+            function_compute_gmbs_max = 50000000,
+            records_max = 100000,
+            external_webhooks_max = 100000,
+            function_logs_max = 100000
+        WHERE name = 'free'
+    `);
+};
+
+exports.down = async function () {};
