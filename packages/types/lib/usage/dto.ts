8bb3aa71c3d263b5018665302734ed448d3eb0d9
kaposke <guilhermebassa@gmail.com>
2025-10-27 18:08:38 -0300

    leftover type

--- packages/types/lib/usage/dto.ts
+++ packages/types/lib/usage/dto.ts
@@ -1,17 +1,3 @@
-import type { DBAccountUsage } from './db.js';
-
-export interface CreateAccountUsageDto {
-    accountId: DBAccountUsage['account_id'];
-    month: DBAccountUsage['month'];
-    actions?: DBAccountUsage['actions'];
-    activeRecords?: DBAccountUsage['active_records'];
-}
-
-export interface UpdateAccountUsageDto {
-    actions?: DBAccountUsage['actions'];
-    activeRecords?: DBAccountUsage['active_records'];
-}
-
 export interface MetricUsageSummary {
     label: string;
     usage: number;
