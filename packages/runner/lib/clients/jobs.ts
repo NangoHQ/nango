63ff0690bef9dbb2d5a5911bdaeb0c9591696e5a
Thomas Bonnin <233326+TBonnin@users.noreply.github.com>
2025-10-28 14:19:12 -0400

    fix(runner): error handling and retry in jobs client
    retry mechanism relies on a rejected promise so we cannot wrap the
    output with Result inside it.
    But we can catch in the jobs client functions so downstream code doesn't
    have to deal with exception but only Result

--- packages/runner/lib/clients/jobs.ts
+++ packages/runner/lib/clients/jobs.ts
@@ -20,79 +20,95 @@ class JobsClient {
     }
 
     async postHeartbeat({ taskId }: PostHeartbeat['Params']): Promise<Result<PostHeartbeat['Success']>> {
-        const resp = await httpFetch(`${this.baseUrl}/tasks/${taskId}/heartbeat`, {
-            method: 'POST'
-        });
-        if (!resp.ok) {
-            return Err(`postHeartbeat_failed`);
-        }
-        return Ok(undefined as PostHeartbeat['Success']);
-    }
-
-    async putTask({ taskId, nangoProps, error, output, telemetryBag }: PutTask['Body'] & PutTask['Params']): Promise<Result<PutTask['Success']>> {
-        const res = await retryWithBackoff(async () => {
-            const resp = await httpFetch(`${this.baseUrl}/tasks/${taskId}`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    nangoProps: nangoProps,
-                    ...(error ? { error, telemetryBag } : { output, telemetryBag })
-                })
-            });
-            if (!resp.ok) {
-                // If the response is request content too large
-                // we send a separate request to update the task with the error
-                if (resp.status === 413) {
-                    return this.putTask({
-                        taskId,
-                        nangoProps,
-                        error: {
-                            type: 'script_output_too_large',
-                            status: 413,
-                            payload: {
-                                message: 'Output is too large'
-                            }
-                        },
-                        telemetryBag: { customLogs: 0, proxyCalls: 0, durationMs: 0, memoryGb: 1 }
-                    });
-                }
-                return Err(`putTask_failed`);
-            }
-            return Ok(undefined as PutTask['Success']);
-        }, defaultRetryOptions);
-        return res as Result<PutTask['Success']>;
-    }
-
-    async postRegister({ nodeId, url }: PostRegister['Params'] & PostRegister['Body']): Promise<Result<PostRegister['Success']>> {
-        const res = await retryWithBackoff(async () => {
-            const resp = await httpFetch(`${this.baseUrl}/runners/${nodeId}/register`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ url })
-            });
-            if (!resp.ok) {
-                return Err(`PostRegister_failed`);
-            }
-            return Ok((await resp.json()) as PostRegister['Success']);
-        }, defaultRetryOptions);
-        return res as Result<PostRegister['Success']>;
-    }
-
-    async postIdle({ nodeId }: PostIdle['Params']): Promise<Result<PostIdle['Success']>> {
-        const res = await retryWithBackoff(async () => {
-            const resp = await httpFetch(`${this.baseUrl}/runners/${nodeId}/idle`, {
+        try {
+            const resp = await httpFetch(`${this.baseUrl}/tasks/${taskId}/heartbeat`, {
                 method: 'POST'
             });
             if (!resp.ok) {
-                return Err(`postIdle_failed`);
+                throw new Error('heartbeat_failed');
             }
-            return Ok((await resp.json()) as PostIdle['Success']);
-        }, defaultRetryOptions);
-        return res as Result<PostIdle['Success']>;
+            return Ok(undefined as PostHeartbeat['Success']);
+        } catch {
+            return Err(`heartbeat_failed`);
+        }
+    }
+
+    async putTask({ taskId, nangoProps, error, output, telemetryBag }: PutTask['Body'] & PutTask['Params']): Promise<Result<PutTask['Success']>> {
+        try {
+            const res = await retryWithBackoff(async () => {
+                const resp = await httpFetch(`${this.baseUrl}/tasks/${taskId}`, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        nangoProps: nangoProps,
+                        ...(error ? { error, telemetryBag } : { output, telemetryBag })
+                    })
+                });
+                if (!resp.ok) {
+                    // If the response is request content too large
+                    // we send a separate request to update the task with the error
+                    if (resp.status === 413) {
+                        return this.putTask({
+                            taskId,
+                            nangoProps,
+                            error: {
+                                type: 'script_output_too_large',
+                                status: 413,
+                                payload: {
+                                    message: 'Output is too large'
+                                }
+                            },
+                            telemetryBag: { customLogs: 0, proxyCalls: 0, durationMs: 0, memoryGb: 1 }
+                        });
+                    }
+                    throw new Error('put_task_failed');
+                }
+                return Ok(undefined as PutTask['Success']);
+            }, defaultRetryOptions);
+            return res as Result<PutTask['Success']>;
+        } catch {
+            return Err(`put_task_failed`);
+        }
+    }
+
+    async postRegister({ nodeId, url }: PostRegister['Params'] & PostRegister['Body']): Promise<Result<PostRegister['Success']>> {
+        try {
+            const res = await retryWithBackoff(async () => {
+                const resp = await httpFetch(`${this.baseUrl}/runners/${nodeId}/register`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ url })
+                });
+                if (!resp.ok) {
+                    throw new Error('register_failed');
+                }
+                return Ok((await resp.json()) as PostRegister['Success']);
+            }, defaultRetryOptions);
+            return res as Result<PostRegister['Success']>;
+        } catch {
+            return Err(`register_failed`);
+        }
+    }
+
+    async postIdle({ nodeId }: PostIdle['Params']): Promise<Result<PostIdle['Success']>> {
+        try {
+            const res = await retryWithBackoff(async () => {
+                const resp = await httpFetch(`${this.baseUrl}/runners/${nodeId}/idle`, {
+                    method: 'POST'
+                });
+                if (!resp.ok) {
+                    throw new Error(`idle_failed`);
+                }
+                return Ok((await resp.json()) as PostIdle['Success']);
+            }, defaultRetryOptions);
+            return res as Result<PostIdle['Success']>;
+        } catch {
+            return Err(`idle_failed`);
+        }
     }
 }
 
--- packages/runner/lib/register.ts
+++ packages/runner/lib/register.ts
@@ -7,14 +7,13 @@ import { logger } from './logger.js';
 import type { Result } from '@nangohq/utils';
 
 export async function register(): Promise<Result<void>> {
-    try {
-        if (envs.RUNNER_NODE_ID && envs.RUNNER_URL) {
-            await jobsClient.postRegister({ nodeId: envs.RUNNER_NODE_ID, url: envs.RUNNER_URL });
-        } else {
-            logger.info(`No registration: RUNNER_NODE_ID or RUNNER_URL not set`);
+    if (envs.RUNNER_NODE_ID && envs.RUNNER_URL) {
+        const res = await jobsClient.postRegister({ nodeId: envs.RUNNER_NODE_ID, url: envs.RUNNER_URL });
+        if (res.isErr()) {
+            return Err(res.error);
         }
-        return Ok(undefined);
-    } catch (err) {
-        return Err(new Error('failed to register runner', { cause: err }));
+    } else {
+        logger.info(`No registration: RUNNER_NODE_ID or RUNNER_URL not set`);
     }
+    return Ok(undefined);
 }
