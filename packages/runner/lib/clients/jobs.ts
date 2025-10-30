e37bea05e1f631460d3999a64907455b97453b60
Thomas Bonnin <233326+TBonnin@users.noreply.github.com>
2025-10-30 16:39:22 -0400

    refactor(runner): move retry in httpFetch

--- packages/runner/lib/clients/jobs.ts
+++ packages/runner/lib/clients/jobs.ts
@@ -1,4 +1,4 @@
-import { Err, Ok, retryWithBackoff } from '@nangohq/utils';
+import { Err, Ok } from '@nangohq/utils';
 
 import { httpFetch } from './http.js';
 import { jobsServiceUrl } from '../env.js';
@@ -20,95 +20,82 @@ class JobsClient {
     }
 
     async postHeartbeat({ taskId }: PostHeartbeat['Params']): Promise<Result<PostHeartbeat['Success']>> {
-        try {
-            const resp = await httpFetch(`${this.baseUrl}/tasks/${taskId}/heartbeat`, {
-                method: 'POST'
-            });
-            if (!resp.ok) {
-                throw new Error('heartbeat_failed');
-            }
-            return Ok(undefined as PostHeartbeat['Success']);
-        } catch {
+        const resp = await httpFetch(`${this.baseUrl}/tasks/${taskId}/heartbeat`, {
+            method: 'POST'
+        });
+        if (!resp.ok) {
             return Err(`heartbeat_failed`);
         }
+        return Ok(undefined as PostHeartbeat['Success']);
     }
 
     async putTask({ taskId, nangoProps, error, output, telemetryBag }: PutTask['Body'] & PutTask['Params']): Promise<Result<PutTask['Success']>> {
-        try {
-            const res = await retryWithBackoff(async () => {
-                const resp = await httpFetch(`${this.baseUrl}/tasks/${taskId}`, {
-                    method: 'PUT',
-                    headers: {
-                        'Content-Type': 'application/json'
+        const resp = await httpFetch(
+            `${this.baseUrl}/tasks/${taskId}`,
+            {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    nangoProps: nangoProps,
+                    ...(error ? { error, telemetryBag } : { output, telemetryBag })
+                })
+            },
+            defaultRetryOptions
+        );
+        if (!resp.ok) {
+            // If the response is request content too large
+            // we send a separate request to update the task with the error
+            if (resp.status === 413) {
+                return this.putTask({
+                    taskId,
+                    nangoProps,
+                    error: {
+                        type: 'script_output_too_large',
+                        status: 413,
+                        payload: {
+                            message: 'Output is too large'
+                        }
                     },
-                    body: JSON.stringify({
-                        nangoProps: nangoProps,
-                        ...(error ? { error, telemetryBag } : { output, telemetryBag })
-                    })
+                    telemetryBag: { customLogs: 0, proxyCalls: 0, durationMs: 0, memoryGb: 1 }
                 });
-                if (!resp.ok) {
-                    // If the response is request content too large
-                    // we send a separate request to update the task with the error
-                    if (resp.status === 413) {
-                        return this.putTask({
-                            taskId,
-                            nangoProps,
-                            error: {
-                                type: 'script_output_too_large',
-                                status: 413,
-                                payload: {
-                                    message: 'Output is too large'
-                                }
-                            },
-                            telemetryBag: { customLogs: 0, proxyCalls: 0, durationMs: 0, memoryGb: 1 }
-                        });
-                    }
-                    throw new Error('put_task_failed');
-                }
-                return Ok(undefined as PutTask['Success']);
-            }, defaultRetryOptions);
-            return res as Result<PutTask['Success']>;
-        } catch {
+            }
             return Err(`put_task_failed`);
         }
+        return Ok(undefined as PutTask['Success']);
     }
 
     async postRegister({ nodeId, url }: PostRegister['Params'] & PostRegister['Body']): Promise<Result<PostRegister['Success']>> {
-        try {
-            const res = await retryWithBackoff(async () => {
-                const resp = await httpFetch(`${this.baseUrl}/runners/${nodeId}/register`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({ url })
-                });
-                if (!resp.ok) {
-                    throw new Error('register_failed');
-                }
-                return Ok((await resp.json()) as PostRegister['Success']);
-            }, defaultRetryOptions);
-            return res as Result<PostRegister['Success']>;
-        } catch {
+        const resp = await httpFetch(
+            `${this.baseUrl}/runners/${nodeId}/register`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ url })
+            },
+            defaultRetryOptions
+        );
+        if (!resp.ok) {
             return Err(`register_failed`);
         }
+        return Ok((await resp.json()) as PostRegister['Success']);
     }
 
     async postIdle({ nodeId }: PostIdle['Params']): Promise<Result<PostIdle['Success']>> {
-        try {
-            const res = await retryWithBackoff(async () => {
-                const resp = await httpFetch(`${this.baseUrl}/runners/${nodeId}/idle`, {
-                    method: 'POST'
-                });
-                if (!resp.ok) {
-                    throw new Error(`idle_failed`);
-                }
-                return Ok((await resp.json()) as PostIdle['Success']);
-            }, defaultRetryOptions);
-            return res as Result<PostIdle['Success']>;
-        } catch {
+        const resp = await httpFetch(
+            `${this.baseUrl}/runners/${nodeId}/idle`,
+            {
+                method: 'POST'
+            },
+            defaultRetryOptions
+        );
+        if (!resp.ok) {
             return Err(`idle_failed`);
         }
+        return Ok((await resp.json()) as PostIdle['Success']);
     }
 }
 
