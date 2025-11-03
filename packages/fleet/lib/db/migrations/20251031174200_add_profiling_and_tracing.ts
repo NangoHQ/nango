6c3982f2d37cac3b4d12756d52468b70217566e6
Ross McEwan <ross@nango.dev>
2025-11-03 16:24:00 +0100

    fix(fleet): update migration files

--- packages/fleet/lib/db/migrations/20251031174200_add_profiling_and_tracing.ts
+++ packages/fleet/lib/db/migrations/20251031174200_add_profiling_and_tracing.ts
@@ -3,15 +3,12 @@ import { NODES_TABLE } from '../../models/nodes.js';
 import { NODE_CONFIG_OVERRIDES_TABLE } from '../../models/node_config_overrides.js';
 
 export async function up(knex: Knex): Promise<void> {
-    await knex.raw(`ALTER TABLE ${NODES_TABLE} ADD COLUMN is_tracing_enabled boolean NOT NULL DEFAULT false`);
-    await knex.raw(`ALTER TABLE ${NODES_TABLE} ADD COLUMN is_profiling_enabled boolean NOT NULL DEFAULT false`);
-    await knex.raw(`ALTER TABLE ${NODE_CONFIG_OVERRIDES_TABLE} ADD COLUMN is_tracing_enabled boolean NOT NULL DEFAULT false`);
-    await knex.raw(`ALTER TABLE ${NODE_CONFIG_OVERRIDES_TABLE} ADD COLUMN is_profiling_enabled boolean NOT NULL DEFAULT false`);
+    await knex.raw(`ALTER TABLE ${NODES_TABLE}
+      ADD COLUMN IF NOT EXISTS is_tracing_enabled boolean NOT NULL DEFAULT false,
+      ADD COLUMN IF NOT EXISTS is_profiling_enabled boolean NOT NULL DEFAULT false`);
+    await knex.raw(`ALTER TABLE ${NODE_CONFIG_OVERRIDES_TABLE}
+      ADD COLUMN IF NOT EXISTS is_tracing_enabled boolean NOT NULL DEFAULT false,
+      ADD COLUMN IF NOT EXISTS is_profiling_enabled boolean NOT NULL DEFAULT false`);
 }
 
-export async function down(knex: Knex): Promise<void> {
-    await knex.raw(`ALTER TABLE ${NODES_TABLE} DROP COLUMN is_tracing_enabled`);
-    await knex.raw(`ALTER TABLE ${NODES_TABLE} DROP COLUMN is_profiling_enabled`);
-    await knex.raw(`ALTER TABLE ${NODE_CONFIG_OVERRIDES_TABLE} DROP COLUMN is_tracing_enabled`);
-    await knex.raw(`ALTER TABLE ${NODE_CONFIG_OVERRIDES_TABLE} DROP COLUMN is_profiling_enabled`);
-}
+export async function down(): Promise<void> {}
--- packages/fleet/lib/db/migrations/20251101124200_remove_not_nullable_from_profiling_and_tracing.ts
+++ packages/fleet/lib/db/migrations/20251101124200_remove_not_nullable_from_profiling_and_tracing.ts
@@ -9,10 +9,4 @@ export async function up(knex: Knex): Promise<void> {
     `);
 }
 
-export async function down(knex: Knex): Promise<void> {
-    await knex.raw(`
-        ALTER TABLE ${NODE_CONFIG_OVERRIDES_TABLE} 
-        ALTER COLUMN is_tracing_enabled SET NOT NULL,
-        ALTER COLUMN is_profiling_enabled SET NOT NULL
-    `);
-}
+export async function down(): Promise<void> {}
