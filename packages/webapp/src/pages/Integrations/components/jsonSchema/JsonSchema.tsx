70a595effe3bceab81b44771b13fa0eb4d690c7c
kaposke <guilhermebassa@gmail.com>
2025-12-11 15:42:15 -0300

    Support unions

--- packages/webapp/src/pages/Integrations/components/jsonSchema/JsonSchema.tsx
+++ packages/webapp/src/pages/Integrations/components/jsonSchema/JsonSchema.tsx
@@ -1,11 +1,11 @@
 import * as Collapsible from '@radix-ui/react-collapsible';
 import { ChevronDown } from 'lucide-react';
 
-import { isArraySchema, isObjectSchema, isPrimitiveSchema } from './utils';
+import { isAnyOfSchema, isArraySchema, isObjectSchema, isOneOfSchema, typeToString } from './utils';
 import { CatalogBadge } from '../CatalogBadge';
 import { IntegrationsBadge } from '../IntegrationsBadge';
 
-import type { JSONSchema7 } from 'json-schema';
+import type { JSONSchema7, JSONSchema7Type } from 'json-schema';
 
 export const JsonSchemaTopLevelObject: React.FC<{ schema: JSONSchema7 }> = ({ schema }) => {
     if (!isObjectSchema(schema) || !schema.properties) {
@@ -24,8 +24,8 @@ export const JsonSchemaTopLevelObject: React.FC<{ schema: JSONSchema7 }> = ({ sc
 };
 
 const JsonSchema: React.FC<{ name: string; schema: JSONSchema7; isArray?: boolean }> = ({ name, schema, isArray = false }) => {
-    if (isPrimitiveSchema(schema)) {
-        return <JsonSchemaPrimitive name={name} schema={schema} isArray={isArray} />;
+    if (isOneOfSchema(schema) || isAnyOfSchema(schema)) {
+        return <JsonSchemaOneOf name={name} schema={schema} isArray={isArray} />;
     }
 
     if (isObjectSchema(schema)) {
@@ -36,67 +36,88 @@ const JsonSchema: React.FC<{ name: string; schema: JSONSchema7; isArray?: boolea
         return <JsonSchema name={name} schema={schema.items as JSONSchema7} isArray={true} />;
     }
 
-    return <p className="text-text-tertiary text-body-small-regular">Not implemented: {schema.type}</p>;
+    return <JsonSchemaGenericInfo name={name} type={typeToString(schema, isArray)} description={schema.description} defaultValue={schema.default} />;
 };
 
-const JsonSchemaGenericInfo: React.FC<{ name: string; schema: JSONSchema7; isArray?: boolean }> = ({ name, schema, isArray = false }) => {
-    const type = typeof schema.type === 'string' ? `${schema.type}${isArray ? '[]' : ''}` : '';
-    const defaultString = typeof schema.default === 'string' || typeof schema.default === 'number' ? String(schema.default) : '';
-
+const JsonSchemaGenericInfo: React.FC<{ name: string; description?: string | undefined; type: string; defaultValue?: JSONSchema7Type | undefined }> = ({
+    name,
+    description,
+    type,
+    defaultValue
+}) => {
+    const defaultString =
+        typeof defaultValue === 'string' || typeof defaultValue === 'number' || typeof defaultValue === 'boolean' ? String(defaultValue) : null;
     return (
         <div className="w-full flex flex-row items-center justify-between gap-1.5">
             <div className="flex flex-col gap-1.5">
                 <span className="text-text-primary text-body-small-semi">{name}</span>
-                {schema.description && <p className="text-text-tertiary text-body-small-medium">{schema.description}</p>}
+                {description && <p className="text-text-tertiary text-body-small-medium">{description}</p>}
                 {defaultString && <IntegrationsBadge label="Default">{defaultString}</IntegrationsBadge>}
             </div>
             <CatalogBadge>{type}</CatalogBadge>
         </div>
     );
 };
-
-const JsonSchemaPrimitive: React.FC<{ name: string; schema: JSONSchema7; isArray?: boolean }> = ({ name, schema, isArray = false }) => {
-    if (!isPrimitiveSchema(schema) || typeof schema.type !== 'string') {
-        throw new Error('Expected primitive schema.');
-    }
-
-    return <JsonSchemaGenericInfo name={name} schema={schema} isArray={isArray} />;
-};
-
 const JsonSchemaObject: React.FC<{ name: string; schema: JSONSchema7; isArray?: boolean }> = ({ name, schema, isArray = false }) => {
     if (!isObjectSchema(schema)) {
         throw new Error('Expected object schema.');
     }
 
     const properties = schema.properties || {};
-    const propertyCount = Object.keys(properties).length;
 
     return (
         <div className="flex flex-col gap-3">
-            <JsonSchemaGenericInfo name={name} schema={schema} isArray={isArray} />
-            <Collapsible.Root>
-                <Collapsible.Trigger disabled={propertyCount === 0} asChild>
-                    <div className="group w-full p-4 flex flex-row items-center justify-between bg-bg-surface border border-transparent rounded focus-default cursor-pointer transition-all hover:bg-bg-elevated hover:border-bg-surface data-[state=open]:bg-bg-elevated data-[state=open]:border data-[state=open]:border-b-0 data-[state=open]:rounded-b-none data-[state=open]:border-bg-surface">
-                        <span className="text-text-primary text-body-small-semi grow">Properties {'{}'}</span>
-                        <div className="inline-flex gap-1 items-center">
-                            <span className="text-text-primary text-body-small-medium">{propertyCount}</span>
-                            <ChevronDown className="size-4 text-icon-primary group-data-[state=open]:rotate-180" />
-                        </div>
-                    </div>
-                </Collapsible.Trigger>
-                <Collapsible.Content asChild>
-                    <div className="p-4 pt-0 rounded-b border border-t-0 border-bg-surface">
-                        <div className="flex flex-col rounded border border-bg-surface">
-                            {Object.entries(properties).map(([name, property]) => (
-                                <div key={name} className="p-4 border-b border-bg-surface last:border-b-0">
-                                    <JsonSchema name={name} schema={property as JSONSchema7} isArray={isArray} />
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                </Collapsible.Content>
-            </Collapsible.Root>
+            <JsonSchemaGenericInfo name={name} type={typeToString(schema, isArray)} description={schema.description} />
+            <CollapsibleProperties properties={properties} />
         </div>
+    );
+};
+
+const JsonSchemaOneOf: React.FC<{ name: string; schema: JSONSchema7; isArray?: boolean }> = ({ name, schema, isArray = false }) => {
+    if (!isOneOfSchema(schema) && !isAnyOfSchema(schema)) {
+        throw new Error('Expected oneOf or anyOf schema.');
+    }
+
+    const schemas = schema.oneOf || schema.anyOf || [];
+
+    return (
+        <div className="flex flex-col gap-3">
+            <JsonSchemaGenericInfo name={name} type={typeToString(schema, isArray)} description={schema.description} defaultValue={schema.default} />
+            <div className="flex flex-col gap-3">
+                {schemas.map((s, index) => (
+                    <>
+                        <CollapsibleProperties key={index} properties={(s as JSONSchema7).properties} />
+                        {/* <p className="text-center text-text-secondary text-body-extra-small-semi last:hidden">OR</p> */}
+                    </>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+const CollapsibleProperties: React.FC<{ properties: JSONSchema7['properties'] }> = ({ properties }) => {
+    const propertyCount = properties ? Object.keys(properties).length : 0;
+    return (
+        <Collapsible.Root disabled={propertyCount === 0}>
+            <Collapsible.Trigger asChild>
+                <div className="group w-full p-4 flex flex-row items-center justify-between bg-bg-surface border border-transparent rounded focus-default cursor-pointer transition-all hover:border-border-default data-[state=open]:border data-[state=open]:border-b-0 data-[state=open]:rounded-b-none data-[state=open]:border-border-muted">
+                    <span className="text-text-primary text-body-small-semi grow">Properties {'{}'}</span>
+                    <div className="inline-flex gap-1 items-center">
+                        <span className="text-text-primary text-body-small-medium">{propertyCount}</span>
+                        <ChevronDown className="size-4 text-icon-primary group-data-[state=open]:rotate-180" />
+                    </div>
+                </div>
+            </Collapsible.Trigger>
+            <Collapsible.Content asChild>
+                <div className="p-4 pt-0 rounded-b bg-bg-surface border border-t-0 border-border-muted">
+                    {Object.entries(properties || {}).map(([name, property]) => (
+                        <div key={name} className="p-4 border-b border-border-muted last:border-b-0">
+                            <JsonSchema name={name} schema={property as JSONSchema7} />
+                        </div>
+                    ))}
+                </div>
+            </Collapsible.Content>
+        </Collapsible.Root>
     );
 };
 
--- packages/webapp/src/pages/Integrations/components/jsonSchema/utils.ts
+++ packages/webapp/src/pages/Integrations/components/jsonSchema/utils.ts
@@ -15,3 +15,52 @@ export function isArraySchema(schema: JSONSchema7 | null): boolean {
 export function isComplexSchema(schema: JSONSchema7 | null): boolean {
     return isObjectSchema(schema) || isArraySchema(schema);
 }
+
+export function isNullSchema(schema: JSONSchema7 | null): boolean {
+    return schema?.type === 'null';
+}
+
+export function isOneOfSchema(schema: JSONSchema7 | null): boolean {
+    return schema?.oneOf !== undefined;
+}
+
+export function isAnyOfSchema(schema: JSONSchema7 | null): boolean {
+    return schema?.anyOf !== undefined;
+}
+
+export function typeToString(schema: JSONSchema7 | null, isArray: boolean): string {
+    if (!schema) {
+        return 'unknown';
+    }
+    const { type, anyOf, oneOf, enum: enumValues, const: constValue } = schema;
+    if (!type && !anyOf && !oneOf) {
+        return 'unknown';
+    }
+
+    if (anyOf || oneOf) {
+        const schemas = anyOf || oneOf || [];
+        const types = schemas.map((s) => typeToString(s as JSONSchema7, false)).join(' | ');
+        return `(${types})${isArray ? '[]' : ''}`;
+    }
+
+    if (Array.isArray(type)) {
+        const types = type.map((t) => t.toString()).join(' | ');
+        if (isArray) {
+            return `(${types})${isArray ? '[]' : ''}`;
+        }
+        return types;
+    }
+
+    if (enumValues && isPrimitiveSchema(schema)) {
+        if (type === 'string') {
+            return enumValues.map((e) => `"${String(e as string)}"`).join(' | ');
+        }
+        return enumValues.map((e) => String(e as number | boolean)).join(' | ');
+    }
+
+    if (constValue) {
+        return JSON.stringify(constValue);
+    }
+
+    return `${type}${isArray ? '[]' : ''}`;
+}
