018003c69166148ba9fab9d36c3219c91e890e36
Ross McEwan <ross@nango.dev>
2026-01-08 17:08:45 -0300

    fix(migration): try knex.raw

--- packages/database/lib/migrations/20260108145100_add_function_runtime_flags.cjs
+++ packages/database/lib/migrations/20260108145100_add_function_runtime_flags.cjs
@@ -4,16 +4,19 @@ exports.config = { transaction: false };
  * @param {import('knex').Knex} knex
  */
 exports.up = async function (knex) {
-    await knex.schema.raw(`
+    await knex.raw(`
         CREATE TYPE function_runtime AS ENUM (
             'runner',
             'lambda'
-        )
+        );
     `);
-    await knex.schema.raw(`ALTER TABLE plans ADD COLUMN IF NOT EXISTS sync_function_runtime function_runtime DEFAULT 'runner'`);
-    await knex.schema.raw(`ALTER TABLE plans ADD COLUMN IF NOT EXISTS action_function_runtime function_runtime DEFAULT 'runner'`);
-    await knex.schema.raw(`ALTER TABLE plans ADD COLUMN IF NOT EXISTS webhook_function_runtime function_runtime DEFAULT 'runner'`);
-    await knex.schema.raw(`ALTER TABLE plans ADD COLUMN IF NOT EXISTS on_event_function_runtime function_runtime DEFAULT 'runner'`);
+    await knex.raw(`
+        ALTER TABLE plans
+        ADD COLUMN IF NOT EXISTS sync_function_runtime function_runtime DEFAULT 'runner',
+        ADD COLUMN IF NOT EXISTS action_function_runtime function_runtime DEFAULT 'runner',
+        ADD COLUMN IF NOT EXISTS webhook_function_runtime function_runtime DEFAULT 'runner',
+        ADD COLUMN IF NOT EXISTS on_event_function_runtime function_runtime DEFAULT 'runner'
+    `);
 };
 
 /**
