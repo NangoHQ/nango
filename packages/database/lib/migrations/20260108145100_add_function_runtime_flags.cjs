fd4e5648710700eb9b69d8f7c3332737e9f9081d
Ross McEwan <ross@nango.dev>
2026-01-08 17:33:10 -0300

    fix(migration): safe retry of create type

--- packages/database/lib/migrations/20260108145100_add_function_runtime_flags.cjs
+++ packages/database/lib/migrations/20260108145100_add_function_runtime_flags.cjs
@@ -4,19 +4,21 @@ exports.config = { transaction: false };
  * @param {import('knex').Knex} knex
  */
 exports.up = async function (knex) {
-    await knex.raw(`
-        CREATE TYPE function_runtime AS ENUM (
-            'runner',
-            'lambda'
+    await knex.raw(`DO $$
+        BEGIN
+            CREATE TYPE function_runtime AS ENUM (
+                'runner',
+                'lambda'
         );
-    `);
-    await knex.raw(`
-        ALTER TABLE plans
-        ADD COLUMN IF NOT EXISTS sync_function_runtime function_runtime DEFAULT 'runner',
-        ADD COLUMN IF NOT EXISTS action_function_runtime function_runtime DEFAULT 'runner',
-        ADD COLUMN IF NOT EXISTS webhook_function_runtime function_runtime DEFAULT 'runner',
-        ADD COLUMN IF NOT EXISTS on_event_function_runtime function_runtime DEFAULT 'runner'
-    `);
+        EXCEPTION
+            WHEN duplicate_object THEN
+                NULL;
+        END
+        $$`);
+    await knex.raw(`ALTER TABLE plans ADD COLUMN IF NOT EXISTS sync_function_runtime function_runtime DEFAULT 'runner'`);
+    await knex.raw(`ALTER TABLE plans ADD COLUMN IF NOT EXISTS action_function_runtime function_runtime DEFAULT 'runner'`);
+    await knex.raw(`ALTER TABLE plans ADD COLUMN IF NOT EXISTS webhook_function_runtime function_runtime DEFAULT 'runner'`);
+    await knex.raw(`ALTER TABLE plans ADD COLUMN IF NOT EXISTS on_event_function_runtime function_runtime DEFAULT 'runner'`);
 };
 
 /**
