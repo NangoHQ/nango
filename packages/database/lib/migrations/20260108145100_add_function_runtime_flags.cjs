1e79057ec94a7cfda8dca095d932764cab27992f
Ross McEwan <ross@nango.dev>
2026-01-08 16:28:57 -0300

    fix(migration): syntax problem

--- packages/database/lib/migrations/20260108145100_add_function_runtime_flags.cjs
+++ packages/database/lib/migrations/20260108145100_add_function_runtime_flags.cjs
@@ -11,10 +11,11 @@ exports.up = async function (knex) {
         );
     `);
     await knex.schema.raw(`
-        ALTER TABLE "plans" ADD COLUMN IF NOT EXISTS "sync_function_runtime" function_runtime DEFAULT 'runner';
-        ALTER TABLE "plans" ADD COLUMN IF NOT EXISTS "action_function_runtime" function_runtime DEFAULT 'runner';
-        ALTER TABLE "plans" ADD COLUMN IF NOT EXISTS "webhook_function_runtime" function_runtime DEFAULT 'runner';
-        ALTER TABLE "plans" ADD COLUMN IF NOT EXISTS "on_event_function_runtime" function_runtime DEFAULT 'runner';
+        ALTER TABLE "plans"
+        ADD COLUMN IF NOT EXISTS "sync_function_runtime" function_runtime DEFAULT 'runner',
+        ADD COLUMN IF NOT EXISTS "action_function_runtime" function_runtime DEFAULT 'runner',
+        ADD COLUMN IF NOT EXISTS "webhook_function_runtime" function_runtime DEFAULT 'runner',
+        ADD COLUMN IF NOT EXISTS "on_event_function_runtime" function_runtime DEFAULT 'runner';
     `);
 };
 
