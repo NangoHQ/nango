863dc8f6b226778bb83906167ef69bed71bef7db
kaposke <guilhermebassa@gmail.com>
2026-01-21 10:27:37 -0300

    Tweak header spacing

--- packages/webapp/src/components-v2/AppHeader.tsx
+++ packages/webapp/src/components-v2/AppHeader.tsx
@@ -6,7 +6,7 @@ import { SlackIcon } from '@/assets/SlackIcon';
 
 export const AppHeader: React.FC = () => {
     return (
-        <header className="h-16 px-10 py-2.5 items-center flex justify-between shrink-0 gap-1.5">
+        <header className="h-16 px-10 pl-2 py-2.5 items-center flex justify-between shrink-0 gap-1.5">
             <Breadcrumbs />
             <div className="flex gap-1.5 justify-end">
                 <ButtonLink to="https://nango.dev/docs" target="_blank" variant="secondary" size="sm">
