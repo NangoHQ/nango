887d06b7f82decde46da859781aac590f06934b2
kaposke <guilhermebassa@gmail.com>
2026-01-21 16:15:35 -0300

    Remove unnecessary key

--- packages/webapp/src/components-v2/Breadcrumbs.tsx
+++ packages/webapp/src/components-v2/Breadcrumbs.tsx
@@ -10,7 +10,7 @@ export const Breadcrumbs = () => {
         <div className="flex gap-1.5 items-center">
             {breadcrumbs.map((breadcrumb) => (
                 <div className="group flex items-center gap-1.5" key={breadcrumb.path}>
-                    <Link to={breadcrumb.path} key={breadcrumb.path}>
+                    <Link to={breadcrumb.path}>
                         <span className="text-breadcrumb-default group-[&:last-child]:text-breadcrumb-press hover:text-breadcrumb-press transition-colors text-body-medium-medium">
                             {breadcrumb.label}
                         </span>
