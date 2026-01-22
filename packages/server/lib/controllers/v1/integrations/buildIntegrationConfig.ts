9e5314a507c8cf1773735ac9a7561e2977fd7083
kaposke <guilhermebassa@gmail.com>
2026-01-22 14:23:14 -0300

    Fix: custom auth creating empty custom object

--- packages/server/lib/controllers/v1/integrations/buildIntegrationConfig.ts
+++ packages/server/lib/controllers/v1/integrations/buildIntegrationConfig.ts
@@ -59,12 +59,15 @@ export async function buildIntegrationConfig(body: PostIntegration['Body'], envi
             config.oauth_client_id = auth.clientId ?? null;
             config.oauth_client_secret = auth.clientSecret ?? null;
             config.app_link = auth.appLink ?? null;
+
             // This is a legacy thing
-            config.custom = {
-                ...config.custom,
-                ...(auth.appId && { app_id: auth.appId }),
-                ...(auth.privateKey && { private_key: Buffer.from(auth.privateKey).toString('base64') })
-            };
+            if (auth.appId || auth.privateKey) {
+                config.custom = {
+                    ...config.custom,
+                    ...(auth.appId && { app_id: auth.appId }),
+                    ...(auth.privateKey && { private_key: Buffer.from(auth.privateKey).toString('base64') })
+                };
+            }
         } else if (auth.authType === 'MCP_OAUTH2') {
             config.oauth_client_id = null;
             config.oauth_client_secret = null;
