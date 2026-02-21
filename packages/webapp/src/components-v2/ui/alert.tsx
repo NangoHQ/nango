ebb4f5ed522716f8532837b9e3e7cf3fed1788cc
kaposke <guilhermebassa@gmail.com>
2026-02-20 22:17:07 -0300

    redesigned toasts

--- packages/webapp/src/components-v2/ui/alert.tsx
+++ packages/webapp/src/components-v2/ui/alert.tsx
@@ -16,8 +16,7 @@ const alertVariants = cva(
         'has-[>div[data-slot="alert-actions"]]:grid-cols-[0_1fr_auto]',
         'has-[>svg]:has-[>div[data-slot="alert-actions"]]:grid-cols-[calc(var(--spacing)*4)_1fr_auto]',
         '[&>svg]:size-4 [&>svg]:text-current [&>svg]:row-span-1 [&>svg]:self-center',
-        'has-[>div[data-slot="alert-title"]]:has-[>div[data-slot="alert-description"]]:grid-rows-2',
-        'has-[>div[data-slot="alert-title"]]:has-[>div[data-slot="alert-description"]]:h-18',
+        'has-[>div[data-slot="alert-title"]]:has-[>div[data-slot="alert-description"]]:grid-rows-[auto_auto]',
         'has-[>div[data-slot="alert-title"]]:has-[>div[data-slot="alert-description"]]:[&>svg]:row-span-2',
         'has-[>div[data-slot="alert-title"]]:has-[>div[data-slot="alert-description"]]:[&>[data-slot="alert-title"]]:self-end',
         'has-[>div[data-slot="alert-title"]]:has-[>div[data-slot="alert-description"]]:[&>[data-slot="alert-description"]]:self-start',
@@ -30,7 +29,7 @@ const alertVariants = cva(
                 success: 'bg-feedback-success-bg text-feedback-success-fg',
                 warning: 'bg-feedback-warning-bg text-feedback-warning-fg',
                 info: 'bg-feedback-info-bg text-feedback-info-fg',
-                destructive: 'bg-feedback-error-bg text-feedback-error-fg'
+                error: 'bg-feedback-error-bg text-feedback-error-fg'
             }
         },
         defaultVariants: {
@@ -44,11 +43,17 @@ function Alert({ className, variant, ...props }: React.ComponentProps<'div'> & V
 }
 
 function AlertTitle({ className, ...props }: React.ComponentProps<'div'>) {
-    return <div data-slot="alert-title" className={cn('col-start-2 line-clamp-1 min-h-4 text-body-large-semi self-center', className)} {...props} />;
+    return <div data-slot="alert-title" className={cn('col-start-2 min-h-4 text-body-large-semi self-center', className)} {...props} />;
 }
 
 function AlertDescription({ className, ...props }: React.ComponentProps<'div'>) {
-    return <div data-slot="alert-description" className={cn('col-start-2 inline-flex gap-1 text-body-medium-regular self-center', className)} {...props} />;
+    return (
+        <div
+            data-slot="alert-description"
+            className={cn('col-start-2 inline-flex gap-1 text-body-medium-regular self-center text-wrap', className)}
+            {...props}
+        />
+    );
 }
 
 function AlertActions({ className, ...props }: React.ComponentProps<'div'>) {
@@ -73,9 +78,8 @@ const alertButtonVariants = cva(
                 info: 'border border-transparent bg-feedback-info-fg text-feedback-info-bg hover:bg-feedback-info-alt/30 hover:text-feedback-info-fg hover:border-feedback-info-fg active:bg-feedback-info-bg active:text-feedback-info-fg',
                 'info-secondary':
                     'border border-feedback-info-fg bg-transparent text-feedback-info-fg hover:bg-feedback-info-alt/30 active:border-transparent active:text-feedback-info-bg active:bg-feedback-info-fg',
-                destructive:
-                    'border border-transparent bg-feedback-error-fg text-feedback-error-bg hover:bg-feedback-error-alt/30 hover:text-feedback-error-fg hover:border-feedback-error-fg active:bg-feedback-error-bg active:text-feedback-error-fg',
-                'destructive-secondary':
+                error: 'border border-transparent bg-feedback-error-fg text-feedback-error-bg hover:bg-feedback-error-alt/30 hover:text-feedback-error-fg hover:border-feedback-error-fg active:bg-feedback-error-bg active:text-feedback-error-fg',
+                'error-secondary':
                     'border border-feedback-error-fg bg-transparent text-feedback-error-fg hover:bg-feedback-error-alt/30 active:border-transparent active:text-feedback-error-bg active:bg-feedback-error-fg',
                 warning:
                     'border border-transparent bg-feedback-warning-fg text-feedback-warning-bg hover:bg-feedback-warning-alt/30 hover:text-feedback-warning-fg hover:border-feedback-warning-fg active:bg-feedback-warning-bg active:text-feedback-warning-fg',
