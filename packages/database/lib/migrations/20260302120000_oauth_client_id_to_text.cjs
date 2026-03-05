a5777b0a4f74a5bfbacd85e583355461225effe8
hassan254-prog <thebigstormtech@gmail.com>
2026-03-05 18:01:09 +0300

    feat(migrations): migrate oauth_client_id to text

---
+++ packages/database/lib/migrations/20260302120000_oauth_client_id_to_text.cjs
@@ -0,0 +1,20 @@
+exports.config = { transaction: false };
+const tableName = '_nango_configs';
+
+/**
+ * @param {import('knex').Knex} knex
+ */
+exports.up = async function (knex) {
+    await knex.schema.alterTable(tableName, (table) => {
+        table.text('oauth_client_id').nullable().defaultTo(null).alter({ alterType: true });
+    });
+};
+
+/**
+ * @param {import('knex').Knex} knex
+ */
+exports.down = async function (knex) {
+    await knex.schema.alterTable(tableName, (table) => {
+        table.string('oauth_client_id', 255).nullable().defaultTo(null).alter({ alterType: true });
+    });
+};
