13fa8478481834fbc33151483dc369e4eabcc509
kaposke <guilhermebassa@gmail.com>
2026-03-25 14:06:55 -0700

    Fix webhook gating

--- packages/webapp/src/pages/Environment/Settings/Webhooks.tsx
+++ packages/webapp/src/pages/Environment/Settings/Webhooks.tsx
@@ -23,7 +23,7 @@ export const Webhooks: React.FC = () => {
     const environment = environmentAndAccount?.environment;
 
     const { can } = usePermissions();
-    const canEditEnvironment = can(permissions.canWriteProdEnvironment) || !environment?.is_production;
+    const canWriteWebhooks = can(permissions.canWriteProdWebhooks) || !environment?.is_production;
 
     const onSave = async (body: PatchWebhook['Body']) => {
         try {
@@ -60,7 +60,7 @@ export const Webhooks: React.FC = () => {
                             initialValue={environmentAndAccount.webhook_settings.primary_url || ''}
                             onSave={(value) => onSave({ primary_url: value })}
                             validate={validateUrl}
-                            canEdit={canEditEnvironment}
+                            canEdit={canWriteWebhooks}
                         />
                     </div>
                     <div className="flex flex-col gap-2">
@@ -71,7 +71,7 @@ export const Webhooks: React.FC = () => {
                             initialValue={environmentAndAccount.webhook_settings.secondary_url || ''}
                             onSave={(value) => onSave({ secondary_url: value })}
                             validate={validateUrl}
-                            canEdit={canEditEnvironment}
+                            canEdit={canWriteWebhooks}
                         />
                     </div>
                 </div>
