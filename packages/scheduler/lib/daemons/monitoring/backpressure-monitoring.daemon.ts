b3f71c4bbcbc03782f48e1b83afef0a1a8254f7e
Pau Freixes <pau@nango.dev>
2026-04-12 14:41:16 +0200

    Revert "fix(orchestrator): Disable max concurrency backpressure query (#5841)"
    This reverts commit aae4931ee5a2981dcb1baced6661446ddcb557ea.

--- packages/scheduler/lib/daemons/monitoring/backpressure-monitoring.daemon.ts
+++ packages/scheduler/lib/daemons/monitoring/backpressure-monitoring.daemon.ts
@@ -1,4 +1,8 @@
+import { metrics } from '@nangohq/utils';
+
 import { envs } from '../../env.js';
+import * as tasks from '../../models/tasks.js';
+import { logger } from '../../utils/logger.js';
 import { SchedulerDaemon } from '../daemon.js';
 
 import type knex from 'knex';
@@ -14,9 +18,16 @@ export class BackpressureMonitoringDaemon extends SchedulerDaemon {
         });
     }
 
-    run(): Promise<void> {
-        // Backpressure monitoring is intentionally disabled to avoid querying
-        // whether groups have reached their max concurrency.
-        return Promise.resolve();
+    async run(): Promise<void> {
+        const result = await tasks.getGroupsWithBackpressure(this.db, { limit: envs.ORCHESTRATOR_BACKPRESSURE_MONITORING_TOP_N });
+        if (result.isErr()) {
+            logger.error(result.error);
+            return;
+        }
+
+        for (const { group_key, queued } of result.value) {
+            const primitive = group_key.split(':')[0]!;
+            metrics.gauge(metrics.Types.ORCH_QUEUE_BACKPRESSURE, queued, { groupKey: group_key, primitive });
+        }
     }
 }
