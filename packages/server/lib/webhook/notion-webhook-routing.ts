b27964603513f0cd466f188547d890d854188c2a
kimiduck <ops@llmvault.dev>
2026-04-21 22:02:29 +0100

    chore: log body

--- packages/server/lib/webhook/notion-webhook-routing.ts
+++ packages/server/lib/webhook/notion-webhook-routing.ts
@@ -25,7 +25,7 @@ const route: WebhookHandler<NotionWebhook | NotionWebhookVerification> = async (
     const verificationToken = nango.integration.custom?.['webhookSecret'];
 
     if ('verification_token' in body) {
-        logger.info('Received verification request, skipping signature validation', { configId: nango.integration.id });
+        logger.info('Received verification request, skipping signature validation', { configId: nango.integration.id, body });
     } else if (verificationToken) {
         if (!signature) {
             logger.error('missing signature', { configId: nango.integration.id });
