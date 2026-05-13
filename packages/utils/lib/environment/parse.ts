e88802e8e67a747e96aac38b31d2c7d0f5eff4da
Thomas Bonnin <233326+TBonnin@users.noreply.github.com>
2026-05-13 17:59:15 -0400

    chore: add env var to control how many old syncs to delete (#6138)
    Adding a separate env var to control how many syncs must be deleted by
    the deleteOldData cron job. It was originally using the default limit of
    1000 but was dropped to 2 when investigating the slowdown of the records
    db.
    Starting with a conservative value of 25 that could be increased to
    match the desired rate
    
    <!-- Describe the problem and your solution --> 
    
    <!-- Issue ticket number and link (if applicable) -->
    
    <!-- Testing instructions (skip if just adding/editing providers) -->

--- packages/utils/lib/environment/parse.ts
+++ packages/utils/lib/environment/parse.ts
@@ -73,6 +73,7 @@ export const ENVS = z.object({
     CRON_DELETE_OLD_OAUTH_SESSION_MAX_DAYS: z.coerce.number().optional().default(2),
     CRON_DELETE_OLD_INVITATIONS_MAX_DAYS: z.coerce.number().optional().default(2),
     CRON_DELETE_OLD_SYNCS_MAX_DAYS: z.coerce.number().optional().default(1),
+    CRON_DELETE_OLD_SYNCS_LIMIT: z.coerce.number().optional().default(25),
     CRON_DELETE_OLD_CONFIGS_MAX_DAYS: z.coerce.number().optional().default(31),
     CRON_DELETE_OLD_SYNC_CONFIGS_MAX_DAYS: z.coerce.number().optional().default(31),
     CRON_DELETE_OLD_CONNECTIONS_MAX_DAYS: z.coerce.number().optional().default(31),
