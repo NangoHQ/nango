a67f27569f2f4274935738f0e7347a22afd2225c
kaposke <guilhermebassa@gmail.com>
2026-05-18 17:05:52 -0300

    UI feedback

--- packages/webapp/src/hooks/useToast.tsx
+++ packages/webapp/src/hooks/useToast.tsx
@@ -7,10 +7,11 @@ interface ToastProps {
     description?: string;
     variant: 'success' | 'error' | 'warning' | 'info';
     id: string | number;
+    action?: React.ReactNode;
 }
 
-function toast({ title, description, variant }: Omit<ToastProps, 'id'>) {
-    return sonnerToast.custom((id) => <Toast id={id} title={title} description={description} variant={variant} />);
+function toast({ title, description, variant, action }: Omit<ToastProps, 'id'>) {
+    return sonnerToast.custom((id) => <Toast id={id} title={title} description={description} variant={variant} action={action} />);
 }
 
 export function useToast() {
