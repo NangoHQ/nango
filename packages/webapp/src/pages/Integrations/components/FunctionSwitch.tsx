4a780cd9de9d47eddef459ad5684ff188d6b8ced
kaposke <guilhermebassa@gmail.com>
2026-05-20 16:08:44 -0300

    Fixes after rebase

--- packages/webapp/src/pages/Integrations/components/FunctionSwitch.tsx
+++ packages/webapp/src/pages/Integrations/components/FunctionSwitch.tsx
@@ -12,10 +12,10 @@ import { useConfirmDialog } from '@/hooks/useConfirmDialog';
 import { usePermissions } from '@/hooks/usePermissions';
 import { useStore } from '@/store';
 
-import type { ApiError, ApiIntegration, NangoActionFunctionDeployed, NangoSyncFunctionDeployed } from '@nangohq/types';
+import type { ApiError, ApiIntegration, DeployedNangoActionFunction, DeployedNangoSyncFunction } from '@nangohq/types';
 
 export const FunctionSwitch: React.FC<{
-    flow: NangoSyncFunctionDeployed | NangoActionFunctionDeployed;
+    flow: DeployedNangoSyncFunction | DeployedNangoActionFunction;
     integration: ApiIntegration;
 }> = ({ flow, integration }) => {
     const { toast } = useToast();
