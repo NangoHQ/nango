0df9fcd292545afb8cfd1128eb64a3b339d73376
Matej Vobornik <vobornik.matej@gmail.com>
2026-06-03 18:40:58 +0200

    fix(webapp): fix prettier formatting in OperationDrawer.tsx

--- packages/webapp/src/pages/Logs/components/OperationDrawer.tsx
+++ packages/webapp/src/pages/Logs/components/OperationDrawer.tsx
@@ -21,7 +21,10 @@ export const OperationDrawer: React.FC<{ operationId: string; onClose: (open: bo
                 <SheetTitle className="sr-only">Operation Details</SheetTitle>
                 <div className="relative h-full select-text">
                     <div className="absolute right-6 top-[35px]">
-                        <SheetClose title="Close" className="bg-transparent text-text-light-gray hover:text-white focus:text-white transition-colors w-8 h-6 flex items-center justify-center">
+                        <SheetClose
+                            title="Close"
+                            className="bg-transparent text-text-light-gray hover:text-white focus:text-white transition-colors w-8 h-6 flex items-center justify-center"
+                        >
                             <Cross1Icon />
                         </SheetClose>
                     </div>
