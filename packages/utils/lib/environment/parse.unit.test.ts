ced8eb12c60502b9ac7c439d21769aae4f8cc222
Ryan West <rbwest48@gmail.com>
2026-06-26 09:46:00 -0400

    Fix test name

--- packages/utils/lib/environment/parse.unit.test.ts
+++ packages/utils/lib/environment/parse.unit.test.ts
@@ -19,7 +19,7 @@ describe('parse', () => {
         expect(res).toMatchObject({ NANGO_DB_SSL: false, NANGO_PERSIST_PORT: 3007 });
     });
 
-    it('should parse NANGO_OAUTH2_TOKEN_MAX_LENGTH', () => {
+    it('should parse NANGO_SERVER_OAUTH2_TOKEN_MAX_LENGTH', () => {
         expect(parseEnvs(ENVS, {}).NANGO_SERVER_OAUTH2_TOKEN_MAX_LENGTH).toBe(16 * 1024);
         expect(parseEnvs(ENVS, { NANGO_SERVER_OAUTH2_TOKEN_MAX_LENGTH: '32768' }).NANGO_SERVER_OAUTH2_TOKEN_MAX_LENGTH).toBe(32768);
     });
