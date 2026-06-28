e57e111e853b5f1c8d04d5950154dc9cbb32421e
Quentin de Quelen <quentin@meilisearch.com>
2026-06-28 20:35:58 +0200

    chore(meilisearch): scaffold zero-yaml integration project

---
+++ integrations/meilisearch/index.ts
@@ -0,0 +1,7 @@
+import './actions/generate-tenant-token.js';
+import './actions/search-documents.js';
+import './actions/get-documents.js';
+import './actions/add-documents.js';
+import './actions/update-documents.js';
+import './actions/delete-documents.js';
+import './actions/get-task.js';
