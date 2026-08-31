6e9c8078871aa89dbcac62be584fd24b0d51f239
Erick R <erickkrocha@gmail.com>
2026-08-31 19:25:50 -0300

    chore: address PR concerns

--- packages/server/lib/controllers/environment/deleteApiKey.ts
+++ packages/server/lib/controllers/environment/deleteApiKey.ts
@@ -45,7 +45,7 @@ export const deletePublicEnvironmentApiKey = asyncWrapper<DeletePublicApiKey>(as
 
     const key = await customerKeyService.getApiKeyByUuid(db.knex, keyUuid, environment.id, account.id);
     if (key.isErr()) {
-        report(key.error);
+        report(key.error, { accountId: account.id, environmentId: environment.id });
         res.status(500).send({ error: { code: 'server_error', message: 'Failed to delete API key' } });
         return;
     }
