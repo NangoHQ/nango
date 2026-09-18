283a4e17660820de09eaefa262bcd9e04c3042c7
hassan254-prog <thebigstormtech@gmail.com>
2026-09-18 11:52:29 +0300

    add try catch

--- packages/server/lib/hooks/connection/providers/apple-calendar/credentials-verification.ts
+++ packages/server/lib/hooks/connection/providers/apple-calendar/credentials-verification.ts
@@ -5,16 +5,24 @@ import type { InternalNango as Nango } from '../../credentials-verification-scri
 export default async function execute(nango: Nango) {
     const { provider_config_key } = nango.getConnection();
 
-    const response = await nango.proxy<string>({
-        method: 'PROPFIND',
-        endpoint: '/',
-        providerConfigKey: provider_config_key,
-        headers: {
-            depth: '0',
-            'content-type': 'application/xml; charset=utf-8'
-        },
-        data: '<propfind xmlns="DAV:"><prop><current-user-principal/></prop></propfind>'
-    });
+    let response;
+    try {
+        response = await nango.proxy<string>({
+            method: 'PROPFIND',
+            endpoint: '/',
+            providerConfigKey: provider_config_key,
+            headers: {
+                depth: '0',
+                'content-type': 'application/xml; charset=utf-8'
+            },
+            data: '<propfind xmlns="DAV:"><prop><current-user-principal/></prop></propfind>'
+        });
+    } catch (err) {
+        if (isAxiosError(err)) {
+            throw new Error('Incorrect Credentials');
+        }
+        throw err;
+    }
 
     if (isAxiosError(response) || response.status !== 207 || typeof response.data !== 'string' || !response.data.includes('current-user-principal')) {
         throw new Error('Incorrect Credentials');
