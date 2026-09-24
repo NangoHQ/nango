d188e8e2b91c533ff4307ba354910896c461d780
Thomas Bonnin <233326+TBonnin@users.noreply.github.com>
2026-09-24 11:20:32 +0200

    fix: add tracing

--- packages/server/lib/hooks/connection/on/post-connection-creation.ts
+++ packages/server/lib/hooks/connection/on/post-connection-creation.ts
@@ -1,3 +1,5 @@
+import tracer from 'dd-trace';
+
 import db from '@nangohq/database';
 import { defaultOperationExpiration, OtlpSpan } from '@nangohq/logs';
 import { functionConfigService, getFunctionMaxConcurrency, onEventScriptService } from '@nangohq/shared';
@@ -13,86 +15,92 @@ export async function postConnectionCreation(
     provider: string,
     logContextGetter: LogContextGetter
 ): Promise<void> {
-    if (!createdConnection) {
-        return;
-    }
-    const { environment, account, connection } = createdConnection;
-    const { config_id } = connection;
+    await tracer.trace('nango.connection.postConnectionCreation', async (span) => {
+        if (!createdConnection) {
+            return;
+        }
+        const { environment, account, connection } = createdConnection;
+        const { config_id } = connection;
 
-    if (!config_id || !connection.id) {
-        return;
-    }
+        if (!config_id || !connection.id) {
+            return;
+        }
 
-    const event = 'post-connection-creation';
+        const event = 'post-connection-creation';
 
-    const createLogCtx = async (id: number, name: string) => {
-        const logCtx = await logContextGetter.create(
-            { operation: { type: 'events', action: 'post_connection_creation' }, expiresAt: defaultOperationExpiration.action() },
-            {
-                account,
-                environment,
-                integration: { id: config_id, name: connection.provider_config_key, provider },
-                connection: { id: connection.id, name: connection.connection_id },
-                syncConfig: { id, name },
-                meta: { event }
+        const createLogCtx = async (id: number, name: string) => {
+            const logCtx = await logContextGetter.create(
+                { operation: { type: 'events', action: 'post_connection_creation' }, expiresAt: defaultOperationExpiration.action() },
+                {
+                    account,
+                    environment,
+                    integration: { id: config_id, name: connection.provider_config_key, provider },
+                    connection: { id: connection.id, name: connection.connection_id },
+                    syncConfig: { id, name },
+                    meta: { event }
+                }
+            );
+            logCtx.attachSpan(new OtlpSpan(logCtx.operation));
+            return logCtx;
+        };
+
+        const search = await functionConfigService.search(db.knex, {
+            environmentId: environment.id,
+            filter: { integrationKey: connection.provider_config_key, enabled: true, trigger: { kind: 'event', event } }
+        });
+        if (search.isErr()) {
+            span.setTag('error', search.error);
+        }
+
+        try {
+            const postConnectionCreationScripts = await onEventScriptService.getByConfig(config_id, event);
+
+            for (const script of postConnectionCreationScripts) {
+                const { name, file_location: fileLocation, version } = script;
+
+                const logCtx = await createLogCtx(script.id, script.name);
+
+                const res = await getOrchestrator().triggerOnEventScript({
+                    accountId: account.id,
+                    connection: createdConnection.connection,
+                    version,
+                    name,
+                    fileLocation,
+                    sdkVersion: script.sdk_version,
+                    async: true,
+                    maxConcurrency: envs.ON_EVENT_ENVIRONMENT_MAX_CONCURRENCY,
+                    logCtx
+                });
+
+                if (res.isErr()) {
+                    await logCtx.failed();
+                }
             }
-        );
-        logCtx.attachSpan(new OtlpSpan(logCtx.operation));
-        return logCtx;
-    };
+        } catch (err) {
+            span.setTag('error', err);
+        }
 
-    const functions = await functionConfigService.search(db.knex, {
-        environmentId: environment.id,
-        filter: { integrationKey: connection.provider_config_key, enabled: true, trigger: { kind: 'event', event } }
+        for (const { config, currentVersion } of search.isOk() ? search.value : []) {
+            const logCtx = await createLogCtx(currentVersion.id, config.name);
+            const res = await getOrchestrator().invokeFunction({
+                environment,
+                connection,
+                functionConfigId: config.id,
+                functionName: config.name,
+                trigger: {
+                    kind: 'event',
+                    input: { event },
+                    connection: { connectionId: connection.connection_id, integrationId: connection.provider_config_key }
+                },
+                async: true,
+                retryMax: 0,
+                maxConcurrency: getFunctionMaxConcurrency(currentVersion),
+                logCtx
+            });
+            if (res.isErr()) {
+                await logCtx.error(res.error.message, { error: res.error });
+                await logCtx.failed();
+            }
+        }
     });
-    if (functions.isErr()) {
-        throw functions.error;
-    }
-
-    const postConnectionCreationScripts = await onEventScriptService.getByConfig(config_id, event);
-
-    for (const script of postConnectionCreationScripts) {
-        const { name, file_location: fileLocation, version } = script;
-
-        const logCtx = await createLogCtx(script.id, script.name);
-
-        const res = await getOrchestrator().triggerOnEventScript({
-            accountId: account.id,
-            connection: createdConnection.connection,
-            version,
-            name,
-            fileLocation,
-            sdkVersion: script.sdk_version,
-            async: true,
-            maxConcurrency: envs.ON_EVENT_ENVIRONMENT_MAX_CONCURRENCY,
-            logCtx
-        });
-
-        if (res.isErr()) {
-            await logCtx.failed();
-        }
-    }
-
-    for (const { config, currentVersion } of functions.value) {
-        const logCtx = await createLogCtx(currentVersion.id, config.name);
-        const res = await getOrchestrator().invokeFunction({
-            environment,
-            connection,
-            functionConfigId: config.id,
-            functionName: config.name,
-            trigger: {
-                kind: 'event',
-                input: { event },
-                connection: { connectionId: connection.connection_id, integrationId: connection.provider_config_key }
-            },
-            async: true,
-            retryMax: 0,
-            maxConcurrency: getFunctionMaxConcurrency(currentVersion),
-            logCtx
-        });
-        if (res.isErr()) {
-            await logCtx.error(res.error.message, { error: res.error });
-            await logCtx.failed();
-        }
-    }
 }
